aeca5114c70866800f050607777070f694451212
Tab Memmott <memmott@ohsu.edu>
2017-10-15 21:23:49 -0700

    remove init from end test files. modify init_save for secondary session handling. give io it's own params file. make first save test.

--- io/save.py
+++ io/save.py
@@ -1,4 +1,4 @@
-import os, errno
+import os, errno, sys
 import datetime
 from shutil import copy2
 
@@ -11,22 +11,29 @@ def init_save_data_structure(data_save_path, user_information, parameters_used):
 	'''
 
 	# make an experiment folder : note datetime is in utc ('2013-11-18T08:18:31.809000')
-	save_folder_name = data_save_path + user_information + datetime.datetime.now().isoformat()
-	helper_folder_name = save_folder_name + '/helpers/'
-
+	save_folder_name = data_save_path + user_information
+	save_folder_run_name = save_folder_name + '/' + user_information + datetime.datetime.now().isoformat()
+	helper_folder_name = save_folder_run_name + '/helpers/'
 	# try making the given path
 	try:
 		# make a directory to 
 	    os.makedirs(save_folder_name)
+	    os.makedirs(save_folder_run_name)
 	    os.makedirs(helper_folder_name)
 	except OSError as e:
 		# If the error is anything other than the file already existing, raise an error
 	    if e.errno != errno.EEXIST:
 	        raise
 
+	    # since this is only called on init, we can just make another instance for this user
+	    os.makedirs(save_folder_run_name)
+	    os.makedirs(helper_folder_name)
+	try:
 
-	# put in static things
-	copy2(parameters_used, save_folder_name)
+		# put in static things
+		copy2(parameters_used, save_folder_name)
+	except Exception as e:
+		print e
 
 	# return path for completion or other data type saving needs
 	return save_folder_name
--- io/tests/test_save.py
+++ io/tests/test_save.py
@@ -2,6 +2,7 @@ import os
 import pytest
 import sys
 import datetime
+sys.path.append('....')
 sys.path.append('..')
 sys.path.append('.')
 from save import init_save_data_structure
@@ -11,7 +12,7 @@ def test_init_save_data_structure():
 	# mock the necessary inputs for initializing the save data function
 	data_save_path = 'data/'
 	user_information = 'user'
-	parameters_used = '../gui/utility/parameters.json'
+	parameters_used = 'parameters.json'
 
 
 	save_folder_name = init_save_data_structure(
@@ -19,7 +20,7 @@ def test_init_save_data_structure():
 		user_information,
 		parameters_used)
 
+	# make sure that it returns the root folder for later usage. Other data material
+		# should be saved below this.
 
-	assert save_folder_name == save_folder_name
-
-# test_init_save_data_structure()
+	assert save_folder_name == data_save_path + user_information
