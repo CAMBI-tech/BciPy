466548fee9ea2588a0530da904ce9346f5eca575
Tab Memmott <memmott@ohsu.edu>
2017-10-19 12:27:42 -0700

    make load functions more robust and add a test file

--- io/load.py
+++ io/load.py
@@ -9,15 +9,19 @@ from tkFileDialog import askopenfilename
 
 def load_json_parameters(path):
     # loads in json parameters and turns it into a dictionary
-    with codecsopen(path, 'r', encoding='utf-8') as f:
-        parameters = []
-        try:
-            parameters = jsonload(f)
-        except ValueError as error:
-            warn("Parameters file is formatted incorrectly!", Warning)
-            raise error
+    try:
+        with codecsopen(path, 'r', encoding='utf-8') as f:
+            parameters = []
+            try:
+                parameters = jsonload(f)
+            except ValueError:
+                raise ValueError(
+                    "Parameters file is formatted incorrectly!")
 
-    f.close()
+        f.close()
+
+    except IOError:
+        raise IOError("Incorrect path to parameters given! Please try again.")
 
     return parameters
 
@@ -25,8 +29,12 @@ def load_json_parameters(path):
 def load_experimental_data():
 
     # use python's internal gui to call file explorers and get the filename
-    Tk().withdraw()  # we don't want a full GUI
-    filename = askopenfilename()  # show dialog box and return the path
+    try:
+        Tk().withdraw()  # we don't want a full GUI
+        filename = askopenfilename()  # show dialog box and return the path
+        print("Loaded Experimental Data From: %s" % filename)
+    except:
+        pass
 
     return filename
 
