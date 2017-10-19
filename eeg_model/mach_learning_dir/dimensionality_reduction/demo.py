ef9a1a7c2c2259294725a78127a9a353c96b9412
Aziz <akocanaogullari@ece.neu.edu>
2017-10-19 11:08:02 -0400

    Channelwise PCA implementation in python.

--- eeg_model/mach_learning_dir/dimensionality_reduction/demo.py
+++ eeg_model/mach_learning_dir/dimensionality_reduction/demo.py
@@ -1,30 +1,58 @@
 import sys
 
 sys.path.append('.\eeg_model\mach_learning_dir\dimensionality_reduction')
-from function_dim_reduction import PrincipalComponentAnalysis
+from function_dim_reduction import PrincipalComponentAnalysis, \
+    ChannelWisePrincipalComponentAnalysis
 import numpy as np
 
-pca = PrincipalComponentAnalysis()
 
-dim_x = 16
-num_x_p = 2000
-num_x_n = 1000
-var_tol = 0.80
+def test_pca(x):
+    pca = PrincipalComponentAnalysis()
+    y = pca.fit_transform(x)
+
+    return [y]
+
+# pca = PrincipalComponentAnalysis()
+#
+# dim_x = 16
+# num_x_p = 2000
+# num_x_n = 1000
+# var_tol = 0.80
+#
+# # We only require the data not labels
+# x_p = 2 * np.random.randn(num_x_p, dim_x)
+# x_n = np.random.randn(num_x_n, dim_x)
+#
+# x = np.concatenate(np.asarray([x_p, x_n]), 0)
+# len_x = x.shape[0]
+#
+# pca.fit(x[0:int(len_x * 9 / 10), :])
+# y = pca.transform(x[int(len_x * 9 / 10):-1, :])
+# print(y.shape)
+#
+# pca.fit(x[0:int(len_x * 9 / 10), :], var_tol=var_tol)
+# y = pca.transform(x[0:int(len_x * 9 / 10), :])
+# print(y.shape)
+#
+# y2 = pca.fit_transform(x[0:int(len_x * 9 / 10), :], var_tol=var_tol)
+# print(np.sum(y2 - y))
+
+##
+
+num_ch = 16
+dim_x = 20
+num_x_p = 100
+num_x_n = 0
+var_tol = 0.2
 
 # We only require the data not labels
-x_p = 2 * np.random.randn(num_x_p, dim_x)
-x_n = np.random.randn(num_x_n, dim_x)
+x_p = 2 * np.random.randn(num_ch, num_x_p, dim_x)
+x_n = np.random.randn(num_ch, num_x_n, dim_x)
+x = np.concatenate((x_n, x_p), axis=1)
+len_x = x.shape[1]
 
-x = np.concatenate(np.asarray([x_p, x_n]), 0)
-len_x = x.shape[0]
+cw_pca = ChannelWisePrincipalComponentAnalysis(num_ch=num_ch)
+y = cw_pca.fit_transform(x, var_tol=var_tol)
+y_2 = cw_pca.transform(x)
+print('x: {}, y: {}'.format(x.shape, y.shape))
 
-pca.fit(x[0:int(len_x * 9 / 10), :])
-y = pca.transform(x[int(len_x * 9 / 10):-1, :])
-print(y.shape)
-
-pca.fit(x[0:int(len_x * 9 / 10), :], var_tol=var_tol)
-y = pca.transform(x[0:int(len_x * 9 / 10), :])
-print(y.shape)
-
-y2 = pca.fit_transform(x[0:int(len_x * 9 / 10), :], var_tol=var_tol)
-print(np.sum(y2 - y))
--- eeg_model/mach_learning_dir/dimensionality_reduction/function_dim_reduction.py
+++ eeg_model/mach_learning_dir/dimensionality_reduction/function_dim_reduction.py
@@ -71,3 +71,93 @@ class PrincipalComponentAnalysis(PCA):
 
     def opt_param(self):
         return 0
+
+
+class ChannelWisePrincipalComponentAnalysis(object):
+    """ Channel wise PCA application. Creates PCA objects respective to the
+        number of channels.
+        Attr:
+            n_components(int): Number of components in PCA
+            copy(bool): Saves the matrix if  True and updates on each fit()
+            whiten(bool): Whitens the PCA matrix to form a tigth frame
+            svd_solver(string): SV Decomposition solver metgod
+            tol=var_tol(float): Unfortunately I re-implemented it
+                Tolarance to the singular values of the matrix
+            random_state(seed): Random state seed
+            num_ch(int): number of channels in expected data
+            var_tol(float): Tolerance to the variance
+         """
+
+    def __init__(self, n_components=None, copy=True, whiten=False,
+                 svd_solver='auto', tol=0.0, iterated_power='arpack',
+                 random_state=None, num_ch=1):
+        self.list_pca = []
+        for idx in range(num_ch):
+            self.list_pca.append(PCA(n_components=n_components, copy=copy,
+                                     whiten=whiten,
+                                     svd_solver=svd_solver, tol=tol,
+                                     iterated_power=iterated_power,
+                                     random_state=random_state))
+        self.var_tol = None
+        self.num_ch = num_ch
+
+    def fit(self, x, y=None, var_tol=None):
+        """ Inherits PCA fit() function from scikit-learn. Fits the
+            transformation matrix wrt. tolerance to each PCA.
+            Args:
+                x(ndarray[float]): C x N x k data array
+                y(ndarray[int]): N x k observation (class) array
+                    N is number of samples k is dimensionality of features
+                    C is number of channels
+                var_tol(float): Threshold to remove lower variance dims.
+                """
+
+        if var_tol:
+            self.var_tol = var_tol
+
+        for i in range(self.num_ch):
+            self.list_pca[i].fit(x[i], y)
+            if var_tol:
+                max_var = self.list_pca[i].explained_variance_ratio_[0]
+                self.list_pca[i].n_components = np.sum(np.array(
+                    self.list_pca[
+                        i].explained_variance_ratio_ >= max_var * self.var_tol))
+
+            self.list_pca[i].fit(x[i, :, :], y)
+
+    def transform(self, x, y=None):
+        f_vector = []
+        for i in range(self.num_ch):
+            f_vector.append(self.list_pca[i].transform(x[i, :, :], y))
+
+        return np.concatenate(f_vector, axis=1)
+
+    def fit_transform(self, x, y=None, var_tol=None):
+        """ Fits parameters wrt. the input matrix and outputs corresponding
+            reduced form feature vector.
+            Args:
+                x(ndarray[float]): C x N x k data array
+                y(ndarray[int]): N x k observation (class) array
+                    N is number of samples k is dimensionality of features
+                    C is number of channels
+                var_tol(float): Threshold to remove lower variance dims.
+            Return:
+                y(ndarray(float)): N x ( sum_i (C x k')) data array
+                    where k' is the new dimension for each PCA
+                """
+
+        if var_tol:
+            self.var_tol = var_tol
+
+        f_vector = []
+        for i in range(self.num_ch):
+            self.list_pca[i].fit(x[i, :, :], y)
+            if var_tol:
+                max_var = self.list_pca[i].explained_variance_ratio_[0]
+                self.list_pca[i].n_components = np.sum(np.array(
+                    self.list_pca[
+                        i].explained_variance_ratio_ >= max_var * self.var_tol))
+
+            f_vector.append(self.list_pca[i].fit_transform(x[i, :, :], y))
+
+        return np.concatenate(f_vector, axis=1)
