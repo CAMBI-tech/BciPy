440cf45c9e8e3122dbafaf5b2db08f0aa1c29ca3
BerkanKadioglu <kadioglub@ece.neu.edu>
2017-10-19 22:47:58 -0400

    moduleLoader fixed

--- utils/moduleLoader.py
+++ utils/moduleLoader.py
@@ -3,15 +3,15 @@ import pip, os, sys
 cDir = os.path.dirname(os.path.realpath(sys.argv[0]))
 print 'Current directory: ' + cDir
 
-f = open("requirements.txt",'r')
+f = open("\\..\\requirements.txt",'r')
 answer = raw_input('Is your OS 64 Bit?(type y or n and press enter): ')
 
 if answer.lower()[0] == 'y':
-    scipy = cDir + '\\scipy-0.19.1-cp27-cp27m-win_amd64.whl'
-    numpy = cDir + '\\numpy-1.13.1+mkl-cp27-cp27m-win_amd64.whl'
+    scipy = cDir + '\\whl\\scipy-0.19.1-cp27-cp27m-win_amd64.whl'
+    numpy = cDir + '\\whl\\numpy-1.13.1+mkl-cp27-cp27m-win_amd64.whl'
 elif answer.lower()[0] == 'n':
-    scipy = cDir + '\\scipy-0.19.1-cp27-cp27m-win32.whl'
-    numpy = cDir + '\\numpy-1.13.1+mkl-cp27-cp27m-win32.whl'
+    scipy = cDir + '\\whl\\scipy-0.19.1-cp27-cp27m-win32.whl'
+    numpy = cDir + '\\whl\\numpy-1.13.1+mkl-cp27-cp27m-win32.whl'
 
 try:
     pip.main(['install', numpy])
