4d4dbd692512b371979949388a541cb63f781c8d
Tab Memmott <memmott@ohsu.edu>
2017-10-30 11:05:43 -0700

    add some helpers to get calibration working (to be replaced by Aziz's text generator function). to-do better positioning from parameters... tuple conversion is off.

--- bci_tasks/RSVP/calibration.py
+++ bci_tasks/RSVP/calibration.py
@@ -1,7 +1,9 @@
 # Calibration Task for RSVP
 
 from __future__ import division
-from psychopy import visual, core
+from psychopy import core
+
+import pdb
 
 from display.rsvp_disp_modes import CalibrationTask
 from utils.trigger_helpers import _write_triggers_from_sequence_calibration
@@ -14,41 +16,39 @@ def RSVP_calibration_task(win, daq, parameters, file_save):
     clock = core.StaticPeriod(screenHz=frame_rate)
     experiment_clock = core.MonotonicClock(start_time=None)
 
-    task_text = ['1/100', '2/100', '3/100']
     try:
         rsvp = CalibrationTask(
             window=win, clock=clock,
             experiment_clock=experiment_clock,
             text_information=parameters['text_text']['value'],
             color_information=parameters['color_text']['value'],
-            pos_information=parameters['pos_text']['value'],
-            height_information=parameters['txt_height']['value'],
+            # pos_information=parameters['pos_text']['value'],
+            height_information=float(parameters['txt_height']['value']),
             font_information=parameters['font_text']['value'],
             color_task=['white'],
             font_task=parameters['font_task']['value'],
-            text_task=task_text[0],
-            height_task=parameters['height_task']['value'],
+            # text_task=task_text[0],
+            height_task=float(parameters['height_task']['value']),
             font_sti=parameters['font_sti']['value'],
-            pos_sti=parameters['pos_sti']['value'],
-            sti_height=parameters['sti_height']['value'],
+            # pos_sti=parameters['pos_sti']['value'],
+            sti_height=float(parameters['sti_height']['value']),
             ele_list_sti=['a'] * 10, color_list_sti=['white'] * 10,
             time_list_sti=[3] * 10,
-            tr_pos_bg=parameters['tr_pos_bg']['value'],
-            bl_pos_bg=parameters['bl_pos_bg']['value'],
-            size_domain_bg=parameters['size_domain_bg']['value'],
+            # tr_pos_bg=parameters['tr_pos_bg']['value'],
+            # bl_pos_bg=parameters['bl_pos_bg']['value'],
+            size_domain_bg=int(parameters['size_domain_bg']['value']),
             color_bg_txt=parameters['color_bg_txt']['value'],
             font_bg_txt=parameters['font_bg_txt']['value'],
             color_bar_bg=parameters['color_bar_bg']['value'],
             is_txt_sti=parameters['is_txt_sti']['value'])
     except Exception as e:
-        raise e
+        pdb.set_trace()
     # Init Task
-    run = True
-    while run is True:
+    # run = True
+    # while run is True:
         # to-do allow pausing and exiting. See psychopy getKeys()
-        (task_text, task_color,
-            task_timing, ele_sti, timing_sti, color_sti) = get_task_info()
-
+    (task_text, task_color, ele_sti, timing_sti, color_sti) = get_task_info()
+    try:
         for idx_o in range(len(task_text)):
 
             rsvp.update_task_state(
@@ -56,7 +56,7 @@ def RSVP_calibration_task(win, daq, parameters, file_save):
                 color_list=task_color[idx_o])
             rsvp.draw_static()
             win.flip()
-            rsvp.sti.height = parameters['sti_height']['value']
+            rsvp.sti.height = float(parameters['sti_height']['value'])
 
             # Schedule a sequence
             rsvp.ele_list_sti = ele_sti[idx_o]
@@ -73,11 +73,31 @@ def RSVP_calibration_task(win, daq, parameters, file_save):
 
             core.wait(.5)
 
-        run = False
-    daq.stop_acquistion()
+            # run = False
+        # daq.stop_acquistion()
 
-    # close the window and file
-    win.close()
-    file.close()
+        # close the window and file
+        # win.close()
+        # file.close()
+    except Exception as e:
+        pdb.set_trace()
 
-    return (daq, file)
+    return (daq, file_save)
+
+
+def get_task_info():
+    ele_sti = [
+        ['B', '+', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', '<', '-', 'L'],
+        ['E', '+', 'F', 'G', 'E', '-', 'S', 'Q', 'W', 'E', '<', 'A', 'Z'],
+        ['W', '+', 'F', 'G', 'E', '-', 'S', 'Q', 'W', 'E', '<', 'A', 'Z'],
+        ['Q', '+', 'F', 'G', 'E', '-', 'S', 'Q', 'W', 'E', '<', 'A', 'Z']]
+    color_sti = [['green', 'red', 'white', 'white', 'white', 'white', 'white',
+                  'white', 'white', 'white', 'white', 'white', 'white']] * 4
+    task_text = ['1/100', '2/100', '3/100', '4/100']
+    task_color = [['white'], ['white'], ['white'], ['white']]
+    time_flash = .25
+    time_target = 2
+    time_cross = .6
+    timing_sti = [[time_target] + [time_cross] + [time_flash] * (len(ele_sti[0]) - 1)] * 4
+
+    return (task_text, task_color, ele_sti, timing_sti, color_sti)
