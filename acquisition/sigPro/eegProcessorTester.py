ceb712a5a3954e2bbeaf70005c58481feca6e454
BerkanKadioglu <kadioglub@ece.neu.edu>
2017-10-30 14:22:54 -0400

    Other test files added, EEG Processor works fine.

--- acquisition/sigPro/eegProcessorTester.py
+++ acquisition/sigPro/eegProcessorTester.py
@@ -62,4 +62,10 @@ plt.show()
 # plt.title('Filtered Signal')
 # plt.show()
 
+# plt.figure(3)
+# plt.plot(t,x[0][:xpassband.size])
+# plt.title('Received Signal')
+# plt.show()
+
+
 raw_input()
---
+++ acquisition/sigPro/eegProcessorTester_Real Data.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from eegProcessor import sigPro
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+
+"""
+Test of filter on real data 
+"""
+
+# reading previous filter from .mat file
+Data = loadmat('sample_dat.mat')
+EEG_Data = np.transpose(Data['x'])
+fs = 256  # downsampling factor
+k=1 # downsampling factor
+
+# New filter: testing filter on real data
+y = sigPro(EEG_Data, fs = fs, k = 1, channels = 20)
+
+# Old filter: testing filter on real data
+filt = loadmat('inputFilterCoef.mat')
+groupDelay = filt['frontendFilter']['groupDelay'][0,0][0][0]
+filterNum = filt['frontendFilter']['Num'][0,0][0] # Den = 1
+# Convolution per channel
+temp = np.convolve(EEG_Data[0][:], filterNum)
+# Filter off-set compensation
+temp = temp[groupDelay-1:];
+# Downsampling
+Y = temp[::k]
+
+plt.figure(1)
+plt.plot(EEG_Data[0][0:200],'b')
+plt.plot(y[0][0:200],'r')
+plt.plot(Y[0:200],'g')
+plt.title('EEG sample data before and after filtering')
+plt.legend(('Before filtering', 'After filtering_new filter','After filtering_old filter'))
+plt.show()
