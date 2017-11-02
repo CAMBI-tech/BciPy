4466ae3343cbad1a1636b4d5f820e644622d0ea3
BerkanKadioglu <kadioglub@ece.neu.edu>
2017-11-02 08:56:29 -0400

    get_system_info.py is halfway done. needs to find location of bci and add it to PYTHONPATH

--- utils/get_system_info.py
+++ utils/get_system_info.py
@@ -1,17 +1,29 @@
-import sys, Tkinter, os, psutil
+import sys, os, psutil, pyglet
+
+from os.path import join, getsize
+for root, dirs, files in os.walk('python/Lib/email'):
+    print root, "consumes",
+    print sum(getsize(join(root, name)) for name in files),
+    print "bytes in", len(files), "non-directory files"
+    if 'CVS' in dirs:
+        dirs.remove('CVS')  # don't visit CVS directories
+
 
 
 def get_system_info():
 
-    tk = Tkinter.Tk()
+
+    # Three lines for getting screen resolution
+    platform = pyglet.window.get_platform()
+    display = platform.get_default_display()
+    screen = display.get_default_screen()
+
     mem = psutil.virtual_memory()
 
-    dict_of_attributes = {
+    return {
         'OS': sys.platform,
         'PYTHON': sys.version,
-        'RESOLUTION': str(tk.winfo_screenwidth()) + 'x' + str(tk.winfo_screenheight()),
-        'PYTHONPATH': os.path,
+        'RESOLUTION': [screen.width, screen.height],
+        'PYTHONPATH': os.environ['PYTHONPATH'].split(os.pathsep),
         'AVAILMEMORYMB': mem.available/1024./1024
     }
-
-    return dict_of_attributes
