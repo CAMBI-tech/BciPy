ad59b3cac907f31fd72d39c884e03644e93de16e
BerkanKadioglu <kadioglub@ece.neu.edu>
2017-11-02 11:26:21 -0400

    Changed screen resolution module that was used. Returning resolution as a list.

--- utils/get_system_info.py
+++ utils/get_system_info.py
@@ -1,17 +1,7 @@
 import sys, os, psutil, pyglet
 
-from os.path import join, getsize
-for root, dirs, files in os.walk('python/Lib/email'):
-    print root, "consumes",
-    print sum(getsize(join(root, name)) for name in files),
-    print "bytes in", len(files), "non-directory files"
-    if 'CVS' in dirs:
-        dirs.remove('CVS')  # don't visit CVS directories
-
-
 
 def get_system_info():
-
 
     # Three lines for getting screen resolution
     platform = pyglet.window.get_platform()
