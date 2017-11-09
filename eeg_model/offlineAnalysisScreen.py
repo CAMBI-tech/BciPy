3df2ca3952fc9ad1829696741f31e79d0d472284
Paula Gonzalez <paula@Antonios-MacBook-Pro.local>
2017-11-09 15:39:49 -0500

    Offline Analysis Screen

---
+++ eeg_model/offlineAnalysisScreen.py
@@ -0,0 +1,56 @@
+""" Offline Analysis Screen. % Generates the information figure following the offlineAnalysis.
+The figure has multiple tabs containing the average ERP plots
+
+x(ndarray[float]): C x N x k data array
+y(ndarray[int]): N x k observation (class) array
+N is number of samples k is dimensionality of features
+C is number of channels
+
+    """
+
+#import
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+
+
+
+# Load data
+fol = loadmat('dummy_dat.mat')
+y = fol['trialTargetness']
+y = np.squeeze(y)
+x = fol['trialData']
+
+#convert from V to microVolts the data
+x=x*np.power(10,6)
+
+
+
+classes = np.unique(y)
+means = [np.mean(x[:,:,np.where(y==i)],3) for i in classes]
+
+
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+
+
+count = 1
+while count < np.shape(means[1])[1]:
+    ax1.plot(means[0][:,count])
+    ax1.plot(means[0][:,count])
+    ax2.plot(means[1][:,count])
+    ax2.plot(means[1][:,count])
+    count += 1
+
+
+# Set common labels
+fig.text(0.5, 0.04, 'time Samples [n]', ha='center', va='center')
+fig.text(0.06, 0.5, '$\mu V$', ha='center', va='center', rotation='vertical')
+
+ax1.set_title('Mean distractor ERP (averaged over trials in the calibration data')
+ax2.set_title('Mean target ERP (averaged over trials in the calibration data)')
+
+
+plt.show()
+
