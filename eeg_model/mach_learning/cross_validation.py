73416789177deb2f7174403e0979af1847fd5782
Aziz <akocanaogullari@ece.neu.edu>
2017-11-09 17:10:17 -0500

    moved csv reader to relevant directory.
    updated cross validation with grid_search
    
    introduces offline_analysis_screen

--- eeg_model/mach_learning/cross_validation.py
+++ eeg_model/mach_learning/cross_validation.py
@@ -29,7 +29,52 @@ def cost_auc(model, opt_el, x, y, param):
     return -auc
 
 
-def param_nonlinear_opt(model, opt_el, x, y, init=None, op_type='cost_auc'):
+def grid_search(model, opt_el, x, y, grid=[10, 10], op_type='cost_auc'):
+    """ Description: This function performs an exhaustive grid search
+                     to estimate the hyper parameters lambda and gamma
+                     that minimize the cost of AUC.
+        Args:
+            model(pipeline): model to be iterated on
+            opt_el(int): number of the element in pipeline to be optimized
+            x(ndarray[float]): N x k data array
+            y(ndarray[int]): N x k observation (class) array
+                 N is number of samples k is dimensionality of features
+            grid(list(int)): a list of 2 numbers for grid
+            op_type(string): type of the optimization
+        Returns:
+            arg_opt(list[float]): optimized hyper parameters
+            """
+    if op_type == 'cost_auc':
+        # This specifies the different candidate values we want to try.
+        # The grid search will try all combination of these parameter values
+        # and select the set of parameters that provides the most accurate
+        # model.
+        param_cand = {  # dictionary of parameter candidates
+            'lam': np.linspace(0, 1, grid[0], endpoint=False),
+            'gam': np.linspace(0, 1, grid[1], endpoint=False),
+        }
+        best_auc = 1  # auc can't be bigger than 1
+        arg_opt = {'lam': 0, 'gam': 0}
+
+        # For every coordinate on the grid, try every combination of
+        # hyper parameters:
+        for i in range(len(param_cand['lam'])):
+            for j in range(len(param_cand['gam'])):
+                auc = cost_auc(model, opt_el, x, y, [param_cand['lam'][i],
+                                                     param_cand['gam'][j]])
+                if auc < best_auc:
+                    best_auc = auc
+                    arg_opt['lam'], arg_opt['gam'] = param_cand['lam'][i], \
+                                                     param_cand['gam'][j]
+    else:
+        # TO DO, Handle this case
+        print('Error: Operation type other than AUC cost.')
+
+    # This returns the parameter estimates with the highest scores:
+    return [arg_opt['lam'], arg_opt['gam']]
+
+
+def nonlinear_opt(model, opt_el, x, y, init=None, op_type='cost_auc'):
     """ Optimizes lambda, gamma values for given  penalty function
         Args:
             model(pipeline): model to be iterated on
@@ -39,6 +84,8 @@ def param_nonlinear_opt(model, opt_el, x, y, init=None, op_type='cost_auc'):
                  N is number of samples k is dimensionality of features
             init(list[float]): initial values for gamma and lambda
             op_type(string): type of the optimization
+        Return:
+            arg_opt(list[float]): optimized hyper parameters
             """
 
     # Get initial values
@@ -78,6 +125,9 @@ def cross_validation(x, y, model, opt_el=1, k_folds=10, split='uniform'):
             k_folds(int): number of folds
             split(string): split type,
                 'uniform': Takes the data as is
+        Return:
+            return(list[list[float],list[float],list[float]]): lists of lambda,
+                gamma and AUC values for each fold respectively.
             """
     num_samples = x.shape[1]
     fold_len = np.floor(float(num_samples) / k_folds)
@@ -107,7 +157,7 @@ def cross_validation(x, y, model, opt_el=1, k_folds=10, split='uniform'):
         y_valid = fold_y[list_valid]
 
         model.fit(x_train, y_train)
-        arg_opt = param_nonlinear_opt(model, opt_el, x_valid, y_valid)
+        arg_opt = nonlinear_opt(model, opt_el, x_valid, y_valid)
         lam.append(arg_opt[0])
         gam.append(arg_opt[1])
         model.pipeline[opt_el].regularize(arg_opt)
--- helpers/load.py
+++ helpers/load.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from Tkinter import Tk
-
+import numpy as np
+import pandas as pd
 from codecs import open as codecsopen
 from json import load as jsonload
 
@@ -28,7 +29,6 @@ def load_json_parameters(path):
 
 
 def load_experimental_data():
-
     # use python's internal gui to call file explorers and get the filename
     try:
         Tk().withdraw()  # we don't want a full GUI
@@ -42,7 +42,6 @@ def load_experimental_data():
 
 
 def load_classifier():
-
     return
 
 
@@ -65,3 +64,50 @@ def load_csv_data():
     print("Loaded EEG Data From: %s" % filename)
 
     return filename
+
+
+def read_data_csv(folder, dat_first_row=4, info_end_row=1):
+    """ Reads the data (.csv) provided by the data acquisition
+        Arg:
+            folder(str): file location for the data
+            dat_first_row(int): row with channel names
+            info_end_row(int): final row related with daq. info
+                where first row idx is '0'
+        Return:
+            raw_dat(ndarray[float]): C x N numpy array with samples
+                where C is number of channels N is number of time samples
+            channels(list[str]): channels used in DAQ
+            stamp_time(ndarray[float]): time stamps for each sample
+            type_amp(str): type of the device used for DAQ
+            fs(int): sampling frequency
+    """
+    dat_file = pd.read_csv(folder, skiprows=dat_first_row)
+
+    cols = list(dat_file.axes[1])
+    channels = cols[2:len(cols)]
+
+    temp = np.array(dat_file)
+    stamp_time = temp[:, 0]
+    raw_dat = temp[:, 1:temp.shape[1]].transpose()
+
+    dat_file_2 = pd.read_csv(folder, nrows=info_end_row)
+    type_amp = list(dat_file_2.axes[1])[1]
+    fs = np.array(dat_file_2)[0][1]
+
+    return raw_dat, stamp_time, channels, type_amp, fs
+
+
+def _test_read_data_csv():
+    # Might cause path issues please be aware!
+    location = 'rawdata.csv'
+    raw_dat, stamp_time, channels, type_amp, fs = read_data_csv(location)
+    print('Channels:{}, AMP:{}, fs:{}'.format(channels, type_amp, fs))
+
+    import matplotlib as mpl
+    mpl.use('TkAgg')
+    import matplotlib.pylab as plt
+
+    plt.plot(stamp_time, raw_dat.transpose(), '*')
+    plt.show()
+
+    return 0
