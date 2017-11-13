41195eb2095abc94933d2edaaf45eefdf95d547e
Aziz <akocanaogullari@ece.neu.edu>
2017-11-13 09:43:22 -0500

    offline analysis asks for the data folder. cross_validation is throws a warning if a fold has just 1 class. model training accepts number of folds as an input parameter.

--- eeg_model/mach_learning/train_model.py
+++ eeg_model/mach_learning/train_model.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from eeg_model.mach_learning.classifier.function_classifier \
     import RegularizedDiscriminantAnalysis
@@ -13,7 +12,7 @@ from scipy.stats import iqr
 
 
 # TODO: Horrible implementation make it modular!
-def train_pca_rda_kde_model(x, y):
+def train_pca_rda_kde_model(x, y, k_folds=10):
     """ Trains the Cw-PCA RDA KDE model given the input data and labels with
         cross validation and returns the model
         Args:
@@ -21,6 +20,7 @@ def train_pca_rda_kde_model(x, y):
             y(ndarray[int]): N x 1 observation (class) array
                 N is number of samples k is dimensionality of features
                 C is number of channels
+            k_folds(int): Number of cross validation folds
         Return:
             model(pipeline): trained likelihood model
             """
@@ -38,7 +38,7 @@ def train_pca_rda_kde_model(x, y):
     auc_init = metrics.auc(fpr, tpr)
 
     # Cross validate
-    arg_cv = cross_validation(x, y, model=model, k_folds=10)
+    arg_cv = cross_validation(x, y, model=model, k_folds=k_folds)
     auc_cv = arg_cv[2]
 
     idx_max_auc = np.where(auc_cv == np.max(auc_cv))[0][0]
