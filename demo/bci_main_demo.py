0868281f34326ccca7522434723a2a4be9801e5c
azizkocana <akocanaogullari@ece.neu.edu>
2017-11-12 23:15:17 -0500

    removed path specification

--- demo/bci_main_demo.py
+++ demo/bci_main_demo.py
@@ -1,9 +1,6 @@
 # This is a demo of the main bci system. It will run whatever task defined here
 #  with code downstream using the parameters file passed to it.
 
-import sys
-sys.path.append('C:\Users\Aziz\Desktop\GIT/bci_2')
-
 import bci_main
 from helpers.load import load_json_parameters
 
