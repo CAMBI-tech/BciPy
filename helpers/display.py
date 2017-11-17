bab6f6603b34f9242a391d6ce682bc03fed739bd
Tab Memmott <memmott@ohsu.edu>
2017-11-16 16:56:36 -0800

    update display documentation

--- helpers/display.py
+++ helpers/display.py
@@ -16,14 +16,27 @@ def init_display_window(parameters):
         http://www.psychopy.org/api/visual/window.html
     """
 
+    # Check is full_screen mode is set and get necessary values
     if parameters['full_screen']['value'] == 'true':
+
+        # get relevant info about the system
         info = get_system_info()
+
+        # set window attributes based on resolution
         window_height = info['RESOLUTION'][1]
         window_width = info['RESOLUTION'][0]
+
+        # set full screen mode to true (removes os dock, explorer etc.)
         full_screen = True
+
+    # otherwise, get user defined window attributes
     else:
+
+        # set window attributes directly from parameters file
         window_height = parameters['window_height']['value']
         window_width = parameters['window_width']['value']
+
+        # make sure full screen is set to false
         full_screen = False
 
     # Initialize PsychoPy Window for Main Display of Stimuli
@@ -38,5 +51,5 @@ def init_display_window(parameters):
         color='black', colorSpace='rgb', blendMode='avg',
         waitBlanking=True)
 
-    # Return it to caller
+    # Return display window to caller
     return display_window
