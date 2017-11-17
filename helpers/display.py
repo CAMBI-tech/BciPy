7521121397c197ef946b40eb302ad4aa4c45e69b
Tab Memmott <memmott@ohsu.edu>
2017-11-16 16:37:46 -0800

    implement full screen parameter and load information using utils.get_system_info

--- helpers/display.py
+++ helpers/display.py
@@ -2,6 +2,7 @@
 
 from __future__ import division
 from psychopy import visual
+from utils.get_system_info import get_system_info
 
 
 def init_display_window(parameters):
@@ -15,13 +16,27 @@ def init_display_window(parameters):
         http://www.psychopy.org/api/visual/window.html
     """
 
+    if parameters['full_screen']['value'] == 'true':
+        info = get_system_info()
+        window_height = info['RESOLUTION'][1]
+        window_width = info['RESOLUTION'][0]
+        full_screen = True
+    else:
+        window_height = parameters['window_height']['value']
+        window_width = parameters['window_width']['value']
+        full_screen = False
+
     # Initialize PsychoPy Window for Main Display of Stimuli
     display_window = visual.Window(
-        size=[parameters['window_width']['value'],
-              parameters['window_height']['value']],
-        fullscr=False, screen=0,
-        allowGUI=False, allowStencil=False, monitor='mainMonitor',
-        color='black', colorSpace='rgb', blendMode='avg', waitBlanking=True)
+        size=[window_width,
+              window_height],
+        screen=0,
+        allowGUI=False,
+        fullscr=full_screen,
+        allowStencil=False,
+        monitor='mainMonitor',
+        color='black', colorSpace='rgb', blendMode='avg',
+        waitBlanking=True)
 
     # Return it to caller
     return display_window
