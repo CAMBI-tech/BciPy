5eb70932e048ea1bc7f706da63b870acfce5f285
DESKTOP-HDEK6JI\csl <demir.a@husky.neu.edu>
2017-11-20 15:00:05 -0500

    inference update with filtered eeg getting reshaped

--- eeg_model/mach_learning/inference.py
+++ eeg_model/mach_learning/inference.py
@@ -1,30 +1,35 @@
 import numpy as np
 from acquisition.sig_pro.sig_pro import sig_pro
+from eeg_model.mach_learning.trial_reshaper import trial_reshaper
 
-def inference(x, y, model, alphabet):
-    """ Evaluates the log likelihood ratios given the model and input.
+def inference(x, y, model, alphabet, trigger_loc):
+    """ 
+        Evaluates the log likelihood ratios given the model and input.
         Then maps the results in the alphabet.
         Args:
-            x(ndarray[float]): raw input sequence c x N
-            y(ndarray[int]): N x 1 label array
-            model(pipeline): trained likelihood model
+            x(ndarray[float]): Input sequence to be filtered. Expected dimensions are 16xT
+            y(ndarray[int]): Label array
+            model(pipeline): Trained likelihood model
+            trigger_loc: Location of the trigger.txt file to read triggers from
             alphabet: 
         Return:
-            scores(ndarray): N x c  log-likelihood array
-                    N is number of samples c is number of classes
-            """
+            lik_r(ndarray): Log-likelihood ratio array   
+    """
+
     # This returns a sequence that is filtered and downsampled
-    dat = sig_pro(input_seq = x)
+    filtered_eeg = sig_pro(input_seq = x)
+    # Reshape the data:
+    labels = trial_reshaper(trigger_loc, filtered_eeg, fs = 256, k = 2)
     # This evaluates the loglikelihood probabilities for p(e|l=1) and p(e|l=0)
-    scores = np.exp(model.transform(dat))
+    scores = np.exp(model.transform(labels))
     # This evaluates the log likelihood ratios:
     scores = -scores[:,1] / scores[:,0]
     # This maps the log likelihood ratios to alphabet:
     # If the letter in alphabet does not exist in y, it takes 1
     # the likelihood ratio.
     lik_r = np.ones(len(alphabet))
+    
     for i in range(len(alphabet)):
         for j in range(len(y)):
             if alphabet(i) == y(j):
                 lik_r[i] = scores[j]
-    return lik_r
