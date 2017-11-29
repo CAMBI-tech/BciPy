8a71f5375a5409861b161932e831d906182d297f
Tab Memmott <memmott@ohsu.edu>
2017-11-29 10:00:46 -0800

    make alphabet function (to be used later to change used letters / icons etc), and move fake decision maker to helpers

---
+++ helpers/bci_task_related.py
@@ -0,0 +1,37 @@
+
+
+def fake_copy_phrase_decision(copy_phrase, target_letter, text_task):
+    if text_task is '*':
+        length_of_spelled_letters = 0
+    else:
+        length_of_spelled_letters = len(text_task)
+
+    length_of_phrase = len(copy_phrase)
+
+    if length_of_spelled_letters is 0:
+        text_task = copy_phrase[length_of_spelled_letters]
+    else:
+        text_task += copy_phrase[length_of_spelled_letters]
+
+    length_of_spelled_letters += 1
+
+    # If there is still text to be spelled, update the text_task
+    # and target letter
+    if length_of_spelled_letters < length_of_phrase:
+        next_target_letter = copy_phrase[length_of_spelled_letters]
+
+        run = True
+
+    # else, end the run
+    else:
+        run = False
+        next_target_letter = None
+        text_task = None
+
+    return next_target_letter, text_task, run
+
+
+def alphabet():
+
+    return ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'R', 'S', 'T', 'U', 'V', 'Y', 'Z', '<', '_']
