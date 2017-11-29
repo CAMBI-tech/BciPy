b39b43566f80a3a4b69ea9f45b8396181863f50d
Tab Memmott <memmott@ohsu.edu>
2017-11-29 09:48:34 -0800

    init test triggers

---
+++ helpers/tests/test_triggers.py
@@ -0,0 +1,14 @@
+import errno
+import os
+import shutil
+import sys
+import unittest
+
+from helpers.trigger_helpers import *
+
+
+class TestTriggers(unittest.TestCase):
+    """This is Test Case for Triggers."""
+
+    def test_triggers_for_calibration(self):
+        return
