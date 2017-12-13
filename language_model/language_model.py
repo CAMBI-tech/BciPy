50ef436d6c1c9ba5de3b98b43c0dbd155a9e6387
Tab Memmott <memmott@ohsu.edu>
2017-12-12 16:45:58 -0800

    get uppercase alphabet for comparisions

--- language_model/language_model.py
+++ language_model/language_model.py
@@ -9,35 +9,7 @@ import logging
 from errors import ConnectionErr, StatusCodeError, DockerDownError
 from helpers.bci_task_related import alphabet
 
-ALPHABET = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-    '<',
-    '#']
+ALPHABET = alphabet()
 
 
 class LangModel:
