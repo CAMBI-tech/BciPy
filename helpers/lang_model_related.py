936b7f6051310b356eba1d8c5fb1f362d00ad9f9
Tab Memmott <memmott@ohsu.edu>
2017-12-14 10:55:36 -0800

    final documentation for lm integration

--- helpers/lang_model_related.py
+++ helpers/lang_model_related.py
@@ -3,8 +3,30 @@ import os
 
 
 def init_language_model(parameters):
+    """
+    Init Language Model.
+
+    Function to Initialize remote language model and get an instance of
+        LangModel wrapper. Assumes a docker image is already loaded.
+
+    See language_model/demo/ for more information of how it works.
+
+    Parameters
+    ----------
+        parameters : dict
+            configuration details and path locations
+
+    Returns
+    -------
+        lmodel: instance
+            instance of lmodel wrapper with connections to docker server
+    """
+
     try:
+        # get the absolute path for fst from parameters
         abs_path_fst = os.path.abspath(parameters['path_to_fst']['value'])
+
+        # Try getting an instance of LM
         lmodel = LangModel(
             abs_path_fst,
             host=str(parameters['lang_model_server_host']['value']),
