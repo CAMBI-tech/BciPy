0fb94a846ccfd8f5d4b9339e97c6492caf912096
BerkanKadioglu <kadioglub@ece.neu.edu>
2018-01-19 17:04:54 -0500

    shuffle begins

--- display/shuffle/shuffle_start.py
+++ display/shuffle/shuffle_start.py
@@ -1,40 +1,49 @@
 from psychopy import visual, core, event
 from utils.get_system_info import get_system_info
 
-
 system_info = get_system_info()
-'''
-system_info is a dictionary with labels:
-        'OS': sys.platform,
-        'PYTHON': sys.version,
-        'RESOLUTION': [screen.width, screen.height],
-        'PYTHONPATH': os.environ['PYTHONPATH'].split(os.pathsep),
-        'AVAILMEMORYMB': mem.available/1024./1024
-'''
 
+# Can you use resolution to find smart positions for stimulus. 26/1920
 
 shuffle_window = visual.Window(system_info['RESOLUTION'],
                                monitor='testMonitor', units='deg')
 
-# create some stimuli
+box1 = visual.Rect(win = shuffle_window, width=12, height=8, pos = (-21, -10.8),
+                   lineWidth=4, lineColorSpace='rgb255', lineColor=(255, 255, 0))
 
-grating = visual.GratingStim(win=shuffle_window, mask="circle", size=3, pos=[-4,0], sf=3)
-fixation = visual.GratingStim(win=shuffle_window, size=0.5, pos=[0,0], sf=0, color='red')
+box2 = visual.Rect(win = shuffle_window, width=12, height=8, pos = (0, -10.8),
+                   lineWidth=4, lineColorSpace='rgb255', lineColor=(255, 80, 80))
+
+box3 = visual.Rect(win = shuffle_window, width=12, height=8, pos = (21, -10.8),
+                   lineWidth=4, lineColorSpace='rgb255', lineColor=(255, 0, 255))
+
+box4 = visual.Rect(win = shuffle_window, width=12, height=8, pos = (21, 10.8),
+                   lineWidth=4, lineColorSpace='rgb255', lineColor=(51, 102, 255))
+
+box5 = visual.Rect(win = shuffle_window, width=12, height=8, pos = (0, 10.8),
+                   lineWidth=4, lineColorSpace='rgb255', lineColor=(51, 204, 204))
+
+box6 = visual.Rect(win = shuffle_window, width=12, height=8, pos = (-21, 10.8),
+                   lineWidth=4, lineColorSpace='rgb255', lineColor=(51, 204, 51))
 
 
 while True:
-    event.clearEvents()
-    grating.setPhase(0.05, '+')  # advance phase by 0.05 of a cycle
-    grating.draw()
-    fixation.draw()
+    box1.draw()
+    box2.draw()
+    box3.draw()
+    box4.draw()
+    box5.draw()
+    box6.draw()
+
     shuffle_window.update()
+
     num_keys = len(event.getKeys())
     if num_keys > 0:
         print num_keys
         break
     event.clearEvents()
 
-core.wait(2.0)
+core.wait(.5)
 
 shuffle_window.close()
 core.quit()
