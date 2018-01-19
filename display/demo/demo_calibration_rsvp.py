45fa102c9cfdced7c7e98006a52674d929c70a22
BerkanKadioglu <kadioglub@ece.neu.edu>
2018-01-19 16:09:43 -0500

    Hopefully fixes conflict

--- display/demo/demo_calibration_rsvp.py
+++ display/demo/demo_calibration_rsvp.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-
 from __future__ import division
 from psychopy import visual, core
+from display.rsvp.rsvp_disp_modes import CalibrationTask
 
-from display.rsvp_disp_modes import CalibrationTask
 
 # Initialize Stimulus Parameters
 # Task Bar
