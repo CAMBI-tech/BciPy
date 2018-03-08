916a8ef541d58cbb8f64a5d3ec893118ab451838
TMemmott <memmott@ohsu.edu>
2018-03-08 11:11:11 -0800

    bci_main_demo protection

--- demo/bci_main_demo.py
+++ demo/bci_main_demo.py
@@ -1,25 +1,28 @@
 # This is a demo of the main bci system. It will run the task defined here
 #  using the parameters file passed to it.
 
-import bci_main
-from helpers.load import load_json_parameters
+def main():
+	import bci_main
+	from helpers.load import load_json_parameters
 
-# Load a parameters file
-parameters = load_json_parameters('parameters/parameters.json')
+	# Load a parameters file
+	parameters = load_json_parameters('parameters/parameters.json')
 
-# Mode: ex. RSVP, Shuffle, Matrix
-test_mode = 'RSVP'
+	# Mode: ex. RSVP, Shuffle, Matrix
+	test_mode = 'RSVP'
 
-# Test Type: ex. RSVP Calibration = 1, Copy Phrase = 2
-test_type = 1
+	# Test Type: ex. RSVP Calibration = 1, Copy Phrase = 2
+	test_type = 1
 
-# Define a user
-user = 'bci_main_demo_user'
+	# Define a user
+	user = 'bci_main_demo_user'
 
-# Try and intialize with bci main
-try:
-    bci_main.bci_main(parameters, user, test_type, test_mode)
-except Exception as e:
-    print("BCI MAIN Fail. Exiting. Error: \n")
-    print(e)
+	# Try and intialize with bci main
+	try:
+	    bci_main.bci_main(parameters, user, test_type, test_mode)
+	except Exception as e:
+	    print("BCI MAIN Fail. Exiting. Error: \n")
+	    print(e)
 
+if __name__ == "__main__":
+    main()
