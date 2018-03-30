0aaf07daa2bcacd6f72c05a4fb66e5e14fc60e31
BerkanKadioglu <kadioglub@ece.neu.edu>
2018-03-30 09:32:33 -0400

    removed pdf plotter

--- signal_model/offline_analysis_m.py
+++ signal_model/offline_analysis_m.py
@@ -4,18 +4,18 @@ sys.path.append('.')
 sys.path.append('..')
 sys.path.append('../..')
 
-from helpers.load import read_data_csv, load_experimental_data
+from helpers.load import load_experimental_data, read_data_csv
 from signal_processing.sig_pro import sig_pro
-from signal_model.mach_learning.train_model import train_m_estimator_pipeline
-from signal_model.mach_learning.trial_reshaper import trial_reshaper
-# from helpers.data_viz import generate_offline_analysis_screen
 from helpers.triggers import trigger_decoder
+from signal_model.mach_learning.trial_reshaper import trial_reshaper
+from signal_model.mach_learning.train_model import train_m_estimator_pipeline
+
 import numpy as np
 import sklearn as sk
 import random
 import pickle
 from time import time
-# import os
+
 
 
 def noise_data(dat, amplitude, length, p, channel_map):
@@ -50,6 +50,7 @@ def noise_data(dat, amplitude, length, p, channel_map):
     activations[d-length:] = 0
 
     artifact_start_indices = np.where(activations)[0]
+
     noise_type = 'artifact'
 
     if noise_type == 'gaussian':
@@ -74,62 +75,6 @@ def noise_data(dat, amplitude, length, p, channel_map):
                 amplitude*np.squeeze(artifacts[:, random.sample(list_indexes, 1), 0:length]*np.hamming(length))
 
     return dat
-
-
-def noise_data_sample_based(x, y, amplitude=1, ratio=None):
-
-    C, N, d = x.shape
-    length = int(d/3)
-    noise_type = 'artifact'
-
-    num_p = np.float(np.sum(y)) * np.float(ratio) / 100.  # num of positive samples to corrupt
-    num_n = (y.size - np.float(np.sum(y))) * np.float(ratio) / 100.
-
-    index_p = np.where(y)[0]  # indexes of positive samples
-    index_n = np.where(np.abs(y - 1))[0]
-
-    selected_p = random.sample(index_p, int(num_p))  # indexes of positive samples to be corrupted
-    selected_n = random.sample(index_n, int(num_n))
-
-    if noise_type == 'gaussian':
-        mean = amplitude*np.ones(length)
-
-        for p_i in selected_p:
-            cov = 100 * sk.datasets.make_spd_matrix(length, random_state=15)
-            start_of_artifact = np.random.randint(low=0, high=d-length, size=1)[0]
-            # start_of_artifact = 0
-            for c in range(C):
-                x[c, p_i, start_of_artifact:start_of_artifact+length] += \
-                    np.random.multivariate_normal(mean=mean, cov=cov)*np.hamming(length)
-
-        for n_i in selected_n:
-            cov = 100 * sk.datasets.make_spd_matrix(length, random_state=15)
-            start_of_artifact = np.random.randint(low=0, high=d-length, size=1)[0]
-            # start_of_artifact = 0
-            for c in range(C):
-                x[c, n_i, start_of_artifact:start_of_artifact+length] += \
-                    np.random.multivariate_normal(mean=mean, cov=cov)*np.hamming(length)
-
-    elif noise_type == 'artifact':
-
-        with open('C:/Users/Berkan/Desktop/data/jaw_muscle.pkl') as f:
-            artifacts = pickle.load(f) # CxN'xd
-        N_prime = artifacts.shape[1]
-        list_indexes = range(N_prime)
-
-        for p_i in selected_p:
-            start_of_artifact = np.random.randint(low=0, high=d-length, size=1)[0]
-            # start_of_artifact = 0
-            x[:, p_i, start_of_artifact:start_of_artifact+length] += \
-                np.squeeze(artifacts[:, random.sample(list_indexes, 1), 0:length]*np.hamming(length))
-
-        for n_i in selected_n:
-            start_of_artifact = np.random.randint(low=0, high=d-length, size=1)[0]
-            # start_of_artifact = 0
-            x[:, n_i, start_of_artifact:start_of_artifact+length] += \
-                np.squeeze(artifacts[:, random.sample(list_indexes, 1), 0:length] * np.hamming(length))
-
-    return x
 
 
 def offline_analysis_m(data_folder=None, add_artifacts = 0):
@@ -170,17 +115,6 @@ def offline_analysis_m(data_folder=None, add_artifacts = 0):
 
     t1 = time() - t1
     print 'Completed in {} mins'.format(t1/60.)
-
-    # if not os.path.exists(data_folder+'/mpca_{}'.format(add_artifacts)):
-    #     os.makedirs(data_folder+'/mpca_{}'.format(add_artifacts))
-    #
-    # print('Saving offline analysis plots!')
-    # generate_offline_analysis_screen(x, y, model, data_folder+'/mpca_{}'.format(add_artifacts))
-    #
-    # print('Saving the model!')
-    # with open(data_folder + '/mpca_{}/mpca_model_duration_{}_auccv_{}.pkl'.format(add_artifacts, t1, auc_cv), 'wb') as output:
-    #     pickle.dump(model, output)
-    # return model
 
 
 if __name__ == '__main__':
