31baec546bd616fc690f244217127cc914b11c11
Matthew Lawhead <lawhead@ohsu.edu>
2018-04-10 16:01:34 -0700

    Removed unused code

--- acquisition/tests/buffer_server_test.py
+++ acquisition/tests/buffer_server_test.py
@@ -8,14 +8,6 @@ from record import Record
 import numpy as np
 
 
-class ClassName(object):
-    """docstring for ClassName"""
-
-    def __init__(self, arg):
-        super(ClassName, self).__init__()
-        self.arg = arg
-
-
 class TestBufferServer(unittest.TestCase):
     """Tests for the buffer_server module."""
 
