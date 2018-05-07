7695e4ebd8fb1ed01a406178dc1da15b28223103
TMemmott <memmott@ohsu.edu>
2018-05-07 11:58:11 -0700

    Refactored dsi_device_test to eliminate creation of multiple servers, which sometimes caused memory errors in tests

--- acquisition/protocols/tests/dsi_device_test.py
+++ acquisition/protocols/tests/dsi_device_test.py
@@ -1,12 +1,14 @@
 
 import pytest
-from datastream import generator, server
+from datastream import server
 from protocols.dsi.dsi_device import dsi
 from protocols.dsi.dsi_device import DsiDevice
 from protocols.dsi.dsi_protocol import DsiProtocol
-from util import StoppableThread
 
 import unittest
+
+HOST = '127.0.0.1'
+DEFAULT_PORT = 9000
 
 
 class TestDsiDevice(unittest.TestCase):
@@ -14,29 +16,29 @@ class TestDsiDevice(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TestDsiDevice, self).__init__(*args, **kwargs)
-        self.host = '127.0.0.1'
-        self.port = 9000
+        self.host = HOST
 
     def connection_params(self):
         return {'host': self.host, 'port': self.port}
 
+    @classmethod
+    def setUpClass(cls):
+        cls.server, cls.port = server.start_socket_server(
+            DsiProtocol(), HOST, DEFAULT_PORT)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.stop()
+
     def setUp(self):
         """Run before each test."""
 
-        protocol = DsiProtocol()
-        channel_count = len(protocol.channels)
-        self.server, self.port = server.start_socket_server(
-            protocol, self.host, self.port)
-
-    def tearDown(self):
-        """Run after each test."""
-        self.server.stop()
-        self.server = None
+        self.port = type(self).port
 
     def test_mismatched_channels(self):
         """An exception should be thrown if parameters do not match data read
         from the device."""
-
+        print("Running test_mismatched_channels")
         device = DsiDevice(connection_params=self.connection_params(),
                            channels=['ch1', 'ch2'])
         self.assertEqual(len(device.channels), 2)
@@ -48,7 +50,7 @@ class TestDsiDevice(unittest.TestCase):
     def test_mismatched_frequency(self):
         """An exception should be thrown if parameters do not match data read
         from the device."""
-
+        print("Running test_mismatched_frequency")
         device = DsiDevice(connection_params=self.connection_params(), fs=100)
         self.assertEqual(device.fs, 100)
         device.connect()
@@ -59,7 +61,7 @@ class TestDsiDevice(unittest.TestCase):
     def test_update_params_on_init(self):
         """Channel and sample rate properties should be updated by reading
         initialization data from the server."""
-
+        print("Running test_update_params_on_init")
         device = DsiDevice(
             connection_params=self.connection_params(), channels=[])
         self.assertEqual(device.fs, dsi.DEFAULT_FS)
@@ -74,6 +76,7 @@ class TestDsiDevice(unittest.TestCase):
     def test_read_data(self):
         """Should produce a valid sensor_data record."""
 
+        print("Running test_read_data")
         device = DsiDevice(connection_params=self.connection_params())
 
         device.connect()
