a60c8e28bbaaa9e56bcf2de36650427a1e53c4e8
Shiran Dudy <dudy@ohsu.edu>
2018-05-07 20:11:27 -0700

    add tests in a separate file

--- language_model/tests/test_language_model.py
+++ language_model/tests/test_language_model.py
@@ -1,27 +1,27 @@
 import os
 import sys
-sys.path.insert(0, ".")
+sys.path.insert(0, "./language_model")
 import unittest
 from language_model import LangModel 
 from errors import StatusCodeError
  
 class TestPreLM(unittest.TestCase):
 
-#   def test_incorrect_class_variables(self):
-#      """
-#      confirm an assertion error as
-#      the provided fst file is invalid
-#      """
-#      abs_path_fst = os.path.abspath("fst/brown_closure.n5.kn.fst")        
-#      # local fst
-#      localfst = abs_path_fst
-#      # init LMWrapper
-#      with self.assertRaises(AssertionError):
-#          lmodel = LangModel(
-#              localfst,
-#              host='127.0.0.1',
-#              port='5000',
-#              logfile="lmwrap.log")
+   def test_incorrect_class_variables(self):
+      """
+      confirm an assertion error as
+      the provided fst file is invalid
+      """
+      abs_path_fst = os.path.abspath("fst/brown_closure.n5.kn.fst")        
+      # local fst
+      localfst = abs_path_fst
+      # init LMWrapper
+      with self.assertRaises(AssertionError):
+          lmodel = LangModel(
+              localfst,
+              host='127.0.0.1',
+              port='5000',
+              logfile="lmwrap.log")
 
    def test_incorrect_input(self):
       """
@@ -40,37 +40,37 @@ class TestPreLM(unittest.TestCase):
           logfile="lmwrap.log")
       lmodel.init()
       # try to get priors
-      with self.assertRaises(StatusCodeError):
+      with self.assertRaises(StatusCodeError) as er:
           lmodel.state_update(['3'])
-      with self.assertRaises(StatusCodeError):
-          lmodel.state_update(['e'])
+     # with self.assertRaises(StatusCodeError):
+     #     lmodel.state_update(['e'])
 
-#   def test_correct_process(self):
-#      """
-#      confirm the process provides the
-#      expected output given a correct
-#      input
-#      """
-#      abs_path_fst = os.path.abspath("./language_model/fst/brown_closure.n5.kn.fst")        
-#      # local fst
-#      localfst = abs_path_fst
-#      # init LMWrapper
-#      lmodel = LangModel(
-#          localfst,
-#          host='127.0.0.1',
-#          port='5000',
-#          logfile="lmwrap.log")
-#      # init LM
-#      lmodel.init()
-#      # get priors
-#      priors = lmodel.state_update(['T'])
-#      # display priors
-#      assert lmodel.decision == 'T'
-#      priors = lmodel.state_update(['H'])
-#      assert lmodel.decision == 'H'
-#      lmodel.reset()
-#      priors = lmodel.state_update(list('THE'))
-#      assert lmodel.decision == 'E'
+   def test_correct_process(self):
+      """
+      confirm the process provides the
+      expected output given a correct
+      input
+      """
+      abs_path_fst = os.path.abspath("./language_model/fst/brown_closure.n5.kn.fst")        
+      # local fst
+      localfst = abs_path_fst
+      # init LMWrapper
+      lmodel = LangModel(
+          localfst,
+          host='127.0.0.1',
+          port='5000',
+          logfile="lmwrap.log")
+      # init LM
+      lmodel.init()
+      # get priors
+      priors = lmodel.state_update(['T'])
+      # display priors
+      assert lmodel.decision == 'T'
+      priors = lmodel.state_update(['H'])
+      assert lmodel.decision == 'H'
+      lmodel.reset()
+      priors = lmodel.state_update(list('THE'))
+      assert lmodel.decision == 'E'
 
 if __name__ == '__main__':
     unittest.main()
