26e8ef4f215c4a8e4d911122badf6951c628b6c1
Aziz Kocanaogullari <kocanaogullari@ad.merl.com>
2018-08-07 11:48:03 -0400

    add: demo file for model validation that uses existing model and independent data

---
+++ bcipy/signal_model/mach_learning/demo/valid_model.py
@@ -0,0 +1,82 @@
+from bcipy.helpers.load import read_data_csv, load_experimental_data, \
+    load_json_parameters
+from bcipy.signal_processing.sig_pro import sig_pro
+from bcipy.helpers.bci_task_related import trial_reshaper
+from bcipy.helpers.triggers import trigger_decoder
+from bcipy.helpers.acquisition_related import analysis_channels
+import sklearn as sc
+
+
+def model_validation(data_folder=None, parameters={}):
+    """ Gets calibration data and trains the model in an offline fashion.
+        pickle dumps the model into a .pkl folder
+        Args:
+            data_folder(str): folder of the data
+                save all information and load all from this folder
+
+        Duty cycle
+        - reads data and information from a .csv calibration file
+        - reads trigger information from a .txt trigger file
+        - filters data
+        - reshapes and labels the data for the training procedure
+        - fits the model to the data
+            - uses cross validation to select parameters
+            - based on the parameters, trains system using all the data
+        - pickle dumps model into .pkl file
+        - generates and saves offline analysis screen
+    """
+
+    if not data_folder:
+        data_folder = load_experimental_data()
+
+    mode = 'calibration'
+
+    raw_dat, stamp_time, channels, type_amp, fs = read_data_csv(
+        data_folder + '/' + parameters.get('raw_data_name', 'raw_data.csv'))
+
+    print(f'Channels read from csv: {channels}')
+    print(f'Device type: {type_amp}')
+
+    downsample_rate = parameters.get('down_sampling_rate', 2)
+    filtered_data = sig_pro(raw_dat, fs=fs, k=downsample_rate)
+
+    # Process triggers.txt
+    triggers_file = parameters.get('triggers_file_name', 'triggers.txt')
+    _, t_t_i, t_i, offset = trigger_decoder(
+        mode=mode,
+        trigger_loc=f"{data_folder}/{triggers_file}")
+
+    # Channel map can be checked from raw_data.csv file.
+    # read_data_csv already removes the timespamp column.
+    channel_map = analysis_channels(channels, type_amp)
+
+    x, y, num_seq, _ = trial_reshaper(t_t_i, t_i, filtered_data,
+                                      mode=mode, fs=fs, k=downsample_rate,
+                                      offset=offset,
+                                      channel_map=channel_map)
+
+    from bcipy.helpers.load import load_classifier
+    model = load_classifier()
+
+    model.transform(x)
+    tmp = model.line_el[-2]
+    fpr, tpr, thresholds = sc.metrics.roc_curve(y, tmp, pos_label=1)
+    auc = sc.metrics.auc(fpr, tpr)
+    print("AUC:{}".format(auc))
+
+    return 0
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--data_folder', default=None)
+    parser.add_argument('-p', '--parameters_file',
+                        default='D:/BCIpy/BciPy/bcipy/parameters/parameters.json')
+    args = parser.parse_args()
+
+    print(f'Loading params from {args.parameters_file}')
+    parameters = load_json_parameters(args.parameters_file,
+                                      value_cast=True)
+    model_validation(args.data_folder, parameters)
