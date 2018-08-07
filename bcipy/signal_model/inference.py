fix: removed normalization

bcipy/signal_model/inference.py
@@ -19,7 +19,7 @@ def inference(x, targets, model, alphabet):
     # Evaluates the likelihood probabilities for p(e|l=1) and p(e|l=0)
     scores = np.exp(model.transform(x))
     # Evaluates the likelihood ratios
-    scores = scores[:, 1] / (scores[:, 0] + np.power(.1, 10))
+    scores = scores[:, 1] / (scores[:, 0] + np.power(.1, 10)) + np.power(.1, 10)
     # print("These values should be between 0 and 1: ", scores)
 
     # This maps the likelihood distribution over the alphabet
@@ -28,7 +28,5 @@ def inference(x, targets, model, alphabet):
     lik_r = np.ones(len(alphabet))
     for idx in range(len(scores)):
         lik_r[alphabet.index(targets[idx])] *= scores[idx]
-
-    lik_r /= np.sum(lik_r)
 
     return lik_r