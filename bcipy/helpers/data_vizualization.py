modify generate offline analysis screen to give correct units; add some mock data

bcipy/helpers/data_vizualization.py
@@ -1,10 +1,18 @@
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 from bcipy.helpers.load import load_csv_data, read_data_csv
 
 
-def generate_offline_analysis_screen(x, y, model, folder):
+def generate_offline_analysis_screen(
+        x,
+        y,
+        model=None,
+        folder=None,
+        plot_lik_dens=True,
+        save_figure=True,
+        down_sample_rate=2,
+        fs=300,
+        plot_x_ticks=8) -> None:
     """ Offline Analysis Screen.
     Generates the information figure following the offlineAnalysis.
     The figure has multiple tabs containing the average ERP plots
@@ -17,11 +25,10 @@ def generate_offline_analysis_screen(x, y, model, folder):
         model(): trained model for data
         folder(str): Folder of the data
 
-        """
-
+    """
     classes = np.unique(y)
-    means = [np.squeeze(np.mean(x[:, np.where(y == i), :], 2))
-             for i in classes]
+
+    means = [np.squeeze(np.mean(x[:, np.where(y == i), :], 2)) for i in classes]
 
     fig = plt.figure(figsize=(20, 10))
     ax1 = fig.add_subplot(211)
@@ -33,36 +40,49 @@ def generate_offline_analysis_screen(x, y, model, folder):
         ax2.plot(means[1][count, :])
         count += 1
 
+    # data points
+    data_length = len(means[0][1, :])
+
+    labels = [(x * down_sample_rate / fs) * 1000 for x in range(data_length)
+              if (x% round(data_length/plot_x_ticks)) == 0]
+
+    labels.insert(0, 0)
+
+    ax1.set_xticklabels(labels)
+    ax2.set_xticklabels(labels)
+
     # Set common labels
-    fig.text(0.5, 0.04, 'time Samples [n]', ha='center', va='center')
+    fig.text(0.5, 0.04, 'Time (Seconds)', ha='center', va='center')
     fig.text(0.06, 0.5, '$\mu V$', ha='center', va='center',
              rotation='vertical')
 
-    ax1.set_title(
-        'Mean distractor ERP (averaged over trials in the calibration data)')
-    ax2.set_title(
-        'Mean target ERP (averaged over trials in the calibration data)')
+    ax1.set_title('Mean non-target ERP')
+    ax2.set_title('Mean target ERP')
 
-    fig.savefig(folder + "\mean_erp.pdf", bbox_inches='tight', format='pdf')
+    if save_figure:
+        fig.savefig(f'{folder}\mean_erp.pdf', bbox_inches='tight', format='pdf')
 
-    fig, ax = plt.subplots()
-    x_plot = np.linspace(np.min(model.line_el[-1]), np.max(model.line_el[-1]),
-                         1000)[:, np.newaxis]
-    ax.plot(model.line_el[2][y == 0], -0.005 - 0.01 * np.random.random(
-        model.line_el[2][y == 0].shape[0]), 'ro', label='class(-)')
-    ax.plot(model.line_el[2][y == 1], -0.005 - 0.01 * np.random.random(
-        model.line_el[2][y == 1].shape[0]), 'go', label='class(+)')
-    for idx in range(len(model.pipeline[2].list_den_est)):
-        log_dens = model.pipeline[2].list_den_est[idx].score_samples(x_plot)
-        ax.plot(x_plot[:, 0], np.exp(log_dens),
-                'r-' * (idx == 0) + 'g--' * (idx == 1), linewidth=2.0)
 
-    ax.legend(loc='upper right')
-    plt.title('Likelihoods Given the Labels')
-    plt.ylabel('p(e|l)')
-    plt.xlabel('scores')
-    fig.savefig(folder + "\lik_dens.pdf", bbox_inches='tight', format='pdf')
-    return
+    if plot_lik_dens:
+        fig, ax = plt.subplots()
+        x_plot = np.linspace(np.min(model.line_el[-1]), np.max(model.line_el[-1]),
+                             1000)[:, np.newaxis]
+        ax.plot(model.line_el[2][y == 0], -0.005 - 0.01 * np.random.random(
+            model.line_el[2][y == 0].shape[0]), 'ro', label='class(-)')
+        ax.plot(model.line_el[2][y == 1], -0.005 - 0.01 * np.random.random(
+            model.line_el[2][y == 1].shape[0]), 'go', label='class(+)')
+        for idx in range(len(model.pipeline[2].list_den_est)):
+            log_dens = model.pipeline[2].list_den_est[idx].score_samples(x_plot)
+            ax.plot(x_plot[:, 0], np.exp(log_dens),
+                    'r-' * (idx == 0) + 'g--' * (idx == 1), linewidth=2.0)
+
+        ax.legend(loc='upper right')
+        plt.title('Likelihoods Given the Labels')
+        plt.ylabel('p(e|l)')
+        plt.xlabel('scores')
+
+        if save_figure:
+            fig.savefig(f'{folder}\lik_dens.pdf', bbox_inches='tight', format='pdf')
 
 
 def visualize_csv_eeg_triggers(trigger_col=None):
@@ -99,3 +119,13 @@ def visualize_csv_eeg_triggers(trigger_col=None):
     # Show us the figure! Depending on your OS / IDE this may not close when
     #  The window is closed, see the message above
     plt.show()
+
+
+if __name__ == '__main__':
+    import pickle
+
+    # load some x, y data from test files
+    x = pickle.load(open('bcipy/helpers/tests/resources/mock_x_generate_erp.pkl', 'rb'))
+    y = pickle.load(open('bcipy/helpers/tests/resources/mock_y_generate_erp.pkl', 'rb'))
+
+    generate_offline_analysis_screen(x, y, folder='bcipy', plot_lik_dens=False, save_figure=True)