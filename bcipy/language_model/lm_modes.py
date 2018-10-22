73a593516acf0cb49614f78c15a9af9512486c23
Matthew Lawhead <lawhead@ohsu.edu>
2018-10-22 11:08:39 -0700

    Made language model fst file path relative and os agnostic

--- bcipy/language_model/lm_modes.py
+++ bcipy/language_model/lm_modes.py
@@ -1,17 +1,21 @@
+import os
+from pathlib import Path
+
+
 class lmtype:
     def __init__(self, lmtype):
 
-       self.type = lmtype
-       self.host = "127.0.0.1"
-       self.dport = "5000"
+        self.type = lmtype
+        self.host = "127.0.0.1"
+        self.dport = "5000"
 
-       if lmtype == 'oclm':
-           self.port = "6000"
-           self.image = "oclmimage:version2.0"
-           self.nbest = "1"
-           
-       elif lmtype == 'prelm':
-           self.port = "5000"
-           self.image = "lmimage:version2.0"
-           self.localfst = "/Users/dudy/CSLU/bci/BciPy/bcipy/language_model/fst/brown_closure.n5.kn.fst"
+        if lmtype == 'oclm':
+            self.port = "6000"
+            self.image = "oclmimage:version2.0"
+            self.nbest = "1"
 
+        elif lmtype == 'prelm':
+            self.port = "5000"
+            self.image = "lmimage:version2.0"
+            self.localfst = str(Path(os.path.dirname(os.path.realpath(
+                __file__))) / "fst" / "brown_closure.n5.kn.fst")
