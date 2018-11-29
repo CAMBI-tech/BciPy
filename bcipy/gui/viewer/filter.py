33b55efb667c70878639ddcb4362db6e39fe8c19
Matthew Lawhead <lawhead@ohsu.edu>
2018-11-29 15:56:45 -0800

    Factored out viewer filters

---
+++ bcipy/gui/viewer/filter.py
@@ -0,0 +1,42 @@
+"""Defines filter functions that can be applied to a viewer stream."""
+import numpy as np
+from bcipy.signal.processing.sig_pro import sig_pro
+
+
+def downsample_filter(factor: int, fs: int):
+    """Returns a downsample filter with the given factor.
+    Parameters:
+    -----------
+        factor - downsample factor
+        fs - samples per second
+    Returns:
+    --------
+        a function that downsamples the provided data.
+    """
+
+    def fn(data):
+        """Data should be an np array with a row (np array: float) for each
+        channel."""
+        # take every nth element of each row
+        return data[:, ::factor]
+        # return np.array(data)[::factor]
+    return fn
+
+
+def sig_pro_filter(factor, fs):
+    """Data filter that calls the sig_pro filter.
+    Parameters:
+    -----------
+        factor - downsample factor
+        fs - samples per second
+    Returns:
+    --------
+        a function that filters the provided data using sig_pro.
+    """
+    def fn(data):
+        """Data should be an np array with a row (np array: float) for each
+        channel."""
+        expected_len = len(data[0, ::factor])
+        filtered = sig_pro(data, k=factor)
+        return filtered[:, 0:expected_len]
+    return fn
