3487740fe79bb829cbb1b5728d91d261f459f77c
Tab Memmott <tab.memmott@gmail.com>
2019-01-21 13:17:50 -0800

    clean up PSD code. add documentation.

--- bcipy/signal/processing/decomposition/psd.py
+++ bcipy/signal/processing/decomposition/psd.py
@@ -7,39 +7,69 @@ from scipy.signal import welch
 from scipy.integrate import simps
 from mne.time_frequency import psd_array_multitaper
 
+from typing import Tuple
+
 
 class PSD_TYPE(Enum):
+    """Power Spectal Density Type.
+
+    Enum used to specify the type of power spectral approximation
+        to use. They each have trade-offs in terms of computational
+        cost and resolution.
+    """
     WELCH = 'Welch'
     MULTITAPER = 'MutliTaper'
 
 
 def power_spectral_density(
         data: np.ndarray,
-        band: tuple,
-        fs: float=100,
-        window_length: float=4,
+        band: Tuple[int, int],
+        sampling_rate: float=100.0,
+        window_length: float=4.0,
         plot: bool=False,
         method: PSD_TYPE=PSD_TYPE.MULTITAPER,
         relative=False):
     """Power spectral density:
 
     Many thanks to: https://raphaelvallat.github.io/bandpower.html
-    """
 
+    Parameters
+    ----------
+        data: Numpy Array.
+            Time series data in the form of a numpy ndarray used to estimate the
+                power spectral density.
+        band: tuple.
+            frequency band psd to export. Note this must be within the Nyquist frequency
+            for your data. Approximated as sampling rate / 2.
+        sampling_rate: float
+            Sampling rate of the data in # of samples per second.
+        window_length: float
+            Length in seconds of data window.
+        plot: boolean.
+            Whether of not to plot the PSD estimated. Helpful for debugging and exploration.
+        method: PSD_TYPE
+            Type of PSD estimation method to use during calculation.
+        relative: boolean
+            Whether or not to express the power in a frequency band as a percentage of the
+            total power of the signal.
+
+    """
     band = np.asarray(band)
     low, high = band
 
     # Compute the modified periodogram (Welch)
     if method == PSD_TYPE.WELCH:
         if window_length is not None:
-            nperseg = window_length * fs
+            nperseg = window_length * sampling_rate
         else:
-            nperseg = (2 / low) * fs
+            nperseg = (2 / low) * sampling_rate
 
-        freqs, psd = welch(data, fs, nperseg=nperseg, scaling='density')
+        freqs, psd = welch(data, sampling_rate, nperseg=nperseg, scaling='density')
 
+    # Compute the modified periodogram (MultiTaper)
     elif method == PSD_TYPE.MULTITAPER:
-        psd, freqs = psd_array_multitaper(data, fs, adaptive=True, normalization='full')
+        psd, freqs = psd_array_multitaper(
+            data, sampling_rate, adaptive=True, normalization='full')
 
     # Find index of band in frequency vector
     idx_min = np.argmax(freqs > low) - 1
@@ -49,10 +79,6 @@ def power_spectral_density(
 
     # Integral approximation of the spectrum using parabola (Simpson's rule)
     bp = simps(psd[idx_band], freqs[idx_band])
-
-    # Define sampling frequency and time vector
-    np.arange(data.size) / fs
-    win = window_length * fs
 
     # Plot the power spectrum
     if plot:
@@ -67,14 +93,16 @@ def power_spectral_density(
         sns.despine()
         plt.show()
 
+    # Whether or not to return PSD as a percentage of total power
     if relative:
         bp /= simps(psd, freqs)
+
     return bp
 
 
 if __name__ == '__main__':
     data = np.loadtxt('bcipy/signal/processing/decomposition/resources/data.txt')
-    fs = 100
+    sampling_rate = 100
     band = (0, 100)
-    np.arange(data.size) / fs
-    power_spectral_density(data, band, fs=fs, method=PSD_TYPE.WELCH, plot=True)
+    np.arange(data.size) / sampling_rate
+    power_spectral_density(data, band, sampling_rate=sampling_rate, method=PSD_TYPE.WELCH, plot=True)
