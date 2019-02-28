2815edb498d3f0ccbe019e94832df78890bb5f05
Matthew Lawhead <lawhead@ohsu.edu>
2019-02-28 15:36:27 -0800

    Updated signal viewer to use the most recent signal processing filters

--- bcipy/gui/viewer/filter.py
+++ bcipy/gui/viewer/filter.py
@@ -1,5 +1,6 @@
 """Defines filter functions that can be applied to a viewer stream."""
 import numpy as np
+from bcipy.signal.process.filter import bandpass, notch
 # from bcipy.signal.processing.sig_pro import sig_pro
 
 
@@ -23,7 +24,12 @@ def downsample_filter(factor: int, fs: int):
     return fn
 
 
-def sig_pro_filter(factor, fs):
+def sig_pro_filter(factor,
+                   fs,
+                   notch_filter_frequency=60,
+                   filter_low=2,
+                   filter_high=45,
+                   filter_order=2):
     """Data filter that calls the sig_pro filter.
     Parameters:
     -----------
@@ -37,5 +43,9 @@ def sig_pro_filter(factor, fs):
     def fn(data):
         """Data should be an np array with a row (np array: float) for each
         channel."""
-        return data[:, ::factor]
+        notch_filtered_data = notch.notch_filter(data, fs, notch_filter_frequency)
+        filtered_data = bandpass.butter_bandpass_filter(
+            notch_filtered_data, filter_low, filter_high, fs, order=filter_order)
+        # return downsampled, filtered data
+        return filtered_data[:, ::factor]
     return fn
