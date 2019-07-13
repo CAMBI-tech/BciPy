d8ad2e94f98fef63286fd45f288157d4390be02a
Tab Memmott <tab.memmott@gmail.com>
2019-07-13 10:52:26 -0700

    remove path additions from init files

--- bcipy/acquisition/datastream/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from os.path import dirname
-sys.path.append(dirname(__file__))
-sys.path.append('.')
-sys.path.append('..')
--- bcipy/acquisition/protocols/dsi/__init__.py
+++ bcipy/acquisition/protocols/dsi/__init__.py
@@ -1,7 +1,1 @@
-"""Binary packet definitions for various EEG Data streaming devices."""
 
-import sys
-from os.path import dirname
-sys.path.append(dirname(__file__))
-sys.path.append('.')
-sys.path.append('..')
