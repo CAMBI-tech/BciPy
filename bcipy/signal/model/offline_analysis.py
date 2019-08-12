aadb717e238b0c8e3a3bcd7b16f37408f6195655
lawhead <lawhead@ohsu.edu>
2019-08-12 13:22:06 -0700

    Re-enable console output for offline_analysis

--- bcipy/signal/model/offline_analysis.py
+++ bcipy/signal/model/offline_analysis.py
@@ -111,6 +111,7 @@ def offline_analysis(data_folder: str = None,
 
 if __name__ == "__main__":
     import argparse
+    import sys
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data_folder', default=None)
@@ -118,7 +119,13 @@ if __name__ == "__main__":
                         default='bcipy/parameters/parameters.json')
     args = parser.parse_args()
 
-    print(f'Loading params from {args.parameters_file}')
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='(%(threadName)-9s) %(message)s')
+
+    log.info(f'Loading params from {args.parameters_file}')
     parameters = load_json_parameters(args.parameters_file,
                                       value_cast=True)
     offline_analysis(args.data_folder, parameters)
+
+    log.info("Offline Analysis complete.")
