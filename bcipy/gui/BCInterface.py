7c9f6f2daffd408d1e145d6acca46e25ddfaeffd
Tab Memmott <tab.memmott@gmail.com>
2020-10-28 14:38:56 -0700

    Refactor BCInterface to use new gui_main
    todo: resize / move assets

--- bcipy/gui/BCInterface.py
+++ bcipy/gui/BCInterface.py
@@ -1,57 +1,60 @@
 import subprocess
 import wx
-from bcipy.gui.gui_main import BCIGui
+import sys
+from bcipy.gui.gui_main import BCIGui, app, PushButton
 
 
 # Make a custom GUI class with the events we want
 class BCInterface(BCIGui):
-    def bind_action(self, action: str, btn: wx.Button) -> None:
-        if action == 'launch_mode':
-            self.Bind(wx.EVT_BUTTON, self.launch_mode, btn)
-        else:
-            self.Bind(wx.EVT_BUTTON, self.on_clicked, btn)
 
-    def launch_mode(self, event) -> None:
-        mode_label = event.GetEventObject().GetLabel()
-        if mode_label == 'RSVP':
-            subprocess.Popen(
-                'python bcipy/gui/mode/RSVPKeyboard.py',
-                shell=True)
+    def launch_rsvp(self) -> None:
+        print('launching')
+        subprocess.Popen(
+            'python bcipy/gui/mode/RSVPKeyboard.py',
+            shell=True)
 
+    def build_buttons(self):
+        self.add_button(
+            message="RSVP",
+            position=[155, 200], size=[100, 100],
+            background_color='red',
+            action=self.launch_rsvp)
+        self.add_button(
+            message="Matrix", position=[280, 200],
+            size=[100, 100],
+            background_color='blue')
+        self.add_button(
+            message="Shuffle", position=[405, 200],
+            size=[100, 100],
+            background_color='green')
 
-app = wx.App(False)
-gui = BCInterface(
-    title="Brain Computer Interface",
-    size=(700, 400), background_color='black')
+    def build_images(self):
+        self.add_image(
+            path='bcipy/static/images/gui_images/ohsu.png', position=[5, 0], size=125)
+        self.add_image(
+            path='bcipy/static/images/gui_images/neu.png', position=[550, 0], size=125)
 
-# STATIC TEXT!
-gui.add_static_text(
-    text='Brain Computer Interface', position=(150, 0), size=25, color='white')
-
-# BUTTONS!
-gui.add_button(
-    message="RSVP",
-    position=(155, 200), size=(100, 100),
-    color=wx.Colour(221, 37, 56),
-    action='launch_mode')
-gui.add_button(
-    message="Matrix", position=(280, 200),
-    size=(100, 100),
-    color=wx.Colour(62, 161, 232),
-    action='launch_mode')
-gui.add_button(
-    message="Shuffle", position=(405, 200),
-    size=(100, 100),
-    color=wx.Colour(117, 173, 48),
-    action='launch_mode')
-
-# Images
-gui.add_image(
-    path='bcipy/static/images/gui_images/ohsu.png', position=(5, 0), size=125)
-gui.add_image(
-    path='bcipy/static/images/gui_images/neu.png', position=(550, 0), size=125)
+    def build_text(self):
+        self.add_static_text(text='Brain Computer Interface', position=[150, 0], size=[25, 25], background_color='white')
 
 
-# Make the GUI Show now
-gui.show_gui()
-app.MainLoop()
+def start_app():
+    """Start BCIGui."""
+    bcipy_gui = app(sys.argv)
+    ex = BCInterface(
+        title="Brain Computer Interface",
+        height=700,
+        width=700,
+        background_color='black')
+
+    ex.build_buttons()
+    ex.build_images()
+    ex.build_text()
+
+
+    ex.show_gui()
+
+    sys.exit(bcipy_gui.exec_())
+
+if __name__ == '__main__':
+    start_app()
