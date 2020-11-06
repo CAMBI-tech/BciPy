f0ede955b8f9af37779d91474d9cfaccf06f4f42
lawhead <lawhead@ohsu.edu>
2020-11-06 15:04:39 -0800

    Fixed demo script for converting

--- bcipy/helpers/demo/demo_convert.py
+++ bcipy/helpers/demo/demo_convert.py
@@ -1,13 +1,23 @@
 """Demonstrates converting raw_data output to other EEG formats"""
-from bcipy.helpers.convert import write_edf
+from bcipy.helpers.convert import convert_to_edf
 from mne.io import read_raw_edf
 
 
-def plot_edf(edf_path: str):
+def plot_edf(edf_path: str, auto_scale: bool = False):
     """Plot data from the raw edf file. Note: this works from an iPython
-    session but seems to throw errors when provided in a script."""
+    session but seems to throw errors when provided in a script.
+    
+    Parameters
+    ----------
+        edf_path - full path to the generated edf file
+        auto_scale - optional; if True will scale the EEG data; this is
+            useful for fake (random) data but makes real data hard to read.
+    """
     edf = read_raw_edf(edf_path, preload=True)
-    edf.plot(scalings='auto')
+    if auto_scale:
+        edf.plot(scalings='auto')
+    else:
+        edf.plot()
 
 
 if __name__ == '__main__':
@@ -20,5 +30,5 @@ if __name__ == '__main__':
         help='Path to the directory with raw_data to be converted',
         required=True)
     args = parser.parse_args()
-    edf_path = write_edf(args.path)
-    print(f"\nWrote edf file to {edf_path}.")
+    edf_path = convert_to_edf(args.path)
+    print(f"\nWrote edf file to {edf_path}")
