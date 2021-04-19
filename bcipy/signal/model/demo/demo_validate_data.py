2ed1520d1a96fc5e6f6c35bff31a72ff6f5135d4
Niklas <niklas.sm+github@gmail.com>
2021-04-19 19:07:01 -0400

    - undo formatting changes in demos and offline_analysis - move demos back to original location

--- bcipy/signal/model/demo/demo_validate_data.py
+++ bcipy/signal/model/demo/demo_validate_data.py
@@ -9,7 +9,7 @@ from bcipy.signal.model.mach_learning.train_model import train_pca_rda_kde_model
 from bcipy.helpers.task import trial_reshaper
 from bcipy.helpers.triggers import trigger_decoder
 
-mpl.use("TkAgg")
+mpl.use('TkAgg')
 
 
 def _demo_validate_data():
@@ -19,8 +19,12 @@ def _demo_validate_data():
 
     num_ch = 20
 
-    x_p_train = np.asarray([np.random.randn(num_x_p, dim_x) for i in range(num_ch)])
-    x_n_train = np.array([np.random.randn(num_x_p, dim_x) for i in range(num_ch)])
+    x_p_train = np.asarray(
+        [np.random.randn(num_x_p, dim_x) for i in
+         range(num_ch)])
+    x_n_train = np.array(
+        [np.random.randn(num_x_p, dim_x) for i in
+         range(num_ch)])
     y_p_train = [1] * num_x_p
     y_n_train = [0] * num_x_n
 
@@ -35,31 +39,28 @@ def _demo_validate_data():
 
     fig = plt.figure()
     ax = fig.add_subplot(211)
-    x_plot = np.linspace(np.min(model.line_el[-1]), np.max(model.line_el[-1]), 1000)[:, np.newaxis]
-    ax.plot(
-        model.line_el[2][y_train == 0],
-        -0.005 - 0.01 * np.random.random(model.line_el[2][y_train == 0].shape[0]),
-        "ro",
-        label="class(-)",
-    )
-    ax.plot(
-        model.line_el[2][y_train == 1],
-        -0.005 - 0.01 * np.random.random(model.line_el[2][y_train == 1].shape[0]),
-        "go",
-        label="class(+)",
-    )
+    x_plot = np.linspace(np.min(model.line_el[-1]), np.max(model.line_el[-1]),
+                         1000)[:, np.newaxis]
+    ax.plot(model.line_el[2][y_train == 0], -0.005 - 0.01 * np.random.random(
+        model.line_el[2][y_train == 0].shape[0]), 'ro', label='class(-)')
+    ax.plot(model.line_el[2][y_train == 1], -0.005 - 0.01 * np.random.random(
+        model.line_el[2][y_train == 1].shape[0]), 'go', label='class(+)')
     for idx in range(len(model.pipeline[2].list_den_est)):
         log_dens = model.pipeline[2].list_den_est[idx].score_samples(x_plot)
-        ax.plot(x_plot[:, 0], np.exp(log_dens), "r-" * (idx == 0) + "g-" * (idx == 1), linewidth=2.0)
+        ax.plot(x_plot[:, 0], np.exp(log_dens),
+                'r-' * (idx == 0) + 'g-' * (idx == 1), linewidth=2.0)
 
-    ax.legend(loc="upper right")
-    plt.title("Training Data")
-    plt.ylabel("p(e|l)")
-    plt.xlabel("scores")
+    ax.legend(loc='upper right')
+    plt.title('Training Data')
+    plt.ylabel('p(e|l)')
+    plt.xlabel('scores')
 
     # Test
-    x_p_test = np.asarray([np.random.randn(num_x_p, dim_x) for i in range(num_ch)])
-    x_n_test = np.array([np.random.randn(num_x_p, dim_x) for i in range(num_ch)])
+    x_p_test = np.asarray(
+        [np.random.randn(num_x_p, dim_x) for i in
+         range(num_ch)])
+    x_n_test = np.array(
+        [np.random.randn(num_x_p, dim_x) for i in range(num_ch)])
     y_p_test = [1] * num_x_p
     y_n_test = [0] * num_x_n
 
@@ -72,33 +73,26 @@ def _demo_validate_data():
 
     model.transform(x_test)
 
-    ax.plot(
-        model.line_el[2][y_test == 0],
-        -0.01 - 0.01 * np.random.random(model.line_el[2][y_test == 0].shape[0]),
-        "bo",
-        label="t_class(-)",
-    )
-    ax.plot(
-        model.line_el[2][y_test == 1],
-        -0.01 - 0.01 * np.random.random(model.line_el[2][y_test == 1].shape[0]),
-        "ko",
-        label="t_class(+)",
-    )
+    ax.plot(model.line_el[2][y_test == 0], -0.01 - 0.01 * np.random.random(
+        model.line_el[2][y_test == 0].shape[0]), 'bo', label='t_class(-)')
+    ax.plot(model.line_el[2][y_test == 1], -0.01 - 0.01 * np.random.random(
+        model.line_el[2][y_test == 1].shape[0]), 'ko', label='t_class(+)')
 
-    bandwidth = (
-        1.06 * min(np.std(model.line_el[2]), iqr(model.line_el[2]) / 1.34) * np.power(model.line_el[2].shape[0], -0.2)
-    )
+    bandwidth = 1.06 * min(np.std(model.line_el[2]),
+                           iqr(model.line_el[2]) / 1.34) * np.power(
+        model.line_el[2].shape[0], -0.2)
     test_kde = KernelDensityEstimate(bandwidth=bandwidth)
     test_kde.fit(model.line_el[2], y_test)
 
     for idx in range(len(model.pipeline[2].list_den_est)):
         log_dens = test_kde.list_den_est[idx].score_samples(x_plot)
-        ax.plot(x_plot[:, 0], np.exp(log_dens), "b--" * (idx == 0) + "k--" * (idx == 1), linewidth=2.0)
+        ax.plot(x_plot[:, 0], np.exp(log_dens),
+                'b--' * (idx == 0) + 'k--' * (idx == 1), linewidth=2.0)
 
-    ax.legend(loc="upper right")
-    plt.title("Training Data")
-    plt.ylabel("p(e|l)")
-    plt.xlabel("scores")
+    ax.legend(loc='upper right')
+    plt.title('Training Data')
+    plt.ylabel('p(e|l)')
+    plt.xlabel('scores')
     plt.show()
 
 
@@ -107,83 +101,80 @@ def _demo_validate_real_data():
     channel_map = [1] * 16 + [0, 0, 1, 1, 0, 1, 1, 1, 0]
     data_train_folder = load_experimental_data()
 
-    mode = "calibration"
+    mode = 'calibration'
 
-    raw_dat, stamp_time, channels, type_amp, fs = read_data_csv(data_train_folder + "/rawdata.csv")
+    raw_dat, stamp_time, channels, type_amp, fs = read_data_csv(
+        data_train_folder + '/rawdata.csv')
 
     dat = bandpass.text_filter(raw_dat, fs=fs, k=ds_rate)
 
     # Get data and labels
-    s_i, t_t_i, t_i = trigger_decoder(mode=mode, trigger_loc=data_train_folder + "/triggers.txt")
-    x_train, y_train, num_inq, _ = trial_reshaper(t_t_i, t_i, dat, mode=mode, fs=fs, k=ds_rate, channel_map=channel_map)
+    s_i, t_t_i, t_i = trigger_decoder(mode=mode,
+                                      trigger_loc=data_train_folder + '/triggers.txt')
+    x_train, y_train, num_inq, _ = trial_reshaper(t_t_i, t_i, dat, mode=mode,
+                                                  fs=fs,
+                                                  k=ds_rate,
+                                                  channel_map=channel_map)
 
     model = train_pca_rda_kde_model(x_train, y_train, k_folds=10)
 
     fig = plt.figure()
     ax = fig.add_subplot(211)
-    x_plot = np.linspace(np.min(model.line_el[-1]), np.max(model.line_el[-1]), 1000)[:, np.newaxis]
-    ax.plot(
-        model.line_el[2][y_train == 0],
-        -0.005 - 0.01 * np.random.random(model.line_el[2][y_train == 0].shape[0]),
-        "ro",
-        label="class(-)",
-    )
-    ax.plot(
-        model.line_el[2][y_train == 1],
-        -0.005 - 0.01 * np.random.random(model.line_el[2][y_train == 1].shape[0]),
-        "go",
-        label="class(+)",
-    )
+    x_plot = np.linspace(np.min(model.line_el[-1]), np.max(model.line_el[-1]),
+                         1000)[:, np.newaxis]
+    ax.plot(model.line_el[2][y_train == 0], -0.005 - 0.01 * np.random.random(
+        model.line_el[2][y_train == 0].shape[0]), 'ro', label='class(-)')
+    ax.plot(model.line_el[2][y_train == 1], -0.005 - 0.01 * np.random.random(
+        model.line_el[2][y_train == 1].shape[0]), 'go', label='class(+)')
     for idx in range(len(model.pipeline[2].list_den_est)):
         log_dens = model.pipeline[2].list_den_est[idx].score_samples(x_plot)
-        ax.plot(x_plot[:, 0], np.exp(log_dens), "r-" * (idx == 0) + "g-" * (idx == 1), linewidth=2.0)
+        ax.plot(x_plot[:, 0], np.exp(log_dens),
+                'r-' * (idx == 0) + 'g-' * (idx == 1), linewidth=2.0)
 
-    ax.legend(loc="upper right")
-    plt.title("Training Data")
-    plt.ylabel("p(e|l)")
-    plt.xlabel("scores")
+    ax.legend(loc='upper right')
+    plt.title('Training Data')
+    plt.ylabel('p(e|l)')
+    plt.xlabel('scores')
 
     # Test
     data_test_folder = load_experimental_data()
 
-    mode = "calibration"
+    mode = 'calibration'
 
-    raw_dat, stamp_time, channels, type_amp, fs = read_data_csv(data_test_folder + "/rawdata.csv")
+    raw_dat, stamp_time, channels, type_amp, fs = read_data_csv(
+        data_test_folder + '/rawdata.csv')
     dat = bandpass.text_filter(raw_dat, fs=fs, k=ds_rate)
 
     # Get data and labels
-    s_i, t_t_i, t_i = trigger_decoder(mode=mode, trigger_loc=data_test_folder + "/triggers.txt")
-    x_test, y_test, num_inq, _ = trial_reshaper(t_t_i, t_i, dat, mode=mode, fs=fs, k=ds_rate, channel_map=channel_map)
+    s_i, t_t_i, t_i = trigger_decoder(mode=mode,
+                                      trigger_loc=data_test_folder + '/triggers.txt')
+    x_test, y_test, num_inq, _ = trial_reshaper(t_t_i, t_i, dat, mode=mode,
+                                                fs=fs,
+                                                k=ds_rate,
+                                                channel_map=channel_map)
 
     model.transform(x_test)
 
-    ax.plot(
-        model.line_el[2][y_test == 0],
-        -0.01 - 0.01 * np.random.random(model.line_el[2][y_test == 0].shape[0]),
-        "bo",
-        label="t_class(-)",
-    )
-    ax.plot(
-        model.line_el[2][y_test == 1],
-        -0.01 - 0.01 * np.random.random(model.line_el[2][y_test == 1].shape[0]),
-        "ko",
-        label="t_class(+)",
-    )
+    ax.plot(model.line_el[2][y_test == 0], -0.01 - 0.01 * np.random.random(
+        model.line_el[2][y_test == 0].shape[0]), 'bo', label='t_class(-)')
+    ax.plot(model.line_el[2][y_test == 1], -0.01 - 0.01 * np.random.random(
+        model.line_el[2][y_test == 1].shape[0]), 'ko', label='t_class(+)')
 
-    bandwidth = (
-        1.06 * min(np.std(model.line_el[2]), iqr(model.line_el[2]) / 1.34) * np.power(model.line_el[2].shape[0], -0.2)
-    )
+    bandwidth = 1.06 * min(np.std(model.line_el[2]),
+                           iqr(model.line_el[2]) / 1.34) * np.power(
+        model.line_el[2].shape[0], -0.2)
     test_kde = KernelDensityEstimate(bandwidth=bandwidth)
     test_kde.fit(model.line_el[2], y_test)
 
     for idx in range(len(model.pipeline[2].list_den_est)):
         log_dens = test_kde.list_den_est[idx].score_samples(x_plot)
-        ax.plot(x_plot[:, 0], np.exp(log_dens), "b--" * (idx == 0) + "k--" * (idx == 1), linewidth=2.0)
+        ax.plot(x_plot[:, 0], np.exp(log_dens),
+                'b--' * (idx == 0) + 'k--' * (idx == 1), linewidth=2.0)
 
-    ax.legend(loc="upper right")
-    plt.title("Training Data")
-    plt.ylabel("p(e|l)")
-    plt.xlabel("scores")
+    ax.legend(loc='upper right')
+    plt.title('Training Data')
+    plt.ylabel('p(e|l)')
+    plt.xlabel('scores')
 
     plt.show()
 
--- bcipy/signal/model/offline_analysis.py
+++ bcipy/signal/model/offline_analysis.py
@@ -1,83 +1,88 @@
 import pickle
 import logging
 
-from bcipy.helpers.load import read_data_csv, load_experimental_data, load_json_parameters
+from bcipy.helpers.load import (
+    read_data_csv,
+    load_experimental_data,
+    load_json_parameters)
 from bcipy.signal.process.filter import bandpass, notch, downsample
 from bcipy.signal.model.mach_learning.train_model import train_pca_rda_kde_model
 from bcipy.helpers.task import trial_reshaper
 from bcipy.helpers.visualization import generate_offline_analysis_screen
 from bcipy.helpers.triggers import trigger_decoder
-from bcipy.helpers.acquisition import analysis_channels, analysis_channel_names_by_pos
+from bcipy.helpers.acquisition import analysis_channels,\
+    analysis_channel_names_by_pos
 from bcipy.helpers.stimuli import play_sound
 
 log = logging.getLogger(__name__)
 
 
-def offline_analysis(
-    data_folder: str = None,
-    parameters: dict = {},
-    alert_finished: bool = True,
-    return_fig_handles: bool = False,
-):
-    """Gets calibration data and trains the model in an offline fashion.
-    pickle dumps the model into a .pkl folder
-    Args:
-        data_folder(str): folder of the data
-            save all information and load all from this folder
-        parameter(dict): parameters for running offline analysis
-        alert_finished(bool): whether or not to alert the user offline analysis complete
-        return_fig_handles(bool): whether or not to return handles for all figures generated
+def offline_analysis(data_folder: str = None,
+                     parameters: dict = {}, alert_finished: bool = True,
+                     return_fig_handles: bool = False):
+    """ Gets calibration data and trains the model in an offline fashion.
+        pickle dumps the model into a .pkl folder
+        Args:
+            data_folder(str): folder of the data
+                save all information and load all from this folder
+            parameter(dict): parameters for running offline analysis
+            alert_finished(bool): whether or not to alert the user offline analysis complete
+            return_fig_handles(bool): whether or not to return handles for all figures generated
 
-    How it Works:
-    - reads data and information from a .csv calibration file
-    - reads trigger information from a .txt trigger file
-    - filters data
-    - reshapes and labels the data for the training procedure
-    - fits the model to the data
-        - uses cross validation to select parameters
-        - based on the parameters, trains system using all the data
-    - pickle dumps model into .pkl file
-    - generates and saves offline analysis screen
-    - [optional] alert the user finished processing
+        How it Works:
+        - reads data and information from a .csv calibration file
+        - reads trigger information from a .txt trigger file
+        - filters data
+        - reshapes and labels the data for the training procedure
+        - fits the model to the data
+            - uses cross validation to select parameters
+            - based on the parameters, trains system using all the data
+        - pickle dumps model into .pkl file
+        - generates and saves offline analysis screen
+        - [optional] alert the user finished processing
     """
 
     if not data_folder:
         data_folder = load_experimental_data()
 
-    mode = "calibration"
+    mode = 'calibration'
 
     # extract relevant session information from parameters file
-    trial_length = parameters.get("trial_length")
-    triggers_file = parameters.get("trigger_file_name", "triggers.txt")
-    raw_data_file = parameters.get("raw_data_name", "raw_data.csv")
+    trial_length = parameters.get('trial_length')
+    triggers_file = parameters.get('trigger_file_name', 'triggers.txt')
+    raw_data_file = parameters.get('raw_data_name', 'raw_data.csv')
 
     # get signal filtering information
-    downsample_rate = parameters.get("down_sampling_rate", 2)
-    notch_filter = parameters.get("notch_filter_frequency", 60)
-    hp_filter = parameters.get("filter_high", 45)
-    lp_filter = parameters.get("filter_low", 2)
-    filter_order = parameters.get("filter_order", 2)
+    downsample_rate = parameters.get('down_sampling_rate', 2)
+    notch_filter = parameters.get('notch_filter_frequency', 60)
+    hp_filter = parameters.get('filter_high', 45)
+    lp_filter = parameters.get('filter_low', 2)
+    filter_order = parameters.get('filter_order', 2)
 
     # get offset and k folds
-    static_offset = parameters.get("static_trigger_offset", 0)
-    k_folds = parameters.get("k_folds", 10)
+    static_offset = parameters.get('static_trigger_offset', 0)
+    k_folds = parameters.get('k_folds', 10)
 
-    raw_dat, _, channels, type_amp, fs = read_data_csv(data_folder + "/" + raw_data_file)
+    raw_dat, _, channels, type_amp, fs = read_data_csv(
+        data_folder + '/' + raw_data_file)
 
-    log.info(f"Channels read from csv: {channels}")
-    log.info(f"Device type: {type_amp}")
+    log.info(f'Channels read from csv: {channels}')
+    log.info(f'Device type: {type_amp}')
 
     # Remove 60hz noise with a notch filter
     notch_filter_data = notch.notch_filter(raw_dat, fs, frequency_to_remove=notch_filter)
 
     # bandpass filter
-    filtered_data = bandpass.butter_bandpass_filter(notch_filter_data, lp_filter, hp_filter, fs, order=filter_order)
+    filtered_data = bandpass.butter_bandpass_filter(
+        notch_filter_data, lp_filter, hp_filter, fs, order=filter_order)
 
     # downsample
     data = downsample.downsample(filtered_data, factor=downsample_rate)
 
     # Process triggers.txt
-    _, t_t_i, t_i, offset = trigger_decoder(mode=mode, trigger_path=f"{data_folder}/{triggers_file}")
+    _, t_t_i, t_i, offset = trigger_decoder(
+        mode=mode,
+        trigger_path=f'{data_folder}/{triggers_file}')
 
     offset = offset + static_offset
 
@@ -85,49 +90,35 @@ def offline_analysis(
     # read_data_csv already removes the timespamp column.
     channel_map = analysis_channels(channels, type_amp)
 
-    # TODO - add inquiry_reshaper to return entire inquiries instead of individual trials
-    # Could move this inside the model perhaps? or give this a flag to ask for trials vs sequences
-    x, y, _, _ = trial_reshaper(
-        t_t_i,
-        t_i,
-        data,
-        mode=mode,
-        fs=fs,
-        k=downsample_rate,
-        offset=offset,
-        channel_map=channel_map,
-        trial_length=trial_length,
-    )
+    x, y, _, _ = trial_reshaper(t_t_i, t_i, data,
+                                mode=mode, fs=fs, k=downsample_rate,
+                                offset=offset,
+                                channel_map=channel_map,
+                                trial_length=trial_length)
 
     model, auc = train_pca_rda_kde_model(x, y, k_folds=k_folds)
 
-    log.info("Saving offline analysis plots!")
+    log.info('Saving offline analysis plots!')
 
     # After obtaining the model get the transformed data for plotting purposes
     model.transform(x)
     fig_handles = generate_offline_analysis_screen(
-        x,
-        y,
-        model=model,
-        folder=data_folder,
+        x, y, model=model, folder=data_folder,
         down_sample_rate=downsample_rate,
-        fs=fs,
-        save_figure=True,
-        show_figure=False,
-        channel_names=analysis_channel_names_by_pos(channels, channel_map),
-    )
+        fs=fs, save_figure=True, show_figure=False,
+        channel_names=analysis_channel_names_by_pos(channels, channel_map))
 
-    log.info("Saving the model!")
-    with open(data_folder + f"/model_{auc}.pkl", "wb") as output:
+    log.info('Saving the model!')
+    with open(data_folder + f'/model_{auc}.pkl', 'wb') as output:
         pickle.dump(model, output)
 
     if alert_finished:
-        offline_analysis_tone = parameters.get("offline_analysis_tone")
+        offline_analysis_tone = parameters.get('offline_analysis_tone')
         play_sound(offline_analysis_tone)
 
     if return_fig_handles:
         return model, fig_handles
-    else:
+    else:      
         return model
 
 
@@ -135,14 +126,18 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--data_folder", default=None)
-    parser.add_argument("-p", "--parameters_file", default="bcipy/parameters/parameters.json")
+    parser.add_argument('-d', '--data_folder', default=None)
+    parser.add_argument('-p', '--parameters_file',
+                        default='bcipy/parameters/parameters.json')
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG, format="(%(threadName)-9s) %(message)s")
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='(%(threadName)-9s) %(message)s')
 
-    log.info(f"Loading params from {args.parameters_file}")
-    parameters = load_json_parameters(args.parameters_file, value_cast=True)
+    log.info(f'Loading params from {args.parameters_file}')
+    parameters = load_json_parameters(args.parameters_file,
+                                      value_cast=True)
     offline_analysis(args.data_folder, parameters)
 
-    log.info("Offline Analysis complete.")
+    log.info('Offline Analysis complete.')
