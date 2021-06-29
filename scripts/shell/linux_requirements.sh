afd0bc0539f5936327e9301f1d303fcee9b9a813
Tab Memmott <tab.memmott@gmail.com>
2021-06-29 14:12:28 -0700

    add linux requirements shell script

---
+++ scripts/shell/linux_requirements.sh
@@ -0,0 +1,13 @@
+sudo apt-get install libgtk-3-dev
+sudo apt-get install python3-pip  # for ubuntu 20.04, this gives python3.8.5
+sudo apt-get install freeglut3-dev
+sudo apt-get install freetype
+sudo apt-get install portaudio
+
+# Python 3.7
+# sudo apt-get install python3.7-tk python3.7-dev 
+# Python 3.8
+sudo apt-get install python3.8-tk python3.8-dev 
+
+# run without a display. All commands must have xvfb-run preceeding them. 
+# sudo apt-get install xvfb
