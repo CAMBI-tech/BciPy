c2b783c6e7e44f6958ef3d57983b6261c5137909
Tab Memmott <tab.memmott@gmail.com>
2021-08-02 15:14:41 -0700

    initial commit language model base class

---
+++ bcipy/language/main.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Tuple, List, Union
+from enum import Enum
+
+
+class ResponseType(Enum):
+    SYMBOL = 'Symbol'
+    WORD = 'Word'
+
+
+class LanguageModel(ABC):
+
+    response_type: ResponseType
+    symbol_set: List[str]
+
+    @abstractmethod
+    def predict(self, evidence: List[Tuple]) -> List[tuple]:
+        """
+        Using the provided data, compute log likelihoods over the entire symbol set.
+        Args:
+            evidence - [('A', .08), ('B', .1)]
+
+        Response:
+            probability - dependant on response type, a list of words or symbols with probability 
+        """
+        ...
+
+    @abstractmethod
+    def update(self) -> None:
+        """Update the model state"""
+        ...
+
+    @abstractmethod
+    def load(self, path: Path) -> None:
+        """Restore model state from the provided checkpoint"""
+        ...
