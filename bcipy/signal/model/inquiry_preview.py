a5e34cba47bc55bb7857add34155c0a7ab60c1d1
Niklas <niklas.sm+github@gmail.com>
2022-01-24 15:39:08 -0500

    WIP - button model needs to provide multiplicative update to be combined with LM later

--- bcipy/signal/model/inquiry_preview.py
+++ bcipy/signal/model/inquiry_preview.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def compute_probs_after_preview(
+def _OLD_compute_probs_after_preview(
     inquiry: List[str], symbol_set: List[str], user_error_prob: float, proceed: bool
 ) -> List[float]:
     """
@@ -29,3 +29,103 @@ def compute_probs_after_preview(
         other_letter_value = (1 - user_error_prob) / (sym_set_len - inq_len)
 
     return [shown_letter_value if letter in inquiry else other_letter_value for letter in symbol_set]
+
+
+def compute_probs_after_preview(
+    query: List[str], alphabet: List[str], user_error_prob: float, proceed: bool
+) -> List[float]:
+    """
+    Notation:
+    - "want" = desired letter
+    - "show" = shown letters
+    - "+" = user selected
+    - "-" = user not selected
+
+    Assumptions:
+    1. "want" independent of "show".
+        - if we start from uniform alphabet, and treat each query as independent event, this is true
+        - this is false if our shown letters are very enriched (in that case, we would bet on the displayed letters!)
+        - used to say p(want | show) = p(want)
+        - To avoid this assumption, we need info on past queries and responses somehow!
+    2. User's errors are symmetric. p(+ | want not in show) = p(- | want in show) = user_error_prob
+
+    Args:
+        query - the symbols presented in the query preview
+        alphabet - the full alphabet of symbols
+        proceed - whether or not the user wants to proceed with this query
+        user_error_prob - the probability that the user selects incorrectly during preview
+    """
+    if not 0 <= user_error_prob <= 1:
+        raise ValueError(f"invalid user error prob: {user_error_prob}")
+
+    uniform_prior = 1 / len(alphabet)
+
+    if proceed:
+        """suppose user_error_prob = 0.05, and len(alphabet) = 28. suppose we present AB (random 2 of 28 letters).
+        the user responds positively ("+")
+            p(want=A | +, show=AB) = p(+ | want=A, show=AB) p(want=A | show=AB) / p(+ | show=AB)
+                                   = p(+ | want in show) p(want=A) / p(+ | show=AB)                 # Use assumption 1
+                                   = 0.95 * (1/28) / [
+                                        p(+ | want=A, show=AB) p(want=A)
+                                        + p(+ | want=B, show=AB) p(want=B)
+                                        + p(+ | want=C, show=AB) p(want=C)
+                                        ...
+                                        + p(+ | want=Z, show=AB) p(want=Z)
+                                    ]
+
+        We can compute this from just 2 terms:
+            shown_term = p(+ | want=shown_letter, show=[....,shown_letter,...]) p(want=shown_letter)
+            unshown_term = p(+ | want=unshown_letter, show=[...]) p(want=unshown_letter)
+
+        Then we can update as follows:
+            p(shown_letter | +, show=[...]) = shown_term / [
+                                                len(show) * shown_term
+                                                + (len(alphabet) - len(show)) * unshown_term
+                                            ]
+
+            p(unshown_letter | +, show=[...]) = unshown_term / [
+                                                len(show) * shown_term
+                                                + (len(alphabet) - len(show)) * unshown_term
+                                            ]"""
+        shown_term = uniform_prior * (1 - user_error_prob)
+        unshown_term = uniform_prior * user_error_prob
+
+        # p(+ | show=inquiry) - the denominator for bayes rule
+        n_shown = len(query)
+        n_unshown = len(alphabet) - n_shown
+        marginal = n_shown * shown_term + n_unshown * unshown_term
+
+        shown_letter_value = shown_term / marginal
+        other_letter_value = unshown_term / marginal
+
+    else:
+        """Same assumptions as above, but now user responds "-"
+            p(want=A | -, show=AB) = p(- | want=A, show=AB) p(want=A | show=AB) / p(- | show=AB)
+                                   = p(- | want in show) p(want=A) / p(- | show=AB)                 # Use assumption 1
+
+        We can compute this from just 2 terms:
+            shown_term = p(- | want=shown_letter, show=[....,shown_letter,...]) p(want=shown_letter)
+            unshown_term = p(- | want=unshown_letter, show=[...]) p(want=unshown_letter)
+
+        Then we can update as follows:
+            p(shown_letter | -, show=[...]) = shown_term / [
+                                                len(show) * shown_term
+                                                + (len(alphabet) - len(show)) * unshown_term
+                                            ]
+
+            p(unshown_letter | -, show=[...]) = unshown_term / [
+                                                len(show) * shown_term
+                                                + (len(alphabet) - len(show)) * unshown_term
+                                            ]"""
+        shown_term = uniform_prior * user_error_prob
+        unshown_term = uniform_prior * (1 - user_error_prob)
+
+        # p(- | show=inquiry) - denominator for bayes rule
+        n_shown = len(query)
+        n_unshown = len(alphabet) - n_shown
+        marginal = n_shown * shown_term + n_unshown * unshown_term
+
+        shown_letter_value = shown_term / marginal
+        other_letter_value = unshown_term / marginal
+
+    return [shown_letter_value if letter in query else other_letter_value for letter in alphabet]
--- bcipy/signal/tests/model/test_inquiry_preview.py
+++ bcipy/signal/tests/model/test_inquiry_preview.py
@@ -1,33 +1,29 @@
+from abc import ABC
 import unittest
 import numpy as np
 from bcipy.signal.model.inquiry_preview import compute_probs_after_preview
+from string import ascii_uppercase
 
 
-class TestInquiryPreview(unittest.TestCase):
-    def setUp(self):
-        self.inquiry = ["A", "E", "I", "O", "U"]
-        self.symbol_set = self.inquiry + ["Y"]
-        self.error_prob = 0.05
+class InqPreviewBaseTest(ABC):
+    def _compare(self, results, p_shown, p_not_shown):
+        expected = np.array(self.show_first * [p_shown] + (len(self.symbol_set) - self.show_first) * [p_not_shown])
+        expected /= expected.sum()
+        self.assertTrue(np.allclose(results, expected))
 
     def test_user_likes_inquiry(self):
-        proceed = True
-        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed)
-
-        p_shown = 0.19  # (1 - 0.05) / 5
-        p_not_shown = 0.05  # 0.05 / (6 - 1)
-        expected = [p_shown, p_shown, p_shown, p_shown, p_shown, p_not_shown]
-
-        self.assertTrue(np.allclose(results, expected))
+        # Upvote the shown letters
+        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed=True)
+        p_shown = self.uniform_prior * (1 - self.error_prob)
+        p_not_shown = self.uniform_prior * self.error_prob
+        self._compare(results, p_shown, p_not_shown)
 
     def test_user_dislikes_inquiry(self):
-        proceed = False
-        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed)
-
-        p_shown = 0.01  # 0.05 / 5
-        p_not_shown = 0.95  # (1 - 0.05) / 1
-        expected = [p_shown, p_shown, p_shown, p_shown, p_shown, p_not_shown]
-
-        self.assertTrue(np.allclose(results, expected))
+        # Downvote the shown letters
+        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed=False)
+        p_shown = self.uniform_prior * self.error_prob
+        p_not_shown = self.uniform_prior * (1 - self.error_prob)
+        self._compare(results, p_shown, p_not_shown)
 
     def test_invalid_error_prob(self):
         with self.assertRaises(ValueError):
@@ -35,6 +31,44 @@ class TestInquiryPreview(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             compute_probs_after_preview(self.inquiry, self.symbol_set, 1.01, True)
+
+    def test_user_likes_with_LM(self):
+        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed=True)
+        p_shown = self.uniform_prior * (1 - self.error_prob)
+        p_not_shown = self.uniform_prior * self.error_prob
+
+        # Dummy LM only uses 2 values (V and 2*V, such that the alphabet is normalized
+        # Its large value is used for some shown, and some unshown letters. Its small value also used for both.
+        lm_prior = np.ones(len(self.symbol_set))
+        lm_prior[0 : self.show_first - 1] = 2
+        lm_prior[-1] = 2
+        lm_prior /= lm_prior.sum()
+
+        update = np.array(self.show_first * [p_shown] + (len(self.symbol_set) - self.show_first) * [p_not_shown])
+        update /= update.sum()
+
+        expected = update * lm_prior
+        expected /= expected.sum()
+        breakpoint()
+
+    def test_user_dislikes_with_LM(self):
+        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed=False)
+
+
+class TestShortAlphabet(unittest.TestCase, InqPreviewBaseTest):
+    def setUp(self):
+        self.symbol_set = list("ABCDEFG")
+        self.inquiry, self.show_first = list("ABC"), 3
+        self.error_prob = 0.05
+        self.uniform_prior = 1 / len(self.symbol_set)
+
+
+class TestLongAlphabet(unittest.TestCase, InqPreviewBaseTest):
+    def setUp(self):
+        self.symbol_set = list(ascii_uppercase)
+        self.inquiry, self.show_first = list("ABCDE"), 5
+        self.error_prob = 0.05
+        self.uniform_prior = 1 / len(self.symbol_set)
 
 
 if __name__ == "__main__":
