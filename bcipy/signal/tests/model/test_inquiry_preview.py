d6e0bbbf158f7a49e4579935c4843254a43164da
Niklas <niklas.sm+github@gmail.com>
2022-01-25 15:11:00 -0500

    - Update inquiry preview function (simpler). - Add explanation in docstring. - Add tests.

--- bcipy/signal/tests/model/test_inquiry_preview.py
+++ bcipy/signal/tests/model/test_inquiry_preview.py
@@ -1,76 +1,46 @@
-from abc import ABC
 import unittest
 import numpy as np
 from bcipy.signal.model.inquiry_preview import compute_probs_after_preview
 from string import ascii_uppercase
 
 
-class InqPreviewBaseTest(ABC):
-    def _compare(self, results, p_shown, p_not_shown):
-        expected = np.array(self.show_first * [p_shown] + (len(self.symbol_set) - self.show_first) * [p_not_shown])
-        expected /= expected.sum()
-        self.assertTrue(np.allclose(results, expected))
-
-    def test_user_likes_inquiry(self):
-        # Upvote the shown letters
-        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed=True)
-        p_shown = self.uniform_prior * (1 - self.error_prob)
-        p_not_shown = self.uniform_prior * self.error_prob
-        breakpoint()
-        self._compare(results, p_shown, p_not_shown)
-
-    def test_user_dislikes_inquiry(self):
-        # Downvote the shown letters
-        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed=False)
-        p_shown = self.uniform_prior * self.error_prob
-        p_not_shown = self.uniform_prior * (1 - self.error_prob)
-        self._compare(results, p_shown, p_not_shown)
-
-    def test_invalid_error_prob(self):
-        with self.assertRaises(ValueError):
-            compute_probs_after_preview(self.inquiry, self.symbol_set, -0.01, True)
-
-        with self.assertRaises(ValueError):
-            compute_probs_after_preview(self.inquiry, self.symbol_set, 1.01, True)
-
-    def test_user_likes_with_LM(self):
-        return
-        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed=True)
-        p_shown = self.uniform_prior * (1 - self.error_prob)
-        p_not_shown = self.uniform_prior * self.error_prob
-
-        # Dummy LM only uses 2 values (V and 2*V, such that the alphabet is normalized
-        # Its large value is used for some shown, and some unshown letters. Its small value also used for both.
-        lm_prior = np.ones(len(self.symbol_set))
-        lm_prior[0 : self.show_first - 1] = 2
-        lm_prior[-1] = 2
-        lm_prior /= lm_prior.sum()
-
-        update = np.array(self.show_first * [p_shown] + (len(self.symbol_set) - self.show_first) * [p_not_shown])
-        update /= update.sum()
-
-        expected = update * lm_prior
-        expected /= expected.sum()
-        breakpoint()
-
-    def test_user_dislikes_with_LM(self):
-        results = compute_probs_after_preview(self.inquiry, self.symbol_set, self.error_prob, proceed=False)
-
-
-class TestShortAlphabet(unittest.TestCase, InqPreviewBaseTest):
-    def setUp(self):
-        self.symbol_set = list("ABCDEFG")
-        self.inquiry, self.show_first = list("ABC"), 3
-        self.error_prob = 0.05
-        self.uniform_prior = 1 / len(self.symbol_set)
-
-
-class TestLongAlphabet(unittest.TestCase, InqPreviewBaseTest):
+class TestInquiryPreview(unittest.TestCase):
     def setUp(self):
         self.symbol_set = list(ascii_uppercase)
-        self.inquiry, self.show_first = list("ABCDE"), 5
-        self.error_prob = 0.05
-        self.uniform_prior = 1 / len(self.symbol_set)
+
+    def _test(self, inquiry_len, error_prob, user_likes: bool):
+        if user_likes:
+            p_shown = 1 - error_prob
+            p_not_shown = error_prob
+        else:
+            p_shown = error_prob
+            p_not_shown = 1 - error_prob
+
+        inquiry = self.symbol_set[:inquiry_len]
+        results = compute_probs_after_preview(inquiry, self.symbol_set, error_prob, user_likes)
+
+        expected = np.array(inquiry_len * [p_shown] + (len(self.symbol_set) - inquiry_len) * [p_not_shown])
+        self.assertTrue(np.allclose(results, expected))
+
+    def test_user_likes_short_inquiry(self):
+        self._test(5, 0.95, True)
+
+    def test_user_likes_long_inquiry(self):
+        self._test(15, 0.95, True)
+
+    def test_user_dislikes_short_inquiry(self):
+        self._test(5, 0.95, False)
+
+    def test_user_dislikes_long_inquiry(self):
+        self._test(15, 0.95, False)
+
+    def test_invalid_error_prob(self):
+        inquiry = self.symbol_set[:10]
+        with self.assertRaises(ValueError):
+            compute_probs_after_preview(inquiry, self.symbol_set, -0.01, True)
+
+        with self.assertRaises(ValueError):
+            compute_probs_after_preview(inquiry, self.symbol_set, 1.01, True)
 
 
 if __name__ == "__main__":
