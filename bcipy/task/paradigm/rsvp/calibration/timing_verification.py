81d09ac0d527f92fdfe547fab7c25d694e8dbe24
Tab Memmott <tab.memmott@gmail.com>
2022-04-20 11:26:10 -0700

    update RSVP with new PhotoDiodeStimuli and get_fixation

--- bcipy/task/paradigm/rsvp/calibration/timing_verification.py
+++ bcipy/task/paradigm/rsvp/calibration/timing_verification.py
@@ -1,10 +1,14 @@
 from itertools import cycle
 from bcipy.task import Task
 from bcipy.task.paradigm.rsvp.calibration.calibration import RSVPCalibrationTask
+from bcipy.helpers.stimuli import PhotoDiodeStimuli, get_fixation
 
 
 class RSVPTimingVerificationCalibration(Task):
-    """RSVP Calibration Task that for verifying timing.
+    """RSVP Calibration Task.
+
+    This task is used for verifying display timing by alternating solid and empty boxes. These
+        stimuli can be used with a photodiode to ensure accurate presentations.
 
     Input:
         win (PsychoPy Display Object)
@@ -20,7 +24,8 @@ class RSVPTimingVerificationCalibration(Task):
     def __init__(self, win, daq, parameters, file_save):
         super(RSVPTimingVerificationCalibration, self).__init__()
         parameters['stim_height'] = 0.8
-        parameters['stim_pos_y'] = 0.2
+        parameters['stim_pos_y'] = 0.0
+        self.stimuli = PhotoDiodeStimuli.list()
         self._task = RSVPCalibrationTask(win, daq, parameters, file_save)
         self._task.generate_stimuli = self.generate_stimuli
 
@@ -34,17 +39,14 @@ class RSVPTimingVerificationCalibration(Task):
                 color(list(list[str])): list of colors)
         """
         samples, times, colors = [], [], []
-
-        solid_box = '\u25A0'
-        empty_box = '\u25A1'
-
-        target = 'x'  # solid_box  # 'X'
-        fixation = '\u25CB'  # circle
-
+        
         # alternate between solid and empty boxes
-        letters = cycle([solid_box, empty_box])
+        letters = cycle(self.stimuli)
         time_prompt, time_fixation, time_stim = self._task.timing
         color_target, color_fixation, color_stim = self._task.color
+
+        target = next(letters)
+        fixation = get_fixation(is_txt=True)
 
         inq_len = self._task.stim_length
         inq_stim = [target, fixation, *[next(letters) for _ in range(inq_len)]]
