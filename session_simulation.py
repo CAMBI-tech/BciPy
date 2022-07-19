dc7c972652b2ca7ff54b28d7bbad7fcde3e94134
Basak Celik <celik.neu@gmail.com>
2022-07-19 10:11:52 -0400

    inquiry length calculation updated

--- session_simulation.py
+++ session_simulation.py
@@ -2,21 +2,24 @@
 import json
 from bcipy.helpers.task import alphabet
 from bcipy.helpers.load import fast_scandir
+from bcipy.helpers.load import load_json_parameters
 import random
 import pdb
 import numpy as np
+from rich.console import Console
+from rich.table import Table
 
 SYMBOLS = alphabet()
 DECISION_THRESHOLD = 0.8 # .60, .65 , .70, .75, .80, .85, .90, .95
 MAX_INQUIRIES = 11 # 3-11
-INQUIRY_LENGTH = 3  # seconds #TODO: don't use constants, calculate it from time_flash parameter
+# INQUIRY_LENGTH = 3  # seconds
 
-def session_simulation(data) -> list:
+def session_simulation(data, inquiry_length) -> list:
     series = data['series']
 
     _series = {
         # '0': {
-        #     'time_to_series': 0,
+        #     'time_to_series': 0,  # seconds
         # }
     } # top level key: series number, two keys: 'time_to_series'
     series_count = 0
@@ -34,7 +37,7 @@ def session_simulation(data) -> list:
             most_likely_letter = SYMBOLS[max_idx]
             # Calculate the end time for series
             if inq_count >= MAX_INQUIRIES:
-                _series[series_count]['time_to_series'] = INQUIRY_LENGTH * inq_count
+                _series[series_count]['time_to_series'] = inquiry_length * inq_count
                 _series[series_count]['inq_count'] = inq_count
                 if target_letter == most_likely_letter:
                     _series[series_count]['correct_selection'] = True
@@ -43,7 +46,7 @@ def session_simulation(data) -> list:
                 break
             
             if max_likelihood >= DECISION_THRESHOLD:
-                _series[series_count]['time_to_series'] = INQUIRY_LENGTH * inq_count
+                _series[series_count]['time_to_series'] = inquiry_length * inq_count
                 _series[series_count]['inq_count'] = inq_count
                 if target_letter == most_likely_letter:
                     _series[series_count]['correct_selection'] = True
@@ -61,13 +64,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data_folder', default=None)
     args = parser.parse_args()
-    data_folder = args.data_folder   #TODO: add all IP copy phrase into the same folder
+    data_folder = args.data_folder   #TODO: add all IP copy phrase for the same person into the same folder
     all_data = []
     for _data in fast_scandir(data_folder):
         print(_data)
         # retrieve target and nontarget likelihoods from EEG for a singe sessions
         data = json.load(open(f'{_data}/session.json'))
-        series_info = session_simulation(data)
+        CP_params = load_json_parameters(f'{_data}/parameters.json')
+        time_flash = float(CP_params["time_flash"]["value"])
+        time_fixation = float(CP_params["time_fixation"]["value"])
+        stim_length = float(CP_params["stim_length"]["value"])
+        inquiry_length = time_fixation + stim_length*time_flash   # seconds
+        print(inquiry_length)
+        series_info = session_simulation(data, inquiry_length)
         print(series_info)
         all_data.append(series_info)
     
