41b11ffbce8307247952a2135e3358c720455d0c
Basak Celik <celik.neu@gmail.com>
2022-07-20 17:42:06 -0400

    - Simulation results for all participants added to a worksheet - Script to create tables - Script to further analyze parameters for max accuracy

--- session_tables.py
+++ session_tables.py
@@ -1,4 +1,3 @@
-
 import json
 from bcipy.helpers.task import alphabet
 from bcipy.helpers.load import fast_scandir
@@ -10,13 +9,10 @@ import pandas as pd
 from rich.console import Console
 from rich.table import Table
 from tabulate import tabulate
-from scipy import stats
 
 SYMBOLS = alphabet()
-DECISION_THRESHOLD_RANGE = [.60, .65 , .70, .75, .80]
+DECISION_THRESHOLD_RANGE = [.60, .65 , .70, .75, .80, .85, .90, .95]
 MAX_INQUIRIES_RANGE = [3, 4, 5, 6, 7, 8, 9, 10, 11]   # max inquiries per series
-
-IDX_COND = ['3-.60', '3-.65', '3-.70', '3-.75', '3-.80', '4-.60', '4-.65', '4-.70', '4-.75', '4-.80', '5-.60', '5-.65', '5-.70', '5-.75', '5-.80', '6-.60', '6-.65', '6-.70', '6-.75', '6-.80', '7-.60', '7-.65', '7-.70', '7-.75', '7-.80', '8-.60', '8-.65', '8-.70', '8-.75', '8-.80', '9-.60', '9-.65', '9-.70', '9-.75', '9-.80', '10-.60', '10-.65', '10-.70', '10-.75', '10-.80', '11-.60', '11-.65', '11-.70', '11-.75', '11-.80']
 
 def session_simulation(data, inquiry_length, max_inquiries, decision_threshold) -> list:
     series = data['series']
@@ -73,12 +69,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data_folder', default=None)
     args = parser.parse_args()
-    data_folder = args.data_folder
-    # all_data = {}
-    all_data = []
-    optimal_idx = []
+    data_folder = args.data_folder 
+    dataframes = []
     for _data in fast_scandir(data_folder):
-        # print(_data)
+        all_data = {}
+        print(_data)
         # retrieve target and nontarget likelihoods from EEG for a singe sessions
         data = json.load(open(f'{_data}/session.json'))
         CP_params = load_json_parameters(f'{_data}/parameters.json')
@@ -86,12 +81,15 @@ if __name__ == "__main__":
         time_fixation = float(CP_params["time_fixation"]["value"])
         stim_length = float(CP_params["stim_length"]["value"])
         inquiry_length = time_fixation + stim_length*time_flash   # seconds
+        # print(f'Inquiry length: {inquiry_length} seconds')
 
         # Simulate the session with different IP parameters
-        new_list = []
-        for index, max_inquiries in enumerate(MAX_INQUIRIES_RANGE):
+        for max_inquiries in MAX_INQUIRIES_RANGE:
+            all_data[max_inquiries] = {}
             for decision_threshold in DECISION_THRESHOLD_RANGE:
+                # print(max_inquiries, decision_threshold)
                 series_info = session_simulation(data, inquiry_length, max_inquiries, decision_threshold)
+                # print(series_info)
 
                 phrase_completion_time = 0  # seconds
                 correct_selection = 0
@@ -101,28 +99,21 @@ if __name__ == "__main__":
 
                 accuracy = correct_selection / len(series_info) * 100  # percentage
                 accuracy = round(accuracy, 1)
-                new_list.append(accuracy)
-            
-        all_data.append(new_list)
-        optimal_idx.append(np.argmax(new_list))
-        # (optimal_idx+1) // len(DECISION_THRESHOLD_RANGE)
-
-            # [[.4, .5, .6 ... ][]]
-
-
-                # all_data[max_inquiries][decision_threshold] = str(phrase_completion_time)+"s, "+str(accuracy)+"%"
-                # all_data[max_inquiries][decision_threshold] = [phrase_completion_time, accuracy]
-
+                # print(f'Phrase completion time: {phrase_completion_time} seconds')
+                # print(f'Accuracy: {accuracy}%')
+                all_data[max_inquiries][decision_threshold] = str(phrase_completion_time)+"s, "+str(accuracy)+"%"
+        # print(all_data)
+        # print('\n')
+        # pdb.set_trace()
 
         # Create a table to display the results
         df = pd.DataFrame(all_data)
-        # print(tabulate(df, headers='keys'))
-        # print("\n")
-
-    print('Optimal Indexes: ', optimal_idx)
-    new_array = np.array(optimal_idx)
-    print(stats.describe(new_array))
-    pdb.set_trace()
-
-
-
+        dataframes.append(df)
+        print(tabulate(df, headers='keys'))
+        print("\n")
+   
+    startrow = 0
+    with pd.ExcelWriter('simulation_results.xlsx') as writer:
+        for dfs in dataframes:
+            dfs.to_excel(writer, sheet_name='All Subjects', startrow=startrow, header='keys')
+            startrow += len(dfs.index) + 1
