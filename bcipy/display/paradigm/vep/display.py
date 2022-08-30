2776d5d0a68d818069cb689835a42ddc8f37da80
Tab Memmott <tab.memmott@gmail.com>
2022-08-30 10:05:19 -0700

    lint + CHANGLEOG

--- bcipy/display/paradigm/vep/display.py
+++ bcipy/display/paradigm/vep/display.py
@@ -17,7 +17,7 @@ from bcipy.helpers.stimuli import resize_image, get_fixation
 
 def create_vep_codes(length=32, count=4) -> List[List[int]]:
     """Create a list of random VEP codes.
-    
+
     length - how many bits in each code. This should be greater than or equal to the refresh rate
         if using these to flicker. For example, if the refresh rate is 60Hz, then the length should
         be at least 60.
