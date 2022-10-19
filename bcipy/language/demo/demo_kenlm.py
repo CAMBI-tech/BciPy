6eee67a6bc241864563910045ff0153ec04666bf
Keith Vertanen <kdv123@gmail.com>
2022-10-19 10:01:19 -0600

    Example of using KenLM and a 12-gram character model.

---
+++ bcipy/language/demo/demo_kenlm.py
@@ -0,0 +1,46 @@
+# Basic sanity test of using KenLM to predict a sentence using a 12-gram character model.
+
+import kenlm
+
+if __name__ == "__main__":
+    # Load a really pruned n-gram language model
+    model = kenlm.LanguageModel("../lms/lm_dec19_char_12gram_1e-5_kenlm_probing.bin")
+
+    # Sum of the log prob of the sentence: <s> i like zebras. </s>
+    # Results using SRILM ngram utility:
+    # % ngram -lm lm_dec19_char_12gram_1e-5.arpa -order 12 -ppl zebras.txt -debug 2
+    # reading 34 1-grams
+    # reading 661 2-grams
+    # reading 4149 3-grams
+    # reading 4609 4-grams
+    # reading 5873 5-grams
+    # reading 6170 6-grams
+    # reading 7768 7-grams
+    # reading 7748 8-grams
+    # reading 8202 9-grams
+    # reading 7702 10-grams
+    # reading 7953 11-grams
+    # reading 8026 12-grams
+    # <s> i <sp> l i k e <sp> z e b r a s . </s>
+    # 	p( i | <s> ) 	= [2gram] 0.234358 [ -0.630121 ]
+    # 	p( <sp> | i ...) 	= [3gram] 0.503908 [ -0.297649 ]
+    # 	p( l | <sp> ...) 	= [4gram] 0.040375 [ -1.393890 ]
+    # 	p( i | l ...) 	= [5gram] 0.441208 [ -0.355357 ]
+    # 	p( k | i ...) 	= [6gram] 0.896195 [ -0.047597 ]
+    # 	p( e | k ...) 	= [7gram] 0.999651 [ -0.000152 ]
+    # 	p( <sp> | e ...) 	= [8gram] 0.929917 [ -0.031556 ]
+    # 	p( z | <sp> ...) 	= [2gram] 0.000114 [ -3.941586 ]
+    # 	p( e | z ...) 	= [2gram] 0.410016 [ -0.387199 ]
+    # 	p( b | e ...) 	= [2gram] 0.002148 [ -2.668019 ]
+    # 	p( r | b ...) 	= [2gram] 0.044926 [ -1.347505 ]
+    # 	p( a | r ...) 	= [2gram] 0.043585 [ -1.360661 ]
+    # 	p( s | a ...) 	= [2gram] 0.070985 [ -1.148835 ]
+    # 	p( . | s ...) 	= [2gram] 0.032252 [ -1.491447 ]
+    # 	p( </s> | . ...) 	= [3gram] 0.728636 [ -0.137489 ]
+    # 1 sentences, 14 words, 0 OOVs
+    # 0 zeroprobs, logprob= -15.2391 ppl= 10.374 ppl1= 12.260
+    #
+    # file zebras.txt: 1 sentences, 14 words, 0 OOVs
+    # 0 zeroprobs, logprob= -15.2391 ppl= 10.374 ppl1= 12.260
+    print(model.score("i <sp> l i k e <sp> z e b r a s .", bos=True, eos=True))
+
