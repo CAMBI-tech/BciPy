d2cfae4bbc376ae1c8850c5abc23e4e63026cd40
Dylan Gaines <dcgaines@mtu.edu>
2022-10-20 22:17:37 -0400

    Added the uniform model to the demo_ppl

--- bcipy/language/demo/demo_ppl.py
+++ bcipy/language/demo/demo_ppl.py
@@ -2,8 +2,12 @@ from bcipy.language.model.gpt2 import GPT2LanguageModel
 from bcipy.helpers.task import alphabet
 from bcipy.language.main import ResponseType
 from math import log10
+from bcipy.language.uniform import (ResponseType, UniformLanguageModel,
+                                    equally_probable)
 
 if __name__ == "__main__":
+
+    print("\nmodel = GPT2\n")
     
     symbol_set = alphabet()
     response_type = ResponseType.SYMBOL
@@ -12,6 +16,8 @@ if __name__ == "__main__":
     
     sentence = "i <sp> l i k e <sp> z e b r a s"
     accum = 0.0
+
+    print(f"sentence = '{sentence}'")
 
     tokens = sentence.split()
     symbols = len(tokens)
@@ -28,11 +34,36 @@ if __name__ == "__main__":
             correct_char = token.upper()
         score = 0.0    
         next_char_pred = lm.state_update(list(context))
-        # print(next_char_pred)
         p = next_char_pred[[c[0] for c in next_char_pred].index(correct_char)][1]
         score = log10(p)
         print(f"p( {token} | {prev} ...) = {p:.6f} [ {score:.6f} ]")
         accum += score
         prev = token
         context += token
-    print(f"sum logprob = {accum:.4f}, per-symbol logprob = {accum / symbols:.4f}, ppl = {pow(10,-1 * accum / symbols):.4f}")
+    print(f"sum logprob = {accum:.4f}, per-symbol logprob = {accum / symbols:.4f}, ppl = {pow(10,-1 * accum / symbols):.4f}\n")
+
+
+    print("\nmodel = Uniform\n")
+
+    lm = UniformLanguageModel()
+    accum = 0.0
+
+    prev = "<s>"
+    context = ""
+
+    for i,token in enumerate(tokens):
+        correct_char = ""
+        if(token == "<sp>"):
+            token = "_"
+            correct_char = "_"
+        else:
+            correct_char = token.upper()
+        score = 0.0    
+        next_char_pred = lm.predict(evidence=list(context))
+        p = next_char_pred[[c[0] for c in next_char_pred].index(correct_char)][1]
+        score = log10(p)
+        print(f"p( {token} | {prev} ...) = {p:.6f} [ {score:.6f} ]")
+        accum += score
+        prev = token
+        context += token
+    print(f"sum logprob = {accum:.4f}, per-symbol logprob = {accum / symbols:.4f}, ppl = {pow(10,-1 * accum / symbols):.4f}\n")
