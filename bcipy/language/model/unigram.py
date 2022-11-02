ca09eeda2f4b064035273c4102010a4e07357bc8
Dylan Gaines <dcgaines@mtu.edu>
2022-11-02 16:30:28 -0400

    Removed unnecessary code from unigram model. Load unigram weights from json file.

--- bcipy/language/model/unigram.py
+++ bcipy/language/model/unigram.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Tuple
 from bcipy.helpers.task import BACKSPACE_CHAR, SPACE_CHAR
 from bcipy.language.main import LanguageModel, ResponseType
+import json
 
 class UnigramLanguageModel(LanguageModel):
     """Character language model based on trained unigram weights"""
@@ -9,21 +10,17 @@ class UnigramLanguageModel(LanguageModel):
         
         super().__init__(response_type=response_type, symbol_set=symbol_set)
         self.model = None
-        self.tokenizer = None
-        self.vocab_size = 0
-        self.idx_to_word = None
-        self.lm_path = lm_path or None
+        self.lm_path = lm_path or "../lms/unigram.json"
 
         # Hard coding a unigram language model trained on ALS phrase dataset
         # for smoothing purpose
         # TODO: Load this from file
-        self.unigram_lm = {'E': 0.0998, 'T': 0.096, 'O': 0.0946, 'I': 0.0835,
-                           'A': 0.0774, 'N': 0.0554, SPACE_CHAR: 0.0523, 'H': 0.0504,
-                           'S': 0.0435, 'L': 0.0408, 'R': 0.0387, 'U': 0.0379,
-                           'D': 0.0358, 'Y': 0.0324, 'W': 0.0288, 'M': 0.0266,
-                           'G': 0.0221, 'C': 0.018, 'K': 0.016, 'P': 0.0145,
-                           'F': 0.0117, 'B': 0.0113, 'V': 0.0091, 'J': 0.0016,
-                           'X': 0.0008, 'Z': 0.0005, 'Q': 0.0002, BACKSPACE_CHAR: 0.0}
+
+        with open(self.lm_path) as json_file:
+            self.unigram_lm = json.load(json_file)
+
+        self.unigram_lm[SPACE_CHAR] = self.unigram_lm.pop("SPACE_CHAR")
+        self.unigram_lm[BACKSPACE_CHAR] = self.unigram_lm.pop("BACKSPACE_CHAR")
 
                            
         assert set(self.unigram_lm.keys()) == set(self.symbol_set), "invalid unigram model symbol set!"
@@ -32,22 +29,6 @@ class UnigramLanguageModel(LanguageModel):
 
     def supported_response_types(self) -> List[ResponseType]:
         return [ResponseType.SYMBOL]
-
-    # may not be needed
-    
-    # def __build_vocab(self) -> Dict[int, str]:
-    #     """
-    #     Build a vocabulary table mapping token index to word strings
-    #     Response:
-    #         a dict that maps token index to word strings
-    #     """
-
-    #     idx_to_word = {}
-    #     for idx in range(self.vocab_size):
-    #         word = self.tokenizer.decode([idx])
-    #         idx_to_word[idx] = word
-
-    #     return idx_to_word
 
     def predict(self, evidence: List[str]) -> List[Tuple]:
         """
