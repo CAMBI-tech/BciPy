a51427204d6d66041d2e0f057ce54d4ded6be9e2
Dylan Gaines <dcgaines@mtu.edu>
2022-11-02 14:17:52 -0400

    Refactored the gpt2.py model to extract the unigram portion to its own model. Created mixture.py model to handle the fusion of gpt2 with unigram.

---
+++ bcipy/language/model/mixture.py
@@ -0,0 +1,131 @@
+from collections import Counter
+from typing import Dict, List, Tuple
+import numpy as np
+from scipy.special import softmax
+
+from bcipy.helpers.task import BACKSPACE_CHAR, SPACE_CHAR, alphabet
+from bcipy.language.main import LanguageModel, ResponseType
+from bcipy.language.model.gpt2 import GPT2LanguageModel
+from bcipy.language.model.unigram import UnigramLanguageModel
+
+class MixtureLanguageModel(LanguageModel):
+    """Character language model that mixes GPT2 with a Unigram"""
+
+    def __init__(self, response_type: ResponseType, symbol_set: List[str], lm_path: str = None):
+        """
+        Initialize instance variables and load the language model with given path
+        Args:
+            response_type - SYMBOL only
+            symbol_set - list of symbol strings
+            lm_path - path to language model files
+        """
+        super().__init__(response_type=response_type, symbol_set=symbol_set)
+        self.gpt_model = None
+        self.unigram_model = None
+
+        # interpolate / rescale coefficients
+        self.interpolate_coeff = 0.8
+        self.rescale_coeff = 0.5
+
+        self.load()
+
+    def supported_response_types(self) -> List[ResponseType]:
+        return [ResponseType.SYMBOL]
+
+    @staticmethod
+    def interpolate_language_models(lm1: Dict[str, float], lm2: Dict[str, float], coeff: float) -> List[Tuple]:
+        """
+        interpolate two language models
+        Args:
+            lm1 - the first language model (a dict with char as keys and prob as values)
+            lm2 - the second language model (same type as lm1)
+            coeff - rescale coefficient, lm1 will be scaled by coeff and lm2 will be
+            scaled by (1-coeff)
+        Response:
+            a list of (char, prob) tuples representing an interpolated language model
+        """
+        combined_lm = Counter()
+
+        for char in lm1:
+            combined_lm[char] = lm1[char] * coeff + lm2[char] * (1 - coeff)
+
+        for char in lm2:
+            if char not in lm1:
+                combined_lm[char] = lm2[char] * (1 - coeff)
+
+        return list(sorted(combined_lm.items(), key=lambda item: item[1], reverse=True))
+
+    @staticmethod
+    def rescale(lm: Dict[str, float], coeff: float):
+        """
+        rescale a languge model with exponential coefficient
+        Args:
+            lm - the language model (a dict with char as keys and prob as values)
+            coeff - rescale coefficient
+        Response:
+            a list of (char, prob) tuples representing a rescaled language model
+        """
+        rescaled_lm = Counter()
+
+        # scale
+        for char in lm:
+            rescaled_lm[char] = lm[char] ** coeff
+
+        # normalize
+        sum_char_prob = sum(rescaled_lm.values())
+        for char in rescaled_lm:
+            rescaled_lm[char] /= sum_char_prob
+
+        return list(sorted(rescaled_lm.items(), key=lambda item: item[1], reverse=True))
+
+    def predict(self, evidence: List[str]) -> List[Tuple]:
+        """
+        Given an evidence of typed string, predict the probability distribution of
+        the next symbol
+        Args:
+            evidence - a list of characters (typed by the user)
+        Response:
+            A list of symbols with probability
+        """
+
+        gpt_pred = self.gpt_model.predict(evidence)
+
+        unigram_pred = self.unigram_model.predict(evidence)
+        
+        # interpolate language models to smooth the probability distribution returned
+        # by GPT2 language model
+        next_char_pred = MixtureLanguageModel.interpolate_language_models(
+            dict(gpt_pred), dict(unigram_pred), self.interpolate_coeff)
+
+        # exponentially rescale the language model
+        next_char_pred = MixtureLanguageModel.rescale(dict(next_char_pred), self.rescale_coeff)
+
+        return next_char_pred
+
+    def update(self) -> None:
+        """Update the model state"""
+        ...
+
+    def load(self) -> None:
+        """
+            Load the language models to be mixed
+        """
+
+        symbol_set = alphabet()
+        response_type = ResponseType.SYMBOL
+        self.gpt_model = GPT2LanguageModel(response_type, symbol_set)
+
+        self.unigram_model = UnigramLanguageModel(response_type, symbol_set)
+
+    def state_update(self, evidence: List[str]) -> List[Tuple]:
+        """
+            Wrapper method that takes in evidence text, and output probability distribution
+            of next character
+        Args:
+            evidence - a list of characters (typed by the user)
+        Response:
+            A list of symbol with probability
+        """
+        next_char_pred = self.predict(evidence)
+
+        return next_char_pred
