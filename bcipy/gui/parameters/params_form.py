495161502ae219957dd95923ae068abfedf83e05
lawhead <lawhead@ohsu.edu>
2022-11-17 13:25:43 -0800

    Updated params_form

--- bcipy/gui/parameters/params_form.py
+++ bcipy/gui/parameters/params_form.py
@@ -149,6 +149,7 @@ def clear_layout(layout):
     while layout.count():
         child = layout.takeAt(0)
         if child.widget() is not None:
+            child.widget().setVisible(False)
             child.widget().deleteLater()
         elif child.layout() is not None:
             clear_layout(child.layout())
