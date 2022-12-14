27f3a47d82783c153785c5d96e1b129589edb0da
Dylan Gaines <dcgaines@mtu.edu>
2022-12-14 12:14:26 -0500

    Added basic prediction timing to the ppl_calc script

--- bcipy/language/demo/ppl_calc.py
+++ bcipy/language/demo/ppl_calc.py
@@ -5,6 +5,8 @@ from bcipy.language.model.unigram import UnigramLanguageModel
 from bcipy.helpers.task import alphabet
 from bcipy.language.main import ResponseType
 from math import log10
+from timeit import default_timer as timer
+from bcipy.helpers.task import BACKSPACE_CHAR, SPACE_CHAR, alphabet
 import argparse
 
 
@@ -31,6 +33,8 @@ if __name__ == "__main__":
     phrase_file.close()
 
     lm = None
+
+    start = timer()
     
     symbol_set = alphabet()
     response_type = ResponseType.SYMBOL
@@ -48,9 +52,12 @@ if __name__ == "__main__":
         parser.print_help()
         exit()
 
+    print(f"Model load time = {timer() - start:.6f}")
+
     phrase_count = 0
     sum_per_symbol_logprob = 0.0
     zero_prob = 0
+    sum_per_symbol_time = 0.0
 
     # Iterate over phrases
     for phrase in phrases:
@@ -69,18 +76,26 @@ if __name__ == "__main__":
         prev_token = "<s>"
         context = ""
 
+        prediction_count = 0
+        predict_time_sum = 0
+
         # Iterate over characters in phrase
         for token in tokens:
+            start_predict = timer()
             correct_char = ""
 
             # BciPy treats space as underscore
             if(token == "<sp>"):
-                token = "_"
-                correct_char = "_"
+                token = SPACE_CHAR
+                correct_char = SPACE_CHAR
             else:
                 correct_char = token.upper()
             score = 0.0    
             next_char_pred = lm.state_update(list(context))
+
+            predict_time = timer() - start_predict
+            predict_time_sum += predict_time
+            prediction_count += 1
 
             # Find the probability for the correct character
             p = next_char_pred[[c[0] for c in next_char_pred].index(correct_char)][1]
@@ -89,6 +104,7 @@ if __name__ == "__main__":
                 accum = 1
                 if verbose >= 2:
                     print(f"p( {token} | {prev_token} ...) = 0")
+                    print(f"prediction time = {predict_time:.6f}")
                 break
             else:
                 score = log10(p)
@@ -96,27 +112,35 @@ if __name__ == "__main__":
                 # Character-level output
                 if verbose >= 2:
                     print(f"p( {token} | {prev_token} ...) = {p:.6f} [ {score:.6f} ]")
+                    print(f"prediction time = {predict_time:.6f}")
                 accum += score
                 prev_token = token
                 context += token
         
+        per_symbol_time = predict_time_sum / prediction_count
+        sum_per_symbol_time += per_symbol_time
+
         if accum == 1:
             if verbose >= 1:
                 print("Zero-prob event encountered, terminating phrase")
+                print(f"per-symbol prediction time = {per_symbol_time:.6f}\n")
         else:
             per_symbol_logprob = accum / symbols
 
             # Phrase-level output
             if verbose >= 1:
-                print(f"sum logprob = {accum:.4f}, per-symbol logprob = {per_symbol_logprob:.4f}, ppl = {pow(10,-1 * per_symbol_logprob):.4f}\n")
+                print(f"sum logprob = {accum:.4f}, per-symbol logprob = {per_symbol_logprob:.4f}, ppl = {pow(10,-1 * per_symbol_logprob):.4f}")
+                print(f"per-symbol prediction time = {per_symbol_time:.6f}\n")
             
             sum_per_symbol_logprob += per_symbol_logprob
             phrase_count += 1
 
     average_per_symbol_logprob = sum_per_symbol_logprob / phrase_count
+    average_per_symbol_time = sum_per_symbol_time / len(phrases)
 
     # Model-level output
     print(f"phrases = {phrase_count}, \
         average per symbol logprob = {average_per_symbol_logprob:.4f}, \
         ppl = {pow(10,-1 * average_per_symbol_logprob):.4f}, \
-        zero-prob events = {zero_prob}\n")
+        zero-prob events = {zero_prob}, \
+        average per symbol prediction time = {average_per_symbol_time:.6f}\n")
