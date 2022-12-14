3acd6b7172d9d42e03cb54346cf95fa2efe27e34
Keith Vertanen <kdv123@gmail.com>
2022-12-14 08:36:57 -0500

    Fine tuning BERT-like against several variants

--- bcipy/language/demo/finetune_bert.py
+++ bcipy/language/demo/finetune_bert.py
@@ -1,4 +1,7 @@
-# Exploring how to finetune a BERT like model
+# Exploring how to finetune an encoder model for doing classification.
+# This code works for a variety of the BERT-like models on Hugging Face.
+#
+# Started with BERT example from:
 # https://huggingface.co/docs/transformers/training
 
 from datasets import load_dataset
@@ -20,17 +23,18 @@ def tokenize_function(examples):
 
 if __name__ == "__main__":
 
-    model_name = "google/bert_uncased_L-2_H-128_A-2"           # Smallest BERT model
+    #model_name = "google/bert_uncased_L-2_H-128_A-2"           # Smallest BERT model
+    #model_name = "google/electra-small-discriminator"
+    model_name = "microsoft/mpnet-base"
+
     dataset = load_dataset("yelp_review_full")
 
-    # If you have a GPU, put everything on cuda
-    device = "cpu"
-    # device = "cuda"   # NVidia GPU
-    #device = "mps"    # M1 mac
-
     # Take only a subset of the data
-    train_small = dataset["train"].shuffle(seed=42).select(range(4000))
+    train_small = dataset["train"].shuffle(seed=42).select(range(1000))
     eval_small = dataset["test"].shuffle(seed=42).select(range(1000))
+
+    #print(f"{train_small[0]}")
+    #print(f"{eval_small[0]}")
 
     # Tokenize the subsets
     tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=512)   # Compared to example, had to add model_max_length
@@ -38,9 +42,13 @@ if __name__ == "__main__":
     eval_small_tokenized = eval_small.map(tokenize_function, batched=True)
 
     model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=5)
-    model.to(device)
 
-    training_args = TrainingArguments(output_dir="test_trainer")
+    training_args = TrainingArguments(seed=42,
+                                      save_strategy="epoch",
+                                      evaluation_strategy="epoch",
+                                      output_dir="out_bert",
+                                      resume_from_checkpoint="out_bert")
+
     metric = evaluate.load("accuracy")
 
     trainer = Trainer(
@@ -52,5 +60,5 @@ if __name__ == "__main__":
     )
 
     trainer.train()
-    results = trainer.evaluate()
-    print(f"{results}")
+#    results = trainer.evaluate()
+#    print(f"{results}")
