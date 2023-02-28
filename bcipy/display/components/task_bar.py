873a50e788edc69a61744896360d7ec331815e60
lawhead <lawhead@ohsu.edu>
2023-02-28 10:59:33 -0800

    Added CalibrationTaskBar and CopyPhraseTaskBar; refactoring

--- bcipy/display/components/task_bar.py
+++ bcipy/display/components/task_bar.py
@@ -6,12 +6,11 @@ from psychopy.visual.basevisual import BaseVisualStim
 import bcipy.display.components.layout as layout
 from bcipy.display.main import TaskDisplayProperties
 
-DEFAULT_TASK_PROPERTIES = TaskDisplayProperties(
-    task_color=['white'],
-    task_font='Courier New',
-    task_pos=(0, 0.9),
-    task_height=0.1,
-    task_text='HELLO_WORLD\n           ')
+DEFAULT_TASK_PROPERTIES = TaskDisplayProperties(task_color=['white'],
+                                                task_font='Courier New',
+                                                task_pos=(0, 0.9),
+                                                task_height=0.1,
+                                                task_text='')
 
 
 class TaskBar:
@@ -33,19 +32,27 @@ class TaskBar:
 
     def compute_height(self):
         """Computes the component height using the provided config."""
-        return (self.config.task_height * 2) + 0.05
+        return self.config.task_height + 0.05
 
     def init_stim(self) -> Dict[str, BaseVisualStim]:
         """Initialize the stimuli elements."""
 
-        task = self.centered_text_stim()
-        # task = self.left_aligned_text_stim()
+        task = self.text_stim(text=self.config.task_text,
+                              pos=(self.layout.left,
+                                   self.layout.vertical_middle),
+                              vertical_anchor='center',
+                              horizontal_anchor='left')
+
         return {'task_text': task, 'border': self.border_stim()}
 
     def draw(self):
         """Draw the task bar elements."""
         for _key, stim in self.stim.items():
             stim.draw()
+
+    def update(self, text: str = ''):
+        """Update the task bar to display the given text."""
+        self.stim['task_text'].text = text
 
     def border_stim(self):
         """Create the task bar outline"""
@@ -57,43 +64,109 @@ class TaskBar:
                                 pos=self.layout.center,
                                 size=(self.layout.width, self.layout.height))
 
-    def build_stim(self,
-                   pos: Tuple[float, float],
-                   vertical_alignment: str = 'center',
-                   horizontal_alignment: str = 'center') -> visual.TextStim:
-        """Builds a TextStim at the given position.
+    def text_stim(self,
+                  text: str,
+                  pos: Tuple[float, float],
+                  vertical_anchor: str = 'center',
+                  horizontal_anchor: str = 'center') -> visual.TextStim:
+        """Builds a TextStim at the given position using the configured properties.
 
         Parameters
         ----------
+            text - content to display
             pos - stim position (x, y) coordinate, in norm units
-            vertical_alignment - vertical alignment of 'top', 'bottom', or 'center'
-            horizontal_alignment - horizontal alignment of 'left', 'right', or 'center'
+            vertical_anchor - anchors text vertically at 'top', 'bottom', or 'center'.
+            horizontal_anchor - anchors text horizontally at 'left', 'right', or 'center'.
         """
-        assert vertical_alignment in ['top', 'bottom',
-                                      'center'], "Invalid option"
-        assert horizontal_alignment in ['left', 'right',
-                                        'center'], "Invalid option"
-        return visual.TextStim(name="task_text",
-                               win=self.layout.win,
-                               units=self.layout.units,
+        assert vertical_anchor in ['top', 'bottom', 'center'], "Invalid option"
+        assert horizontal_anchor in ['left', 'right',
+                                     'center'], "Invalid option"
+        return visual.TextStim(win=self.layout.win,
+                               text=text,
                                pos=pos,
-                               text=self.config.task_text,
+                               units=self.layout.units,
                                font=self.config.task_font,
                                height=self.config.task_height,
                                color=self.config.task_color[0],
-                               anchorVert=vertical_alignment,
-                               anchorHoriz=horizontal_alignment)
+                               anchorVert=vertical_anchor,
+                               anchorHoriz=horizontal_anchor)
 
-    def centered_text_stim(self) -> visual.TextStim:
-        """Creates a centered text stim."""
-        return self.build_stim(pos=self.layout.center,
-                               vertical_alignment='center',
-                               horizontal_alignment='center')
 
-    def left_aligned_text_stim(self) -> visual.TextStim:
-        """Creates a left-aligned text stim
-        """
-        return self.build_stim(pos=(self.layout.left,
-                                    self.layout.vertical_middle),
-                               vertical_alignment='center',
-                               horizontal_alignment='left')
+class CalibrationTaskBar(TaskBar):
+    """Task bar for Calibration tasks. Displays the count of inquiries.
+
+    Parameters
+    ----------
+        win - visual.Window on which to render elements
+        config - properties specifying fonts, colors, height, etc.
+        inquiry_count - total number of inquiries to complete
+        current_index - index of the current inquiry
+    """
+    def __init__(self,
+                 win: visual.Window,
+                 config: TaskDisplayProperties = DEFAULT_TASK_PROPERTIES,
+                 inquiry_count: int = 100,
+                 current_index: int = 1):
+        self.inquiry_count = inquiry_count
+        self.current_index = current_index
+        super().__init__(win, config)
+
+    def init_stim(self) -> Dict[str, BaseVisualStim]:
+        """Initialize the stimuli elements."""
+
+        task = self.text_stim(text=self.displayed_text(),
+                              pos=(self.layout.left,
+                                   self.layout.vertical_middle),
+                              vertical_anchor='center',
+                              horizontal_anchor='left')
+
+        return {'task_text': task, 'border': self.border_stim()}
+
+    def update(self, text: str = ''):
+        """Update the displayed text"""
+        if self.current_index < self.inquiry_count:
+            self.current_index += 1
+        self.stim['task_text'].text = self.displayed_text()
+
+    def displayed_text(self) -> str:
+        """Text to display. Computed from the current_index and inquiry_count. Ex. '(2/100)'"""
+        return f"({self.current_index}/{self.inquiry_count})"
+
+
+class CopyPhraseTaskBar(TaskBar):
+    """Task bar for the Copy Phrase Task"""
+    def __init__(self,
+                 win: visual.Window,
+                 config: TaskDisplayProperties = DEFAULT_TASK_PROPERTIES,
+                 spelled_text: str = ''):
+
+        # TODO: pad to length
+        self.spelled_text = spelled_text
+        super().__init__(win, config)
+
+    def compute_height(self):
+        """Computes the component height using the provided config."""
+        return (self.config.task_height * 2) + 0.05
+
+    def init_stim(self) -> Dict[str, BaseVisualStim]:
+        """Initialize the stimuli elements."""
+        task = self.text_stim(text=self.config.task_text,
+                              pos=self.layout.center,
+                              vertical_anchor='bottom',
+                              horizontal_anchor='center')
+        spelled = self.text_stim(text=self.spelled_text,
+                                 pos=self.layout.center,
+                                 vertical_anchor='top',
+                                 horizontal_anchor='center')
+        spelled.setColor(self.config.task_color[-1])
+
+        return {
+            'task_text': task,
+            'spelled_text': spelled,
+            'border': self.border_stim()
+        }
+
+    def update(self, text: str = ''):
+        """Update the task bar to display the given text."""
+        # TODO: pad the text for alignment
+        self.stim['spelled_text'].text = text
--- bcipy/display/demo/components/demo_task_bar.py
+++ bcipy/display/demo/components/demo_task_bar.py
@@ -3,11 +3,11 @@ import time
 
 from psychopy import visual
 
-from bcipy.display.components.task_bar import TaskBar
+from bcipy.display.components.task_bar import CalibrationTaskBar, CopyPhraseTaskBar
 
 
 def make_window():
-    """Make a sample window on which to draw"""
+    """Make a sample window on which to draw."""
     return visual.Window(size=[500, 500],
                          fullscr=False,
                          winType='pyglet',
@@ -15,14 +15,22 @@ def make_window():
                          waitBlanking=False,
                          color='black')
 
-def main(seconds: int=100):
+
+def demo_calibration(seconds: int = 30):
+    """Demo calibration task bar."""
     try:
         win = make_window()
-        task_bar = TaskBar(win)
-
+        task_bar = CalibrationTaskBar(win, inquiry_count=100, current_index=1)
         task_bar.draw()
         win.flip()
-        print(f'Displaying window for {seconds}s... (Interrupt [Ctl-C] to stop)\n')
+        print(
+            f'Displaying window for {seconds}s... (Interrupt [Ctl-C] to stop)\n'
+        )
+
+        time.sleep(2)
+        task_bar.update()
+        task_bar.draw()
+        win.flip()
 
         while True:
             time.sleep(seconds)
@@ -36,5 +44,37 @@ def main(seconds: int=100):
         win.close()
         print('Demo complete.')
 
+
+def demo_copy_phrase(seconds: int = 30):
+    """Demo copy phrase task bar."""
+    try:
+        win = make_window()
+        task_bar = CopyPhraseTaskBar(win, spelled_text='HELLO')
+
+        task_bar.draw()
+        win.flip()
+        print(
+            f'Displaying window for {seconds}s... (Interrupt [Ctl-C] to stop)\n'
+        )
+
+        time.sleep(1)
+        task_bar.update("HELLO_")
+        task_bar.draw()
+        win.flip()
+
+        while True:
+            time.sleep(seconds)
+            break
+    except KeyboardInterrupt:
+        print('Keyboard Interrupt: Demo stopped')
+    except Exception as e:
+        print(f'{e}')
+        raise e
+    finally:
+        win.close()
+        print('Demo complete.')
+
+
 if __name__ == '__main__':
-    main()
+    # demo_copy_phrase()
+    demo_calibration()
