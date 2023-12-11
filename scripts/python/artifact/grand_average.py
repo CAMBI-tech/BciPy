cd0ea9578f0b50a165b87023bdf3bb39fd075ede
Tab Memmott <tab.memmott@gmail.com>
2023-12-11 15:04:01 -0800

    WIP figure and stats generation

--- scripts/python/artifact/grand_average.py
+++ scripts/python/artifact/grand_average.py
@@ -49,35 +49,38 @@ if __name__ == "__main__":
     non_target_cf = []
     target_cf = []
     all_epochs_cf = []
+    non_target_of = []
+    target_of = []
+    all_epochs_of = []
     for session in Path(data_path).iterdir():
         if session.is_dir():
 
             try:
                 session_folder = str(session.resolve())
 
-                # # Online Filtering Data
-                # raw_data, data, labels, trigger_timing, channel_map, poststim_length, def_transform, _, channels = load_data_inquiries(
-                #                     data_folder=session_folder,
-                #                     trial_length=0.72,
-                #                     apply_filter=True)
+                # Online Filtering Data
+                raw_data, data, labels, trigger_timing, channel_map, poststim_length, def_transform, _, channels = load_data_inquiries(
+                                    data_folder=session_folder,
+                                    trial_length=0.72,
+                                    apply_filter=True)
                 
-                # # turn data and labels into epochs
-                # channel_types = ['eeg'] * len(channels)
-                # mne_info = mne.create_info(channels, sfreq=150, ch_types='eeg')
-                # ten_twenty_montage = mne.channels.make_standard_montage('standard_1020')
-                # epochs = mne.EpochsArray(data, info=mne_info)
-                # epochs.set_montage(ten_twenty_montage)
-                # # epochs.apply_function(lambda x: x * 1e-6)
-                # nt = []
-                # t = []
-                # for i, label in enumerate(labels):
-                #     if label == 0:
-                #         nt.append(epochs[i])
-                #     else:
-                #         t.append(epochs[i])
-                # non_target_of.append(mne.concatenate_epochs(nt))
-                # target_of.append(mne.concatenate_epochs(t))
-                # all_epochs_of.append(epochs)
+                # turn data and labels into epochs
+                channel_types = ['eeg'] * len(channels)
+                mne_info = mne.create_info(channels, sfreq=150, ch_types='eeg')
+                ten_twenty_montage = mne.channels.make_standard_montage('standard_1020')
+                epochs = mne.EpochsArray(data, info=mne_info)
+                epochs.set_montage(ten_twenty_montage)
+                # epochs.apply_function(lambda x: x * 1e-6)
+                nt = []
+                t = []
+                for i, label in enumerate(labels):
+                    if label == 0:
+                        nt.append(epochs[i])
+                    else:
+                        t.append(epochs[i])
+                non_target_of.append(mne.concatenate_epochs(nt))
+                target_of.append(mne.concatenate_epochs(t))
+                all_epochs_of.append(epochs)
 
                 # breakpoint()
                 # CF Data
@@ -93,30 +96,30 @@ if __name__ == "__main__":
                 target_cf.append(epochs['2'])
                 all_epochs_cf.append(epochs)
 
-                # load the mne auto artifact labelled data
-                mne_data = mne.io.read_raw_fif(f'{session}/{AUTO_ARTIFACT_LABELLED_FILENAME}')
-                raw_data, trial_data, labels, trigger_timing, channel_map, poststim_length, default_transform, dl, epochs  = load_data_mne(
-                    data_folder=session_folder,
-                    mne_data_annotations=mne_data.annotations,
-                    drop_artifacts=True,
-                    trial_length=0.72,
-                    parameters=parameters)
+                # # load the mne auto artifact labelled data
+                # mne_data = mne.io.read_raw_fif(f'{session}/{AUTO_ARTIFACT_LABELLED_FILENAME}')
+                # raw_data, trial_data, labels, trigger_timing, channel_map, poststim_length, default_transform, dl, epochs  = load_data_mne(
+                #     data_folder=session_folder,
+                #     mne_data_annotations=mne_data.annotations,
+                #     drop_artifacts=True,
+                #     trial_length=0.72,
+                #     parameters=parameters)
 
-                non_target_cf_auto.append(epochs['1'])
-                target_cf_auto.append(epochs['2'])
-                all_epochs_cf_auto.append(epochs)
+                # non_target_cf_auto.append(epochs['1'])
+                # target_cf_auto.append(epochs['2'])
+                # all_epochs_cf_auto.append(epochs)
 
-                mne_data = mne.io.read_raw_fif(f'{session}/{ARTIFACT_LABELLED_FILENAME}')
-                raw_data, trial_data, labels, trigger_timing, channel_map, poststim_length, default_transform, dl, epochs  = load_data_mne(
-                    data_folder=session_folder,
-                    mne_data_annotations=mne_data.annotations,
-                    drop_artifacts=True,
-                    trial_length=0.72,
-                    parameters=parameters)
+                # mne_data = mne.io.read_raw_fif(f'{session}/{ARTIFACT_LABELLED_FILENAME}')
+                # raw_data, trial_data, labels, trigger_timing, channel_map, poststim_length, default_transform, dl, epochs  = load_data_mne(
+                #     data_folder=session_folder,
+                #     mne_data_annotations=mne_data.annotations,
+                #     drop_artifacts=True,
+                #     trial_length=0.72,
+                #     parameters=parameters)
 
-                non_target_cf_sar.append(epochs['1'])
-                target_cf_sar.append(epochs['2'])
-                all_epochs_cf_sar.append(epochs)
+                # non_target_cf_sar.append(epochs['1'])
+                # target_cf_sar.append(epochs['2'])
+                # all_epochs_cf_sar.append(epochs)
             except Exception as e:
                 print(f"Error processing session {session}: \n {e}")
                 breakpoint()
@@ -126,36 +129,46 @@ if __name__ == "__main__":
     # breakpoint()
     # all_mne_epochs = mne.concatenate_epochs(all_epochs_of)
     # AAR
-    target_cf_auto = mne.concatenate_epochs(target_cf_auto)
-    target_cf_auto = target_cf_auto.crop(tmin=0.0, tmax=0.7)
-    target_cf_auto_evoked = target_cf_auto.average()
-    non_target_cf_auto = mne.concatenate_epochs(non_target_cf_auto)
-    non_target_cf_auto = non_target_cf_auto.crop(tmin=0.0, tmax=0.7)
-    non_target_cf_auto_evoked = non_target_cf_auto.average()
+    # target_cf_auto = mne.concatenate_epochs(target_cf_auto)
+    # target_cf_auto = target_cf_auto.crop(tmin=0.0, tmax=0.7)
+    # target_cf_auto_evoked = target_cf_auto.average()
+    # non_target_cf_auto = mne.concatenate_epochs(non_target_cf_auto)
+    # non_target_cf_auto = non_target_cf_auto.crop(tmin=0.0, tmax=0.7)
+    # non_target_cf_auto_evoked = non_target_cf_auto.average()
     
     # SAR
-    target_cf_sar = mne.concatenate_epochs(target_cf_sar)
-    target_cf_sar = target_cf_sar.crop(tmin=0.0, tmax=0.7)
-    target_cf_sar_evoked = target_cf_sar.average()
-    non_target_cf_sar = mne.concatenate_epochs(non_target_cf_sar)
-    non_target_cf_sar = non_target_cf_sar.crop(tmin=0.0, tmax=0.7)
-    non_target_cf_sar_evoked = non_target_cf_sar.average()
+    # target_cf_sar = mne.concatenate_epochs(target_cf_sar)
+    # target_cf_sar = target_cf_sar.crop(tmin=0.0, tmax=0.7)
+    # target_cf_sar_evoked = target_cf_sar.average()
+    # non_target_cf_sar = mne.concatenate_epochs(non_target_cf_sar)
+    # non_target_cf_sar = non_target_cf_sar.crop(tmin=0.0, tmax=0.7)
+    # non_target_cf_sar_evoked = non_target_cf_sar.average()
 
     # all_data_epochs = mne.concatenate_epochs([non_target_of, target_of])
-    # REGULAR
+    # REGULAR CF
     target_cf = mne.concatenate_epochs(target_cf)
     target_cf = target_cf.crop(tmin=0.0, tmax=0.7)
     target_cf_evoked = target_cf.average()
     non_target_cf = mne.concatenate_epochs(non_target_cf)
     non_target_cf = non_target_cf.crop(tmin=0.0, tmax=0.7)
     non_target_cf_evoked = non_target_cf.average()
+
+    # OF
+    target_of = mne.concatenate_epochs(target_of)
+    target_of = target_of.crop(tmin=0.0, tmax=0.7)
+    target_of_evoked = target_of.average()
+    non_target_of = mne.concatenate_epochs(non_target_of)
+    non_target_of = non_target_of.crop(tmin=0.0, tmax=0.7)
+    non_target_of_evoked = non_target_of.average()
     
     # data = [non_target_of, target_of]
     # visualize_evokeds(data, show=True)
 
-    # evokeds = { 'Online Filter Target': target_of_evoked, 'Online Filter Non-Target': non_target_of_evoked, 'Conventional Filter Non-Target': target_cf_evoked, 'Conventional Filter Target': non_target_cf_evoked }
+    evokeds = { 'Online Filter Target': target_of_evoked, 'Online Filter Non-Target': non_target_of_evoked, 'Conventional Filter Non-Target': target_cf_evoked, 'Conventional Filter Target': non_target_cf_evoked }
     
-    # all_diff_no = mne.combine_evoked([evokeds['Conventional Filter Target'], evokeds['Online Filter Target']], weights=[1, -1])
+    all_diff_no = mne.combine_evoked([evokeds['Conventional Filter Target'], evokeds['Online Filter Target']], weights=[1, -1])
+    all_diff_no.plot_joint(times=plot_joint_times, title='ERP Difference Between Online and Conventional Filtering')
+    all_diff_no.plot()
     # plot the averages using grand_average
     # target_evoked, non_target_evoked = group_average(target, non_target)
     # breakpoint()
@@ -163,14 +176,15 @@ if __name__ == "__main__":
     # non_target_evoked.plot_image()
     # visualize_joint_average((non_target, target), ['Non-Target', 'Target'], show=True, plot_joint_times=plot_joint_times)
 
-    roi = ['Pz', 'Cz', 'Oz', 'P3', 'P4', 'O1', 'O2']
-    evokeds_erp_compare = dict(cf_auto=list(target_cf_auto.pick_channels(roi).iter_evoked()),
-        cf=list(target_cf.pick_channels(roi).iter_evoked()),
-        cf_sar=list(target_cf_sar.pick_channels(roi).iter_evoked()))
-    fig = mne.viz.plot_compare_evokeds(evokeds_erp_compare, combine='mean', show=True, ci=False, show_sensors='upper right')
+    roi = ['Pz', 'Cz', 'P3', 'P4', 'O1', 'O2']
+    of_data = target_of.pick_channels(roi)
+    cf_data = target_cf.pick_channels(roi)
+    # roi = channels
+    evokeds_erp_compare = dict(of=list(target_of.pick_channels(roi).iter_evoked()),  cf=list(target_cf.pick_channels(roi).iter_evoked()),)
+    fig = mne.viz.plot_compare_evokeds(evokeds_erp_compare, combine='mean', show=True, show_sensors='upper right')
     # Show a ERP of the two events using gfp by default and no CI?
     # visualize_evokeds([target_cf, target_of], show=True)
-    # # plot_compare_evokeds([target_cf_evoked, non_target_cf_evoked], picks=roi, title='ERP Difference Between Online and Conventional Filtering')
+    plot_compare_evokeds([target_cf.average(), target_of.average()], picks=roi, title='ERP Difference Between Online and Conventional Filtering', combine='mean', ci=0.95, show=True)
     # eco = plot_compare_evokeds([cf_evokeds_diff, of_evokeds_diff], picks=roi, title='ERP Difference Between Online and Conventional Filtering')
     # plot_compare_evokeds(evokeds, picks=roi, title='ERP Difference Between Online and Conventional Filtering')
     # plt.show()
--- scripts/python/artifact/stats.py
+++ scripts/python/artifact/stats.py
@@ -1,19 +1,105 @@
+from scipy import stats
+from mne.stats import permutation_t_test, permutation_cluster_1samp_test
+import pandas as pd
+import numpy as np
 
+from bcipy.helpers.load import load_experimental_data
 
 def paired_ttest(df, col1, col2):
-    from scipy import stats
     return stats.ttest_rel(df[col1], df[col2])
 
 def one_way_anova(df, cols):
-    from scipy import stats
     return stats.f_oneway(*[df[col] for col in cols])
 
 def permutation_ttest_mne(data, perm_count=1000, threshold=2.0, tail=0):
-    from mne.stats import permutation_cluster_1samp_test
-
     # data = np.random.randn(2, 16, 1000)
     # data[1, :, :] += 1
-    return permutation_cluster_1samp_test(data, n_permutations=perm_count, threshold=threshold, tail=tail)
+    return permutation_t_test(data, n_permutations=perm_count, tail=tail)
 
 if __name__ == "__main__":
-    pass
+    
+    path = load_experimental_data()
+    filename = 'of_cf_flat_all_models.csv'
+    df = pd.read_csv(f'{path}/{filename}')
+    de_f = len(df) - 1  # degrees of freedom
+    alpha = 0.05
+    export_stats = {}
+    t_thresh = stats.distributions.t.ppf(1 - alpha / 2, df=de_f)
+    models = ['LR', 'SVM', 'NN', 'PRK', 'RF', 'LDA']
+
+
+    of_mcc_cols = [col for col in df.columns if 'OF' in col and 'mcc' in col and 'std' not in col and 'RF' not in col]
+    of_mcc_df = df[of_mcc_cols]
+    cf_mcc_cols = [col for col in df.columns if 'WD' in col and 'mcc' in col and 'std' not in col and 'RF' not in col]
+    cf_mcc_df = df[cf_mcc_cols]
+
+    of_cols = of_mcc_cols + cf_mcc_cols
+    all_mcc_df = df[of_cols]
+    # result_anova = one_way_anova(df, cols)
+
+    export_stats['MCC'] = {}
+    for model in models:
+        of_model_mcc_col = [col for col in of_mcc_df.columns if model in col]
+        of_model_mcc_df = of_mcc_df[of_model_mcc_col]
+        cf_model_mcc_col = [col for col in cf_mcc_df.columns if model in col]
+        cf_model_mcc_df = cf_mcc_df[cf_model_mcc_col]
+        # print(f'{model} OF vs CF MCC: {paired_ttest(df, of_model_mcc_col, cf_model_mcc_col)}')
+
+        # find the difference between OF and CF MCC for each model
+        diff = of_model_mcc_df.to_numpy() - cf_model_mcc_df.to_numpy()
+        # print(f'{model} OF vs CF MCC: {permutation_ttest_mne(diff)}')
+        # result = permutation_ttest_mne([df[col] for col in cols])
+        permutations = 50000
+        t_obs, clust, p_value, h_s = permutation_cluster_1samp_test(diff, n_permutations=permutations, threshold=t_thresh, adjacency=None, n_jobs=None, tail=0)
+        print(f'{model} OF vs CF MCC: {p_value}')
+        export_stats['MCC'][model] = {
+            'of_mean': of_model_mcc_df.mean(),
+            'of_std': of_model_mcc_df.std(),
+            'cf_mean': cf_model_mcc_df.mean(),
+            'cf_std': cf_model_mcc_df.std(),
+            'p_value': p_value,
+        }
+    
+    of_ba_cols = [col for col in df.columns if 'OF' in col and 'ba' in col and 'std' not in col and 'RF' not in col]
+    of_ba_df = df[of_ba_cols]
+    cf_ba_cols = [col for col in df.columns if 'WD' in col and 'ba' in col and 'std' not in col and 'RF' not in col]
+    cf_ba_df = df[cf_ba_cols]
+
+    of_cols = of_ba_cols + cf_ba_cols
+    all_ba_df = df[of_cols]
+
+    export_stats['BA'] = {}
+    for model in models:
+        of_model_ba_col = [col for col in of_ba_df.columns if model in col]
+        of_model_ba_df = of_ba_df[of_model_ba_col]
+        cf_model_ba_col = [col for col in cf_ba_df.columns if model in col]
+        cf_model_ba_df = cf_ba_df[cf_model_ba_col]
+        # print(f'{model} OF vs CF ba: {paired_ttest(df, of_model_ba_col, cf_model_ba_col)}')
+
+        # find the difference between OF and CF ba for each model
+        diff = of_model_ba_df.to_numpy() - cf_model_ba_df.to_numpy()
+        # print(f'{model} OF vs CF ba: {permutation_ttest_mne(diff)}')
+        # result = permutation_ttest_mne([df[col] for col in cols])
+        permutations = 50000
+        t_obs, clust, p_value, h_s = permutation_cluster_1samp_test(diff, n_permutations=permutations, threshold=t_thresh, adjacency=None, n_jobs=None, tail=0)
+        print(f'{model} OF vs CF ba: {p_value}')
+        export_stats['BA'][model] = {
+            'of_mean': of_model_ba_df.mean(),
+            'of_std': of_model_ba_df.std(),
+            'cf_mean': cf_model_ba_df.mean(),
+            'cf_std': cf_model_ba_df.std(),
+            'p_value': p_value,
+        }
+
+    # get the average of all models
+    of_all_mcc_df = of_mcc_df.mean(axis=1)
+    cf_all_mcc_df = cf_mcc_df.mean(axis=1)
+    of_all_ba_df = of_ba_df.mean(axis=1)
+    cf_all_ba_df = cf_ba_df.mean(axis=1)
+
+    # obvs, p_values, h_stats = permutation_t_test(all_mcc_df, n_permutations=permutations, tail=0)
+    breakpoint()
+
+    # find the rows with the largest difference between OF and CF in SVM mcc
+    df['diff'] = df['OF_NAR_IIR_SVM_mcc'] - df['WD_NAR_IIR_SVM_mcc']
+    df['diff'] = df['OF_NAR_IIR_PRK_mcc'] - df['WD_NAR_IIR_PRK_mcc']
