5a550d13ce5d5ae942d1b07453b795841eaafd27
lawhead <lawhead@ohsu.edu>
2023-12-12 09:37:46 -0800

    Cleanup

--- bcipy/helpers/acquisition.py
+++ bcipy/helpers/acquisition.py
@@ -165,7 +165,7 @@ def init_lsl_client(parameters: dict,
     """Initialize a client that acquires data from LabStreamingLayer."""
 
     data_buffer_seconds = round(max_inquiry_duration(parameters))
-
+    log.info(f"Setting an acquisition buffer of {data_buffer_seconds} seconds")
     return LslAcquisitionClient(max_buffer_len=data_buffer_seconds,
                                 device_spec=device_spec,
                                 save_directory=save_folder,
