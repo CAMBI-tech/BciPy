e91f1ff415bdb5432c0cde61eb19216f91997793
sreekaroo <sreekaroo@gmail.com>
2024-02-04 19:01:12 -0500

    backspace prob included in lm evidence

--- bcipy/simulator/helpers/model_handler.py
+++ bcipy/simulator/helpers/model_handler.py
@@ -5,8 +5,8 @@ from typing import Optional
 
 import numpy as np
 
-from bcipy.helpers.language_model import init_language_model, histogram
-from bcipy.helpers.symbols import alphabet
+from bcipy.helpers.language_model import init_language_model, histogram, with_min_prob
+from bcipy.helpers.symbols import alphabet, BACKSPACE_CHAR
 from bcipy.language import LanguageModel
 from bcipy.signal.model import SignalModel
 from bcipy.simulator.helpers.state_manager import SimState
@@ -59,6 +59,7 @@ class SigLmModelHandler1(ModelHandler):
         self.signal_model: Optional[SignalModel] = None
         self.lm_model: LanguageModel = init_language_model(parameters)
         self.alp = alphabet()
+        self.backspace_prob: float = 0.05
 
         with open(self.sm_model_file, "rb") as signal_file:
             self.signal_model = pickle.load(signal_file)
@@ -68,10 +69,14 @@ class SigLmModelHandler1(ModelHandler):
         eeg_evidence = self.signal_model.predict(eeg_responses, stimuli, self.alp)
         lm_evidence = self.lm_model.predict(
             list(state.current_sentence)) if state.inquiry_n == 0 else None
-        # TODO check lm_parameters for default backspace prob
 
         ret_evidence = {}
         if lm_evidence is not None:
+
+            # adjusting evidence to include backspace prob
+            if BACKSPACE_CHAR in stimuli:
+                lm_evidence = with_min_prob(lm_evidence, (BACKSPACE_CHAR, self.backspace_prob))
+
             ret_evidence['lm'] = SimEvidence(EvidenceType.LM, np.array(lm_evidence), self.alp)
             log.debug(f"LM evidence for '{state.current_sentence}' -> \n {histogram(lm_evidence)}")
 
