19d98d118961d3fd57648ae004659dc4723efcdf
Srikar Ananthoju <46632027+sreekaroo@users.noreply.github.com>
2024-02-24 16:28:28 -0800

    Multiple run feature for sim (#317)
    * moving artifact related code into helper file
    
    * multi run save directory and logging configs done
    
    * averaged metrics from multi run. abstracted up some fields into simulator_base.py.py

--- bcipy/simulator/__main__.py
+++ bcipy/simulator/__main__.py
@@ -2,53 +2,11 @@
 
 import argparse
 import datetime
-import logging
-import os
-import random
-import sys
 from pathlib import Path
 
-from bcipy.simulator.helpers.decision import MaxIterationsSim
+from bcipy.simulator.helpers.sim_runner import MultiSimRunner, SingleSimRunner
 from bcipy.simulator.sim_factory import SimulationFactoryV2
 from bcipy.simulator.simulator_base import Simulator
-
-
-def configure_logger(log_path, file_name):
-    """ configures logger for standard out nad file output """
-
-    log = logging.getLogger(None)  # configuring root logger
-    log.setLevel(logging.DEBUG)
-    # Create handlers for logging to the standard output and a file
-    stdoutHandler = logging.StreamHandler(stream=sys.stdout)
-    fileHandler = logging.FileHandler(f"{log_path}/{file_name}.log")
-
-    # Set the log levels on the handlers
-    stdoutHandler.setLevel(logging.INFO)
-    fileHandler.setLevel(logging.DEBUG)
-
-    # Create a log format using Log Record attributes
-    fmt_file = logging.Formatter("%(levelname)s | %(filename)s >> %(message)s")
-
-    fmt = logging.Formatter("%(message)s")
-
-    # Set the log format on each handler
-    stdoutHandler.setFormatter(fmt)
-    fileHandler.setFormatter(fmt_file)
-
-    # Add each handler to the Logger object
-    log.addHandler(stdoutHandler)
-    log.addHandler(fileHandler)
-
-
-def init_save_dir(output_path, save_dir_name):
-    # creating wrapper dir to save to within /generated. Adds a unique 4 digit id to end
-    unique_id = random.sample(range(1000, 10000), 1)[0]
-    save_dir = f"{output_path}/SIM_{save_dir_name}_{unique_id}"
-    os.makedirs(save_dir)
-    os.makedirs(f"{save_dir}/logs")
-
-    return save_dir
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -65,26 +23,22 @@ if __name__ == "__main__":
         type=Path,
         required=True,
         help="Signal models to be used")
-
     parser.add_argument(
         "-p",
         "--parameters",
         type=Path,
         required=True,
         help="Parameter File to be used")
-
     parser.add_argument("-o", "--out_dir", type=Path, default=Path(__file__).resolve().parent)
-
     args = vars(parser.parse_args())
-
-    output_path = "bcipy/simulator/generated"  # TODO read from parameters
-    now_time = datetime.datetime.now().strftime("%m-%d-%H:%M")
-    args['save_dir'] = init_save_dir(output_path, now_time)
-
-    # setting up logging
-    log_path = f"{args['save_dir']}/logs"
-    configure_logger(log_path, now_time)
 
     simulator: Simulator = SimulationFactoryV2.create(**args)
 
-    simulator.run()
+    sim_run_count = simulator.get_parameters().get('sim_run_count', 1)
+
+    if sim_run_count > 1:  # running multiple times
+        runner = MultiSimRunner(simulator, sim_run_count)
+        runner.run()
+    else:
+        runner = SingleSimRunner(simulator)
+        runner.run()
--- bcipy/simulator/simulator_base.py
+++ bcipy/simulator/simulator_base.py
@@ -3,6 +3,11 @@
 from abc import ABC, abstractmethod
 
 from bcipy.helpers.parameters import Parameters
+from bcipy.simulator.helpers import metrics
+from bcipy.simulator.helpers.data_engine import DataEngine
+from bcipy.simulator.helpers.model_handler import ModelHandler
+from bcipy.simulator.helpers.sampler import Sampler
+from bcipy.simulator.helpers.state_manager import StateManager
 
 
 class Simulator(ABC):
@@ -14,6 +19,13 @@ class Simulator(ABC):
     - run closed loop simulation of {TaskType} with {data} with {simulation_params}
     """
 
+    model_handler: ModelHandler
+    sampler: Sampler
+    state_manager: StateManager
+    data_engine: DataEngine
+    referee: metrics.MetricReferee
+    parameters: Parameters
+
     @abstractmethod
     def run(self):
         """ Run loop for simulation"""
@@ -21,3 +33,6 @@ class Simulator(ABC):
     @abstractmethod
     def get_parameters(self) -> Parameters:
         """ retrieving parameters copy"""
+
+    def reset(self):
+        ...
