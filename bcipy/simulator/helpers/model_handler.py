29e1ab2bbde38eb688da5c33438d51f71f74b0d8
sreekaroo <sreekaroo@gmail.com>
2024-03-25 15:38:34 -0400

    LM always on. Read lm_type from sim_parameters.json

--- bcipy/simulator/helpers/model_handler.py
+++ bcipy/simulator/helpers/model_handler.py
@@ -6,13 +6,14 @@ from typing import Optional
 import numpy as np
 
 from bcipy.helpers.language_model import (histogram, init_language_model,
-                                          with_min_prob)
+                                          with_min_prob, language_models_by_name)
 from bcipy.helpers.symbols import BACKSPACE_CHAR, alphabet
-from bcipy.language import LanguageModel
+from bcipy.language import LanguageModel, ResponseType
 from bcipy.signal.model import SignalModel
 from bcipy.simulator.helpers.state_manager import SimState
 from bcipy.simulator.helpers.types import SimEvidence
 from bcipy.task.data import EvidenceType
+import inspect
 
 log = logging.getLogger(__name__)
 
@@ -47,6 +48,33 @@ class SignalModelHandler1(ModelHandler):
         return self.signal_model
 
 
+def sim_init_lm(parameters) -> LanguageModel:
+    """
+    Init Language Model configured in the sim parameters.
+    Mostly copied from language_model.init_lang_model
+
+    Parameters
+    ----------
+        parameters : dict
+            configuration details and path locations
+
+    Returns
+    -------
+        instance of a LanguageModel
+    """
+
+    lm_classes = language_models_by_name()
+    model_class: LanguageModel.__class__ = lm_classes[parameters.get("sim_lm_type", "UNIFORM")]
+    # introspect the model arguments to determine what parameters to pass.
+    args = inspect.signature(model_class).parameters.keys()
+
+    # select the relevant parameters into a dict.
+    params = {key: parameters[key] for key in args & parameters.keys()}
+    return model_class(response_type=ResponseType.SYMBOL,
+                       symbol_set=alphabet(parameters),
+                       **params)
+
+
 class SigLmModelHandler1(ModelHandler):
 
     def __init__(self, sm_model_file, parameters):
@@ -58,7 +86,8 @@ class SigLmModelHandler1(ModelHandler):
         """
         self.sm_model_file = sm_model_file
         self.signal_model: Optional[SignalModel] = None
-        self.lm_model: LanguageModel = init_language_model(parameters)
+        self.lm_model: LanguageModel = sim_init_lm(parameters)
+        log.info(f"Language Model Type: {self.lm_model.name()}")
         self.alp = alphabet()
         self.backspace_prob: float = parameters.get('lm_backspace_prob', 0.05)
 
--- bcipy/simulator/sim_factory.py
+++ bcipy/simulator/sim_factory.py
@@ -1,15 +1,14 @@
 """ Factory for Simulator objects """
+import logging
 from pathlib import Path
 from typing import List
 
 from bcipy.helpers.load import load_json_parameters
-from bcipy.simulator.helpers.data_engine import (RawDataEngine,
-                                                 RawDataEngineWrapper)
+from bcipy.simulator.helpers.data_engine import (RawDataEngine)
 from bcipy.simulator.helpers.metrics import (MetricReferee, RefereeImpl,
                                              SimMetrics1Handler)
 from bcipy.simulator.helpers.model_handler import (ModelHandler,
-                                                   SigLmModelHandler1,
-                                                   SignalModelHandler1)
+                                                   SigLmModelHandler1, )
 from bcipy.simulator.helpers.sampler import EEGByLetterSampler, Sampler
 from bcipy.simulator.helpers.state_manager import (StateManager,
                                                    StateManagerImpl)
@@ -24,6 +23,7 @@ class SimulationFactoryV2:
                sim_param_path="bcipy/simulator/sim_parameters.json", save_dir=None, **kwargs):
         # out_dir = kwargs.get('out_dir', Path(__file__).resolve().parent)
 
+        # combining parameters
         model_file = Path(smodel_files.pop())
         sim_parameters = load_json_parameters(sim_param_path, value_cast=True)
         base_parameters = load_json_parameters(kwargs.get('parameters'), value_cast=True)
@@ -32,8 +32,7 @@ class SimulationFactoryV2:
         data_engine = RawDataEngine(source_dirs, base_parameters)
         state_manager: StateManager = StateManagerImpl(base_parameters)
         sampler: Sampler = EEGByLetterSampler(data_engine)
-        model_handler: ModelHandler = SigLmModelHandler1(model_file, base_parameters) \
-            if sim_parameters.get("sim_lm_active", 0) == 1 else SignalModelHandler1(model_file)
+        model_handler: ModelHandler = SigLmModelHandler1(model_file, base_parameters)
         referee: MetricReferee = RefereeImpl(metric_handlers={'basic': SimMetrics1Handler()})
 
         sim = SimulatorCopyPhrase(data_engine, model_handler, sampler, state_manager, referee,
