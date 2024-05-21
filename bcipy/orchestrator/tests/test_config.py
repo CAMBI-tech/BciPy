64393dab22f9e2879fc05a9204b970932cf901bb
Carson Reader <carsonthemonkey@gmail.com>
2024-05-21 16:35:07 -0700

    Adds additional tests for action parsing

--- bcipy/orchestrator/tests/test_config.py
+++ bcipy/orchestrator/tests/test_config.py
@@ -1,15 +1,22 @@
 import unittest
 from bcipy.orchestrator.config import parse_sequence, serialize_sequence, validate_sequence_string
 from bcipy.task import TaskType
+from bcipy.orchestrator.actions import Action, OfflineAnalysisAction
 
 
 class TestConfig(unittest.TestCase):
 
     def test_parses_one_task(self) -> None:
-        actions = 'RSVP Calibration'
-        parsed = parse_sequence(actions)
+        sequence = 'RSVP Calibration'
+        parsed = parse_sequence(sequence)
         assert len(parsed) == 1
         assert parsed[0] == TaskType.RSVP_CALIBRATION
+
+    def test_parses_one_action(self) -> None:
+        actions = 'Offline Analysis'
+        parsed = parse_sequence(actions)
+        assert len(parsed) == 1
+        assert parsed[0] is OfflineAnalysisAction
 
     def test_parses_multiple_tasks(self) -> None:
         actions = 'RSVP Calibration -> RSVP Copy Phrase'
@@ -17,7 +24,20 @@ class TestConfig(unittest.TestCase):
         assert len(parsed) == 2
         assert parsed[0] == TaskType.RSVP_CALIBRATION
         assert parsed[1] == TaskType.RSVP_COPY_PHRASE
-        
+    
+    def test_parses_actions_and_tasks(self) -> None:
+        sequence = 'RSVP Calibration -> Offline Analysis -> RSVP Copy Phrase'
+        parsed = parse_sequence(sequence)
+        assert len(parsed) == 3
+        assert parsed[0] == TaskType.RSVP_CALIBRATION
+        assert parsed[1] is OfflineAnalysisAction
+        assert parsed[2] == TaskType.RSVP_COPY_PHRASE
+    
+    def test_throws_exception_on_invalid_action(self) -> None:
+        actions = 'RSVP Calibration -> does not exist'
+        with self.assertRaises(ValueError):
+            parse_sequence(actions)
+
     def test_throws_exception_on_invalid_task(self) -> None:
         actions = 'RSVP Calibration -> RSVP Copy Phrase -> does not exist'
         with self.assertRaises(ValueError):
