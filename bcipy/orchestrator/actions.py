a67693bbf69edce50231717841aa6cc3c78cadf9
Tab Memmott <tab.memmott@gmail.com>
2024-05-27 12:48:24 -0700

    Add more actions

--- bcipy/orchestrator/actions.py
+++ bcipy/orchestrator/actions.py
@@ -1,32 +1,76 @@
-from abc import ABC
+from task import Task
+from typing import Optional
+import subprocess
 
-class Action(ABC):
+class CallbackAction(Task):
     """
-    Abstract class for actions
+    Action for running a callback.
     """
-    @staticmethod
-    def get_all_actions():
-        """
-        Returns a list of all actions
-        """
-        return Action.__subclasses__()
-    label: str
-    @classmethod
-    def match(cls, action_str: str) -> bool:
-        """
-        Determines if the action matches the given string
-        """
-        return action_str == cls.label
+
+    def __init__(self, callback: callable, *args, **kwargs) -> None:
+        super(CallbackAction, self).__init__()
+        self.callback = callback
+        self.args = args
+        self.kwargs = kwargs
 
     def execute(self):
-        ...
+        self.logger.info(f'Executing callback action {self.callback} with args {self.args} and kwargs {self.kwargs}')
+        self.callback(*self.args, **self.kwargs)
+        self.logger.info(f'Callback action {self.callback} executed')
+        return self.name
+    
+    @property
+    def name(self):
+        return 'CallbackAction'
 
+class CodeHookAction(Task):
+    """
+    Action for running generic code hooks.
+    """
 
-class OfflineAnalysisAction(Action):
-    """
-    Action for offline analysis
-    """
-    label = 'Offline Analysis'
+    def __init__(self, code_hook: str, subprocess=True) -> None:
+        super(CodeHookAction, self).__init__()
+        self.code_hook = code_hook
+        self.subprocess = subprocess
 
     def execute(self):
-        raise NotImplementedError
+        if self.subprocess:
+            subprocess.Popen(self.code_hook, shell=True)
+    
+        else:
+            subprocess.run(self.code_hook, shell=True)
+
+        return self.code_hook
+    
+    @property
+    def name(self):
+        return 'CodeHookAction'
+    
+
+class OfflineAnalysisAction(Task):
+    """
+    Action for running offline analysis.
+    """
+
+    def __init__(self, data_directory: str, parameters_path: Optional[str] = None, alert=True) -> None:
+        super(OfflineAnalysisAction, self).__init__()
+        self.parameters_path = parameters_path
+        self.alert = alert
+        self.data_directory = data_directory
+
+    def execute(self):
+        cmd = self.construct_command()
+        subprocess.Popen(cmd, shell=True)
+        return self.data_directory
+    
+    def construct_command(self):
+        command = 'python -m bcipy.signal.model.offline_analysis'
+        command += ' --data_directory ' + self.data_directory
+        if self.parameters_path:
+            command += ' --parameters_path ' + self.parameters_path
+        if self.alert:
+            command += ' --alert'
+        return command
+
+    def name(self):
+        return 'OfflineAnalysisAction'
--- bcipy/orchestrator/session_orchestrator.py
+++ bcipy/orchestrator/session_orchestrator.py
@@ -8,7 +8,7 @@ from bcipy.helpers.parameters import Parameters
 from bcipy.helpers.validate import validate_bcipy_session, validate_experiment
 from bcipy.helpers.visualization import visualize_session_data
 from bcipy.main import execute_task
-from bcipy.task import TaskType
+from bcipy.task import Task
 from bcipy.config import DEFAULT_EXPERIMENT_ID, DEFAULT_PARAMETERS_PATH
 from bcipy.signal.model import SignalModel
 from bcipy.language.main import LanguageModel
@@ -17,19 +17,14 @@ from bcipy.helpers.save import init_save_data_structure
 from bcipy.helpers.system_utils import get_system_info
 
 
-@dataclass
-class TaskInfo:
-    """A Task to be executed, with config specific to it"""
+from bcipy.orchestrator.actions import CallbackAction, CodeHookAction, OfflineAnalysisAction
 
-    task_type: TaskType
-    parameter_location: str
-    visualize: bool = True
-    alert: bool = False
-    pause_after: bool = True
 
+# Test SessionOrchestrator using Actions 
+# Should execute return a status code or a boolean?
 
 class SessionOrchestrator:
-    tasks: List[TaskInfo]
+    tasks: List[Task]
     signal_models: Union[List[SignalModel], None] = None
     language_model: LanguageModel = None
     parameters: Parameters
@@ -45,71 +40,39 @@ class SessionOrchestrator:
         parameters_path: str = DEFAULT_PARAMETERS_PATH,
     ) -> None:
         validate_experiment(experiment_id)
-        self.parameters_path = parameters_path
+        self.parameters_path = parameters_path # TODO: load parameters and cast them to the correct values
         self.user = user
         self.experiment_id = experiment_id
         self.log = logging.getLogger(__name__)
         self.sys_info = get_system_info()
         self.tasks = []
+        
+        self.session_data = []
+        # TODO create datasave structure and provide it to the tasks
 
-    def queue_task(self, task: TaskInfo) -> None:
+        self.ready_to_execute = False
+
+    def add_task(self, task) -> None:
         # Loading task specific parameters could happen here
         self.tasks.append(task)
 
     def execute(self) -> None:
         """Executes queued tasks in order"""
         for task in self.tasks:
-            self._execute_task(task)
-            if task.pause_after:
-                # pause here and show ui?
-                pass
+            data_save_location = init_save_data_structure(self.experiment_id, self.user, self.parameters_path, task)
+            self.session_data.append(data_save_location)
+            with task.initialize(self.parameters, data_save_location) as task:
+                task.execute() # TODO: add an __exit__ could be used to cleanup the session and would be called automatically in case of an exception
 
-    def _execute_task(self, task: TaskInfo):
-        """Executes a single task"""
-
-        parameters = load_json_parameters(task.parameter_location, value_cast=True)
-        if not validate_bcipy_session(parameters, False):  # fake is false for now
-            raise TaskConfigurationException('Invalid task parameters')
-        parameters['parameter_location'] = task.parameter_location
-
-        if task.parameter_location != DEFAULT_PARAMETERS_PATH:
-            parameters.save()
-            default_params = load_json_parameters(DEFAULT_PARAMETERS_PATH, value_cast=True)
-            if parameters.add_missing_items(default_params):
-                msg = 'Paramaters file out of date'
-                self.log.exception(msg)
-                raise Exception(msg)  # TODO: use a more descriptive exception
-
-        save_folder = init_save_data_structure(
-            parameters['data_save_loc'],
-            self.user,
-            task.parameter_location,
-            task=task.task_type.label,
-            experiment_id=self.experiment_id
-        )
-
-        if execute_task(
-            task.task_type,
-            parameters,
-            save_folder,
-            task.alert,
-            fake=False
-        ):
-            if task.visualize:
-                try:
-                    visualize_session_data(save_folder, parameters)
-                except Exception as e:
-                    self.log.info(f'Error visualizing session data: {e}')
-                return True
-            return False
+    def save(self) -> None:
+        # Save the session data
+        ...
 
 
 def demo_orchestrator():
+    action1 = CodeHookAction("echo 'Hello World'")
     orchestrator = SessionOrchestrator()
-    task = TaskInfo(TaskType.RSVP_CALIBRATION, DEFAULT_PARAMETERS_PATH)
-    orchestrator.queue_task(task)
-    task = TaskInfo(TaskType.MATRIX_CALIBRATION, DEFAULT_PARAMETERS_PATH)
-    orchestrator.queue_task(task)
+
     orchestrator.execute()
 
 
