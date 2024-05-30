836e284f911936a3cfaac7f2a535d5ead7e88800
Carson Reader <carsonthemonkey@gmail.com>
2024-05-29 17:12:32 -0700

    removes randomly failing test

--- bcipy/helpers/tests/test_stimuli.py
+++ bcipy/helpers/tests/test_stimuli.py
@@ -730,7 +730,6 @@ class TestStimuliGeneration(unittest.TestCase):
 
     def test_best_case_inquiry_gen_with_inq_constants(self):
         """Test best_case_rsvp_inq_gen with inquiry constants"""
-
         alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
         n = 5
 
@@ -761,7 +760,6 @@ class TestStimuliGeneration(unittest.TestCase):
         for letter in expected:
             self.assertTrue(letter in first_inq)
 
-        self.assertNotEqual(expected, first_inq, 'Should be in random order.')
         self.assertEqual([1] + ([0.2] * n), times[0])
         self.assertEqual(['red'] + (['white'] * n), colors[0])
 
