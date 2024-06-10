248bf8621ca8585db2e7b1fb2fd8c4fbdc7045ae
Carson Reader <carsonthemonkey@gmail.com>
2024-06-10 16:06:10 -0700

    fixes orchestrator tests

--- bcipy/orchestrator/tests/test_orchestrator.py
+++ bcipy/orchestrator/tests/test_orchestrator.py
@@ -1,5 +1,7 @@
 import unittest
-from bcipy.orchestrator.orchestrator import SessionOrchestrator, TaskInfo
+from unittest.mock import Mock
+from bcipy.orchestrator.orchestrator import SessionOrchestrator
+from bcipy.task import Task
 from bcipy.task import TaskType
 from bcipy.config import DEFAULT_PARAMETERS_PATH
 from bcipy.helpers.exceptions import UnregisteredExperimentException
@@ -14,11 +16,8 @@ class TestSessionOrchestrator(unittest.TestCase):
             _ = SessionOrchestrator(experiment_id=experiment)
 
     def test_orchestrator_queues_task(self) -> None:
+        task = Mock(spec=Task)
         orchestrator = SessionOrchestrator()
-        task = TaskInfo(
-            TaskType.RSVP_CALIBRATION,
-            DEFAULT_PARAMETERS_PATH,
-        )
         assert len(orchestrator.tasks) == 0
-        orchestrator.queue_task(task)
+        orchestrator.add_task(task)
         assert len(orchestrator.tasks) == 1
