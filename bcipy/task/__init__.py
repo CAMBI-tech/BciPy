4cc7f5d7a86ae03ad8a99794fbe7a4b20c142989
Carson Reader <carsonthemonkey@gmail.com>
2024-07-08 13:26:29 -0700

    imports tasks in their module's `__init__.py`
    This makes all tasks available to the task registry when it is initialized

--- bcipy/task/__init__.py
+++ bcipy/task/__init__.py
@@ -4,7 +4,14 @@ This import statement allows users to import submodules from Task
 from .main import Task
 from .task_registry import TaskType
 
+# Makes the following classes available to the task registry
+import bcipy.orchestrator.actions
+from . import base_calibration
+from .paradigm import rsvp, vep, matrix
+from .task_registry import TaskRegistry
+
 __all__ = [
     'Task',
     'TaskType',
+    'TaskRegistry'
 ]
---
+++ bcipy/task/paradigm/rsvp/__init__.py
@@ -0,0 +1,4 @@
+# Import all RSVP tasks to make them available to the task registry
+from .calibration.calibration import RSVPCalibrationTask
+from .calibration.timing_verification import RSVPTimingVerificationCalibration
+from .copy_phrase import RSVPCopyPhraseTask
