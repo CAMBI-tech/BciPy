61013a791049cda135b326b219fed2763d592443
Carson Reader <carsonthemonkey@gmail.com>
2024-08-09 16:03:32 -0700

    add beginning of intertask action ui

---
+++ bcipy/gui/intertask_action.py
@@ -0,0 +1,46 @@
+from bcipy.gui.bciui import BCIUI, run_bciui
+from PyQt6.QtWidgets import QLabel
+from PyQt6.QtWidgets import (
+    QWidget,
+    QApplication,
+    QVBoxLayout,
+    QLabel,
+    QHBoxLayout,
+    QPushButton,
+    QComboBox,
+    QScrollArea,
+    QLineEdit,
+    QLayout,
+    QSizePolicy,
+    QMessageBox,
+    QProgressBar
+)
+
+
+class IntertaskAction(BCIUI):
+
+    def __init__(self, total_tasks: int = 0, current_task_index: int = 0):
+        self.total_tasks = total_tasks
+        self.current_task_index = current_task_index
+        print('initialized')
+        super().__init__("Progress", 400, 300)
+
+    def app(self):
+        self.contents.addLayout(BCIUI.centered(QLabel("Experiment Progress")))
+
+        self.progress = QProgressBar()
+        self.progress.setValue(75)
+        self.progress.setTextVisible(False)
+        self.contents.addWidget(self.progress)
+
+        self.contents.addStretch(1)
+        self.next_button = QPushButton("Next")
+        self.stop_button = QPushButton("Stop")
+        self.stop_button.setStyleSheet("background-color: red")
+        buttons_layout = QHBoxLayout()
+        buttons_layout.addWidget(self.stop_button)
+        buttons_layout.addWidget(self.next_button)
+        self.contents.addLayout(buttons_layout)
+
+if __name__ == "__main__":
+    run_bciui(IntertaskAction)
