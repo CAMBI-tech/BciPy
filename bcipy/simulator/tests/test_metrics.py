e71c244d1b4afbeb1f85b51082c454467a34777f
lawhead <lawhead@ohsu.edu>
2024-12-17 13:05:04 -0800

    Add duration metrics

--- bcipy/simulator/tests/test_metrics.py
+++ bcipy/simulator/tests/test_metrics.py
@@ -2,7 +2,8 @@
 import unittest
 from unittest.mock import patch
 
-from bcipy.simulator.metrics import (add_item, add_items, get_final_typed,
+from bcipy.simulator.metrics import (add_item, add_items, calculate_duration,
+                                     get_final_typed, plot_save_path,
                                      rename_df_column, summarize)
 
 SAMPLE_SERIES_DATA = {
@@ -86,7 +87,7 @@ class TestSimMetrics(unittest.TestCase):
         """Test session metrics accumulation"""
         combined = {}
         session_data = SAMPLE_SESSION1
-        add_items(combined, session_data)
+        add_items(combined, session_data, inquiry_seconds=2)
         for key in [
                 "total_number_series", "total_inquiries", "total_selections",
                 "inquiries_per_selection", "task_summary__selections_correct",
@@ -97,17 +98,23 @@ class TestSimMetrics(unittest.TestCase):
             self.assertTrue(key in combined)
             self.assertEqual(1, len(combined[key]))
 
+    @patch('bcipy.simulator.metrics.max_inquiry_duration')
+    @patch('bcipy.simulator.metrics.sim_parameters')
     @patch('bcipy.simulator.metrics.load')
     @patch('bcipy.simulator.metrics.open')
     @patch('bcipy.simulator.metrics.session_paths')
-    def test_summarize(self, session_path_mock, open_mock, json_mock):
+    def test_summarize(self, session_path_mock, open_mock, json_mock,
+                       sim_parameters_mock, max_inquiry_duration_mock):
         """Test summary function"""
         session_path_mock.return_value = [
             'run1/session.json', 'run2/session.json'
         ]
         json_mock.side_effect = [SAMPLE_SESSION1, SAMPLE_SESSION2]
+        max_inquiry_duration_mock.return_value = 2
         result = summarize("sim-dir")
 
+        sim_parameters_mock.assert_called_once_with("sim-dir")
+        max_inquiry_duration_mock.assert_called_once()
         self.assertEqual(open_mock.call_count, 2)
         for key in [
                 "total_number_series", "total_inquiries", "total_selections",
@@ -125,6 +132,15 @@ class TestSimMetrics(unittest.TestCase):
                          rename_df_column("total_inquiries"))
         self.assertEqual("selections_correct",
                          rename_df_column("task_summary__selections_correct"))
+
+    def test_plot_name(self):
+        """Test naming for plots"""
+        self.assertEqual("sim_dir/metrics.png", plot_save_path("sim_dir"))
+
+    def test_compute_duration(self):
+        """Test the calculation of the overall session duration"""
+        self.assertEqual(
+            250, calculate_duration(inquiry_count=100, inquiry_seconds=2.5))
 
 
 if __name__ == '__main__':
