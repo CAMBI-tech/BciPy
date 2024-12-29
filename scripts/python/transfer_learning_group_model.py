c9c5a8fa051ec9dc7bdcd5c3341d35a033fbebff
Tab Memmott <tab.memmott@gmail.com>
2024-12-29 13:27:30 -0800

    Add pytorch implementation of EEGNet

--- scripts/python/transfer_learning_group_model.py
+++ scripts/python/transfer_learning_group_model.py
@@ -20,19 +20,28 @@ from bcipy.config import (
     DEFAULT_PARAMETERS_PATH)
 from bcipy.io.load import load_experimental_data, load_raw_data, load_json_parameters
 from bcipy.core.triggers import trigger_decoder, TriggerType
+from bcipy.io.save import save_model
 from bcipy.helpers.acquisition import analysis_channels
 from bcipy.core.stimuli import InquiryReshaper, update_inquiry_timing
 from bcipy.core.parameters import Parameters
 from bcipy.signal.process import (ERPTransformParams,
                                   filter_inquiries, get_default_transform)
+from bcipy.signal.model import PcaRdaKdeModel, SignalModel
+from eeg_net import EEGNet
+from bcipy.signal.model.base_model import SignalModelMetadata
+from bcipy.helpers.utils import report_execution_time
 
 import bcipy.acquisition.devices as devices
+from bcipy.acquisition.devices import DeviceSpec
+from bcipy.signal.process import Composition
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras import utils as np_utils
+from sklearn.model_selection import train_test_split
+
 devices_by_name = devices.load(
         Path(DEFAULT_DEVICES_PATH, DEFAULT_DEVICE_SPEC_FILENAME), replace=True)
-logging.basicConfig(
-            level=logging.DEBUG, 
-            format='%(levelname)s: %(message)s'
-        )
+
+GROUP_MODEL_NAME = "EEGNet_RSVP_ME_group_model"
 
 class DirectoryFinder(QMainWindow):
     def __init__(
@@ -51,12 +60,7 @@ class DirectoryFinder(QMainWindow):
         """
         super().__init__()
         
-        # Configure logging
-        logging.basicConfig(
-            level=logging.INFO, 
-            format='%(levelname)s: %(message)s'
-        )
-        
+
         self.root_directory = os.path.abspath(root_directory)
         self.default_search_pattern = default_search_pattern
         self.validate_bcipy = validate_bcipy
@@ -275,7 +279,10 @@ def find_directories(
     
     return finder.selected_paths
 
-def load_group_trials(directories: List[str], parameters: Parameters) -> Tuple[List[np.ndarray], List[int]]:
+def load_group_trials(
+        directories: List[str],
+        parameters: Parameters,
+        external_model: bool = True) -> Tuple[np.ndarray, List[int], DeviceSpec, Composition]:
     """Given a list of directories, load the group trials for training a model.
     
     It should return two lists: one containing the trials and the other containing the labels.
@@ -283,6 +290,9 @@ def load_group_trials(directories: List[str], parameters: Parameters) -> Tuple[L
     Args:
         directories (list): List of directories containing valid experimental data.
         parameters (Parameters): Parameters object containing the necessary parameters for loading trials.
+        external_model (bool, optional): Whether to use an external model. Defaults to False.
+            The default will return the data in (Channels X Trials X Samples).
+            If True, the trials will be transposed before returning into (Trials X Channels x Samples).
 
     Returns:
         tuple: A tuple containing the list of trials and the list of labels.
@@ -340,7 +350,7 @@ def load_group_trials(directories: List[str], parameters: Parameters) -> Tuple[L
         # The timestamp column [0] is already excluded.
         channel_map = analysis_channels(channels, device_spec)
         channels_used = [channels[i] for i, keep in enumerate(channel_map) if keep == 1]
-        logging.info(f'Channels used in analysis: {channels_used}')
+        print(f'Channels used in analysis: {channels_used}')
 
         data, fs = erp_data.by_channel()
 
@@ -364,22 +374,88 @@ def load_group_trials(directories: List[str], parameters: Parameters) -> Tuple[L
         # define the training classes using integers, where 0=nontargets/1=targets
         labels = inquiry_labels.flatten().tolist()
 
-        all_trials.extend(data)
+        if external_model:
+            data = np.transpose(data, (1, 0, 2))
+        all_trials.append(data)
         all_labels.extend(labels)
-        
-    return all_trials, all_labels
+    
+    if not external_model:
+        all_trials = np.concatenate(all_trials, axis=1)
+    else:
+        all_trials = np.concatenate(all_trials, axis=0)
+    return all_trials, all_labels, device_spec, default_transform
 
-def train_model(trials: List[np.ndarray], labels: List[int]):
+@report_execution_time
+def train_model(
+        trials: np.ndarray,
+        labels: List[int],
+        device_spec: dict = None,
+        default_transform: dict = None,
+        model_type: str = "EEGNet") -> SignalModel:
     """
     Train a model using the given trials and labels.
+
+    TODO implement a scikit-learn model to train on the trials and labels
     
     Args:
         trials (list): List of trials to train the model on
         labels (list): List of labels corresponding to the trials
+        device_spec (dict, optional): Device specification dictionary
+        default_transform (dict, optional): Default transformation dictionary
     """
-    # Insert model training code here
-    logging.info("Model training not implemented yet.")
-    pass
+    if model_type == "PcaRdaKdeModel":
+        # Data is in the form (Channels X Trials X Samples)
+        # Insert model training code here
+        model = PcaRdaKdeModel()
+        print("Training model... This may take a while.")
+        model.fit(trials, labels)
+        model.metadata = SignalModelMetadata(
+            device_spec=device_spec,
+            transform=default_transform,
+            evidence_type="ERP",
+            auc=model.auc)
+        save_model(model, f"{GROUP_MODEL_NAME}_{model.auc:0.4f}.pkl")
+    
+    if model_type == "EEGNet":
+        # Split the data into training, testing, and validation sets (60/20/20)
+        chans, samples, kernels = trials.shape[1], trials.shape[2], 1
+        print(f"Channels: {chans}, Samples: {samples}, Kernels: {kernels}")
+        X_train, X_test, Y_train, Y_test = train_test_split(trials, labels, test_size=0.2, random_state=42)
+        X_validate, X_test, Y_validate, Y_test = train_test_split(X_test, Y_test, test_size=0.5, random_state=42)
+        X_train      = X_train.reshape(X_train.shape[0], chans, samples, kernels)
+        X_validate   = X_validate.reshape(X_validate.shape[0], chans, samples, kernels)
+        X_test       = X_test.reshape(X_test.shape[0], chans, samples, kernels)
+        
+        # make labels np.array
+        Y_test       = np.array(Y_test)
+        Y_train      = np.array(Y_train)
+        Y_validate   = np.array(Y_validate)
+        Y_train      = Y_train.reshape(Y_train.shape[0], 1)
+        Y_validate   = Y_validate.reshape(Y_validate.shape[0], 1)
+        Y_test       = Y_test.reshape(Y_test.shape[0], 1)
+                # Insert model training code here
+        model = EEGNet(nb_classes=2, Chans=chans, Samples=samples)
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        # count number of parameters in the model
+        numParams    = model.count_params()
+        print(f"Number of parameters in model: {numParams}")
+        class_weights = {0:1, 1:1}
+
+        # set a valid path for your system to record model checkpoints
+        checkpointer = ModelCheckpoint(filepath='/tmp/checkpoint.keras', verbose=1,
+                                    save_best_only=True)
+        fittedModel = model.fit(X_train, Y_train, epochs = len(labels), batch_size = 16,
+                        verbose = 2, validation_data=(X_validate, Y_validate),
+                        callbacks=[checkpointer], class_weight = class_weights)
+
+
+        model.load_weights('/tmp/checkpoint.keras')
+        probs       = model.predict(X_test)
+        preds       = probs.argmax(axis = -1)  
+        acc         = np.mean(preds == Y_test.argmax(axis=-1))
+        print("Classification accuracy: %f " % (acc))
+        breakpoint()
+    return model
 
 # Example usage
 if __name__ == "__main__":
@@ -389,7 +465,7 @@ if __name__ == "__main__":
     # Find and select directories
     selected_dirs = find_directories(
         start_dir,
-        search_pattern="*Calibration*",
+        search_pattern="*RSVP_Calibration*",
         recursive=True,
         validate_bcipy=True
     )
@@ -405,8 +481,12 @@ if __name__ == "__main__":
     # Load group trials
     parameters = load_json_parameters(parameters_path, True)
     
-    trials, labels = load_group_trials(selected_dirs, parameters)
-    logging.info(
-        f"Loaded {len(trials)} trials and {len(labels)} labels. Accrued from {len(selected_dirs)} directories.")
+    trials, labels, device_spec, transform = load_group_trials(
+        selected_dirs,
+        parameters,
+        external_model=True)
+    print(
+        f"Loaded {trials.shape} and {len(labels)} labels. Accrued from {len(selected_dirs)} directories.")
 
-    train_model(trials, labels)
+    model = train_model(trials, labels, device_spec, transform)
+    breakpoint()  # Debugging breakpoint
