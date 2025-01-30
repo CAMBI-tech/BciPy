51c9d210c7db8058cc0d2f0ee2a104cca2958455
lawhead <lawhead@ohsu.edu>
2025-01-30 10:49:53 -0800

    Refactored args for InquiryRangeSampler for better understandability

--- bcipy/simulator/data/sampler/inquiry_range_sampler.py
+++ bcipy/simulator/data/sampler/inquiry_range_sampler.py
@@ -9,40 +9,56 @@ log = logging.getLogger(__name__)
 
 
 class InquiryRangeSampler(TargetNontargetSampler):
-    """Sampler that queries a subset of inquiries for each series."""
+    """Sampler that queries a subset of inquiries for each series.
+
+    Parameters
+    ----------
+        data_engine - data repository to be queried
+        inquiry_start - index of first inquiry to include (starting at 1)
+        inquiry_count - number of inquiries to include
+        series_start - index of first series to include (starting at 1)
+        series_end - optional index of last series to include
+    """
 
     def __init__(self,
                  data_engine: RawDataEngine,
-                 inquiry_end: int = 3,
-                 inquiry_start: int = 0,
+                 inquiry_start: int = 1,
+                 inquiry_count: int = 3,
                  series_start: int = 1,
-                 series_end: Optional[int] = None):
+                 series_count: Optional[int] = None):
         super().__init__(data_engine)
-        assert inquiry_start >= 0, "Inquiry_start can't be negative"
-        assert inquiry_end >= inquiry_start, "Range definition error"
+        if inquiry_start < 1:
+            raise ValueError("inquiry_start can't be less than 1")
+        if inquiry_count < 1:
+            raise ValueError("inquiry_count must be at least 1")
+
         self.inquiry_start = inquiry_start
-        self.inquiry_end = inquiry_end
+        self.inquiry_count = inquiry_count
         self.series_start = series_start
-        self.series_end = series_end
+        self.series_count = series_count
 
     def query_filters(self, symbol: str, is_target: bool) -> List[QueryFilter]:
         """Expression used to query for a single sample."""
+        # In the data, inquiries are 0-indexed.
+        inq_start = self.inquiry_start - 1
+        inq_end = inq_start + self.inquiry_count
         filters = [
             QueryFilter('target', '==', int(is_target)),
-            QueryFilter('series_inquiry', '>=', self.inquiry_start),
-            QueryFilter('series_inquiry', '<=', self.inquiry_end),
+            QueryFilter('series_inquiry', '>=', inq_start),
+            QueryFilter('series_inquiry', '<', inq_end),
             QueryFilter('series', '>=', self.series_start)
         ]
-        if self.series_end:
-            filters.append(QueryFilter('series', '<=', self.series_end))
+        if self.series_count:
+            series_end = self.series_start + self.series_count
+            filters.append(QueryFilter('series', '<', series_end))
         return filters
 
     def __str__(self):
         fields = [
-            f"inquiry_start='{self.inquiry_start}'",
-            f"inquiry_end={self.inquiry_end}",
+            f"inquiry_start={self.inquiry_start}",
+            f"inquiry_count={self.inquiry_count}",
             f"series_start={self.series_start}",
-            f"series_end={self.series_end}"
+            f"series_count={self.series_count}"
         ]
         return f"InquiryRangeSampler({', '.join(fields)})"
 
--- bcipy/simulator/tests/test_inquiry_range_sampler.py
+++ bcipy/simulator/tests/test_inquiry_range_sampler.py
@@ -12,27 +12,63 @@ class InquiryRangeSamplerTest(unittest.TestCase):
 
     def test_init(self):
         """Test initialization"""
-        sampler = InquiryRangeSampler(data_engine=Mock(), inquiry_end=3)
+        sampler = InquiryRangeSampler(data_engine=Mock(), inquiry_count=3)
 
-        self.assertEqual(0, sampler.inquiry_start)
-        self.assertEqual(3, sampler.inquiry_end)
-
-        self.assertRaises(
-            AssertionError, lambda: InquiryRangeSampler(
-                data_engine=Mock(), inquiry_end=3, inquiry_start=-1))
+        self.assertEqual(1, sampler.inquiry_start)
+        self.assertEqual(3, sampler.inquiry_count)
 
         self.assertRaises(
-            AssertionError, lambda: InquiryRangeSampler(
-                data_engine=Mock(), inquiry_end=1, inquiry_start=2))
+            ValueError, lambda: InquiryRangeSampler(
+                data_engine=Mock(), inquiry_count=3, inquiry_start=-1))
+
+        self.assertRaises(
+            ValueError, lambda: InquiryRangeSampler(
+                data_engine=Mock(), inquiry_count=0, inquiry_start=2))
 
     def test_query_filters(self):
         """Test that queries are limited to the configured range"""
-        sampler = InquiryRangeSampler(data_engine=Mock(), inquiry_end=3)
+        sampler = InquiryRangeSampler(data_engine=Mock(), inquiry_count=3)
 
         filters = sampler.query_filters(symbol='A', is_target=True)
 
+        target_filter = QueryFilter('target', '==', 1)
+        nontarget_filter = QueryFilter('target', '==', 0)
         start_filter = QueryFilter('series_inquiry', '>=', 0)
-        end_filter = QueryFilter('series_inquiry', '<=', 3)
+        end_filter = QueryFilter('series_inquiry', '<', 3)
+        series_start_filter = QueryFilter('series', '>=', 1)
+
+        self.assertEqual(4, len(filters))
+        self.assertTrue(target_filter in filters)
+        self.assertFalse(nontarget_filter in filters)
+        self.assertTrue(start_filter in filters)
+        self.assertTrue(end_filter in filters)
+        self.assertTrue(series_start_filter in filters)
+
+        filters = sampler.query_filters(symbol='A', is_target=False)
+        self.assertFalse(target_filter in filters)
+        self.assertTrue(nontarget_filter in filters)
+
+    def test_filters_with_series_count(self):
+        """Test when a series count is provided."""
+        sampler = InquiryRangeSampler(data_engine=Mock(),
+                                      inquiry_count=3,
+                                      series_start=2,
+                                      series_count=2)
+        series_start_filter = QueryFilter('series', '>=', 2)
+        series_end_filter = QueryFilter('series', '<', 4)
+        filters = sampler.query_filters(symbol='A', is_target=True)
+        self.assertEqual(5, len(filters))
+        self.assertTrue(series_start_filter in filters)
+        self.assertTrue(series_end_filter in filters)
+
+    def test_filter_with_nondefault_start(self):
+        """Test with non-defaults"""
+        sampler = InquiryRangeSampler(data_engine=Mock(),
+                                      inquiry_start=5,
+                                      inquiry_count=1)
+        start_filter = QueryFilter('series_inquiry', '>=', 4)
+        end_filter = QueryFilter('series_inquiry', '<', 5)
+        filters = sampler.query_filters(symbol='A', is_target=True)
 
         self.assertTrue(start_filter in filters)
         self.assertTrue(end_filter in filters)
