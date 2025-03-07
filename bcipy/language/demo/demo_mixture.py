6e1c4b582204bf5555b66dfad8dc673f1b820fc3
Dylan Gaines <dcgaines@mtu.edu>
2025-03-07 10:10:24 -0500

    Updated demos to use new adapter classes

--- bcipy/language/demo/demo_mixture.py
+++ bcipy/language/demo/demo_mixture.py
@@ -1,22 +1,37 @@
-from bcipy.language.model.mixture import MixtureLanguageModel
-from bcipy.core.symbols import alphabet
+from bcipy.language.model.mixture import MixtureLanguageModelAdapter
+from bcipy.core.symbols import DEFAULT_SYMBOL_SET
 from bcipy.language.main import ResponseType
 
 
 if __name__ == "__main__":
-    symbol_set = alphabet()
     response_type = ResponseType.SYMBOL
-    lm = MixtureLanguageModel(response_type, symbol_set)
+    # Load the default mixture model from lm_params.json
+    lm = MixtureLanguageModelAdapter(response_type, DEFAULT_SYMBOL_SET)
 
-    next_char_pred = lm.state_update(list("does_it_make_sen"))
-    print(next_char_pred)
+    print("Target sentence: does_it_make_sense\n")
+
+    next_char_pred = lm.predict(list("does_it_make_sen"))
+    print(f"Context: does_it_make_sen")
+    print(f"Predictions: {next_char_pred}")
     correct_char_rank = [c[0] for c in next_char_pred].index("S") + 1
-    print(correct_char_rank)
-    next_char_pred = lm.state_update(list("does_it_make_sens"))
-    print(next_char_pred)
+    print(f"Correct character rank: {correct_char_rank}\n")
+
+    next_char_pred = lm.predict(list("does_it_make_sens"))
+    print(f"Context: does_it_make_sens")
+    print(f"Predictions: {next_char_pred}")
     correct_char_rank = [c[0] for c in next_char_pred].index("E") + 1
-    print(correct_char_rank)
-    next_char_pred = lm.state_update(list("does_it_make_sense"))
-    print(next_char_pred)
+    print(f"Correct character rank: {correct_char_rank}\n")
+
+    next_char_pred = lm.predict(list("does_it_make_sense"))
+    print(f"Context: does_it_make_sense")
+    print(f"Predictions: {next_char_pred}")
     correct_char_rank = [c[0] for c in next_char_pred].index("_") + 1
-    print(correct_char_rank)
+    print(f"Correct character rank: {correct_char_rank}\n")
+
+    print("Target sentence: i_like_zebras\n")
+
+    next_char_pred = lm.predict(list("i_like_zebra"))
+    print(f"Context: i_like_zebra")
+    print(f"Predictions: {next_char_pred}")
+    correct_char_rank = [c[0] for c in next_char_pred].index("S") + 1
+    print(f"Correct character rank: {correct_char_rank}\n")
