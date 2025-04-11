fff7893a68e8d3e82a1aadb1df7d58f11412b1d1
Tab Memmott <tab.memmott@gmail.com>
2025-04-10 18:54:15 -0700

    Remove unused channels and add the ref

--- bcipy/core/raw_data.py
+++ bcipy/core/raw_data.py
@@ -417,8 +417,7 @@ def get_1020_channels() -> List[str]:
     return [
         'Fp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'T3', 'C3', 'Cz', 'C4',
         'T4', 'T5', 'P3', 'Pz', 'P4', 'T6', 'O1', 'O2', 'PO7', 'PO3', 'POz',
-        'PO4', 'PO8', 'Oz', 'FC1', 'FC2', 'CP1', 'CP2', 'FC5', 'FC6', 'CP5',
-        'CP6', 'TP9', 'TP10'
+        'PO4', 'PO8', 'Oz', 'A1', 'A2',
     ]
 
 
